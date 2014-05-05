07c39ef999a27fe08ef2811a9bc8b869097eab7c
Pieter Verstraete <pfverstr@phoenix.elis.UGent.be>
2014-05-05 09:59:28 +0200

    Added file for math intrinsics (libdevice)

---
+++ src/math.jl
@@ -0,0 +1,16 @@
+# Support for cuda math intrinsics
+
+
+# Trigonometric
+sin(x::Float64) = Base.llvmcall(false, """%2 = call double @__nv_sin( double %0 )
+										  ret double %2""", Float64, (Float64,), x)
+
+cos(x::Float64) = Base.llvmcall(false, """%2 = call double @__nv_cos( double %0 )
+										  ret double %2""", Float64, (Float64,), x)
+
+# Rounding
+floor(x::Float64) = Base.llvmcall(false, """%2 = call double @__nv_floor( double %0 )
+											ret double %2""", Float64, (Float64,), x)
+
+floor(x::Float32) = Base.llvmcall(false, """%2 = call float @__nv_floorf( float %0 )
+											ret float %2""", Float32, (Float32,), x)
