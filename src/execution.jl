0065de43d65bac2049e436fabdf45032d7a73ebe
Tim Besard <tim.besard@gmail.com>
2014-11-13 13:25:00 +0100

    Simplify dim3 construction.

--- src/execution.jl
+++ src/execution.jl
@@ -1,49 +1,29 @@
-# CUDA Execution control
+# Execution control
 
 export
     launch, CuDim
 
 
-get_dim_x(g::Int) = g
-get_dim_x(g::(Int, Int)) = g[1]
-get_dim_x(g::(Int, Int, Int)) = g[1]
-
-get_dim_y(g::Int) = 1
-get_dim_y(g::(Int, Int)) = g[2]
-get_dim_y(g::(Int, Int, Int)) = g[2]
-
-get_dim_z(g::Int) = 1
-get_dim_z(g::(Int, Int)) = 1
-get_dim_z(g::(Int, Int, Int)) = g[3]
-
 typealias CuDim Union(Int, (Int, Int), (Int, Int, Int))
-
-# Stream management
+dim3(g::Int) = (g[1], 1, 1)
+dim3(g::(Int, Int)) = (g[1], g[2], 1)
+dim3(g::(Int, Int, Int)) = g
 
 function launch(f::CuFunction, grid::CuDim, block::CuDim, args::Tuple;
                 shmem_bytes::Int=4, stream::CuStream=default_stream())
-    gx = get_dim_x(grid)
-    gy = get_dim_y(grid)
-    gz = get_dim_z(grid)
-
-    tx = get_dim_x(block)
-    ty = get_dim_y(block)
-    tz = get_dim_z(block)
+	griddim = dim3(grid)
+	blockdim = dim3(block)
 
     kernel_args = [ptrbox(arg) for arg in args]
 
     @cucall(:cuLaunchKernel, (
-        Ptr{Void},  # function
-        Cuint,  # grid dim x
-        Cuint,  # grid dim y
-        Cuint,  # grid dim z
-        Cuint,  # block dim x
-        Cuint,  # block dim y
-        Cuint,  # block dim z
-        Cuint,  # shared memory bytes, 
-        Ptr{Void}, # stream 
-        Ptr{Ptr{Void}}, # kernel parameters, 
-        Ptr{Ptr{Void}}), # extra parameters
-        f.handle, gx, gy, gz, tx, ty, tz, shmem_bytes, stream.handle, kernel_args, 0)
+        Ptr{Void},  			# function
+        Cuint, Cuint, Cuint,  	# grid dimensions (x, y, z)
+        Cuint, Cuint, Cuint,  	# block dimensions (x, y, z)
+        Cuint,  				# shared memory bytes, 
+        Ptr{Void}, 				# stream 
+        Ptr{Ptr{Void}}, 		# kernel parameters
+        Ptr{Ptr{Void}}), 		# extra parameters
+        f.handle, griddim[1], griddim[2], griddim[3], blockdim[1], blockdim[2],
+        blockdim[3], shmem_bytes, stream.handle, kernel_args, C_NULL)
 end
-
