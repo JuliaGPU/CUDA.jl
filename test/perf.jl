1a1fec0568f3c3c6079441fddd496bb5c3185264
Tim Besard <tim.besard@gmail.com>
2014-11-27 16:48:46 +0100

    Add some WIP perf tests.

--- test/perf.jl
+++ test/perf.jl
@@ -2,6 +2,8 @@ using CUDA, Base.Test
 
 include("perfutil.jl")
 
+# TODO: deduplicate with runtests? only perf if arg, no perf.jl?
+
 
 # set-up
 
@@ -11,18 +13,9 @@ ctx = CuContext(dev)
 initialize_codegen(ctx, dev)
 
 
-# measurement 1: @cuda time (purely the staged function)
-
-export dummy
-
-@target ptx dummy() = return nothing
-
-@timeit begin
-        @eval @cuda (0, 0) dummy()
-    end "cuda_lowered" "lowered @cuda"
-
-
-# measurement 2: compilation time
+#
+# @cuda
+#
 
 i = 0
 @timeit_init begin
@@ -30,6 +23,57 @@ i = 0
     end begin
         # initialization
         i += 1
-        fname = symbol("dummy_$i")
+        fname = symbol("kernel_dummy_$i")
         @eval @target ptx $fname() = return nothing
-    end "cuda_unlowered" "unlowered @cuda + compilation"
+    end "macro_full" "unlowered call to @cuda (staged function execution, kernel compilation, runtime API interactions and asynchronous kernel launch)"
+
+synchronize(ctx)
+
+
+@target ptx kernel_dummy() = return nothing
+
+@timeit begin
+        @cuda (0, 0) kernel_dummy()
+    end "macro_lowered" "lowered call to @cuda (runtime API interactions and asynchronous kernel launch)"
+
+synchronize(ctx)
+
+
+#
+# vadd
+#
+
+@target ptx function kernel_vadd(a::CuDeviceArray{Float32}, b::CuDeviceArray{Float32},
+                                 c::CuDeviceArray{Float32})
+    i = blockId_x() + (threadId_x()-1) * numBlocks_x()
+    c[i] = a[i] + b[i]
+
+    return nothing
+end
+
+siz = (3, 4)
+len = prod(siz)
+
+
+# test 1: manually managed data
+
+# TODO: new context with CTX_SCHED_BLOCKING_SYNC flag instead of synchronize(ctx)
+
+a = round(rand(Float32, siz) * 100)
+b = round(rand(Float32, siz) * 100)
+
+a_dev = CuArray(a)
+b_dev = CuArray(b)
+c_dev = CuArray(Float32, siz)
+
+@timeit begin
+        @cuda (len, 1) kernel_vadd(a_dev, b_dev, c_dev)
+        synchronize(ctx)
+    end "vadd_manual" "vector addition on manually added GPU arrays"
+
+c = to_host(c_dev)
+@test_approx_eq (a + b) c
+
+free(a_dev)
+free(b_dev)
+free(c_dev)
