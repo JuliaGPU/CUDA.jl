89627d4862fead7c0f417f64fa27cfb077fdceca
Tim Besard <tim.besard@gmail.com>
2015-12-14 15:01:08 +0100

    Remove some crap from the old diffing.

---
+++ test/bugs/undef_func.jl
@@ -0,0 +1,19 @@
+using CUDA
+
+@target ptx function kernel_crash(a::CuDeviceArray, b::CuDeviceArray)
+	b[1] = length(a)
+    return nothing
+end
+
+dev = CuDevice(0)
+ctx = CuContext(dev)
+cgctx = CuCodegenContext(ctx, dev)
+
+a = [1, 2]
+b = [0]
+@cuda (1, 1) kernel_crash(CuIn(a), CuOut(b))
+
+@assert length(a) == b[1]
+
+destroy(ctx)
+destroy(cgctx)
