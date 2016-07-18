51262cf0c7b9aa20891879eee72dea6b5cebdadb
Tim Besard <tim.besard@gmail.com>
2016-07-18 18:39:23 +0200

    Implement preliminary bounds checking and limited views.

--- src/array.jl
+++ src/array.jl
@@ -3,7 +3,7 @@
 import Base: length, size, getindex, setindex!, convert
 
 export
-    CuDeviceArray
+    CuDeviceArray, CuBoundsError
 
 
 immutable CuDeviceArray{T,N} <: AbstractArray{T,N}
@@ -37,3 +37,19 @@ size(g::CuDeviceArray) = g.shape
     Base.pointerref(a.ptr.inner, Base.to_index(i0), 8)::T
 @target ptx setindex!{T}(a::CuDeviceArray{T}, x::T, i0::Real) =
     Base.pointerset(a.ptr.inner, convert(T, x)::T, Base.to_index(i0), 8)
+
+
+# TODO: remove this hack as soon as immutables with heap references (such as BoundsError)
+#       can be stack-allocated
+import Base: throw_boundserror
+immutable CuBoundsError <: Exception end
+@target ptx throw_boundserror{T,N}(A::CuDeviceArray{T,N}, I) = (Base.@_noinline_meta; throw(CuBoundsError()))
+
+# TODO: same for SubArray, although it might be too complex to every be non-allocating
+import Base: unsafe_view, ViewIndex
+function unsafe_view{T}(A::CuDeviceArray{T,1}, I::Vararg{ViewIndex,1})
+    Base.@_inline_meta
+    ptr = DevicePtr{T}(A.ptr.inner + I[1].start*sizeof(T), true)
+    len = I[1].stop - I[1].start
+    CuDeviceArray{T,1}(T, len, ptr)
+end
