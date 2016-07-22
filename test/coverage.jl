3d9d5debac89282e840a1f8b2457774ce2b74823
Tim Besard <tim.besard@gmail.com>
2016-07-22 17:09:52 +0200

    Re-add lcov coverage for testing coverage of unpublished code.

--- test/coverage.jl
+++ test/coverage.jl
@@ -1,18 +1,37 @@
 #!/usr/bin/env julia
 
-using Coverage
+codecov = haskey(ENV, "USE_CODECOV")
+lcov    = haskey(ENV, "USE_LCOV")
+if !codecov && !lcov
+    codecov = true
+end
 
-ENV["REPO_TOKEN"] = "d8b27424-577e-4851-a490-9e5459b5d787"
+using Coverage
 
 dir, _ = splitdir(Base.source_path())
 root = "$dir/../"
 cd(root)
+
+run(`julia --compilecache=no --inline=no --code-coverage=user "test/runtests.jl"`)
+
+ENV["DEBUG"] = 1
+run(`julia --compilecache=no --inline=no --code-coverage=user "test/runtests.jl"`)
 
 ENV["TRACE"] = 1
 run(`julia --compilecache=no --inline=no --code-coverage=user "test/runtests.jl"`)
 
 coverage = process_folder()
 
-Codecov.submit_token(coverage)
+if codecov
+    ENV["REPO_TOKEN"] = "d8b27424-577e-4851-a490-9e5459b5d787"
+    Codecov.submit_token(coverage)
+end
+
+if lcov
+    isdir("test/coverage") || mkdir("test/coverage")
+    LCOV.writefile("test/coverage/lcov.info", coverage)
+    run(`genhtml test/coverage/lcov.info -o test/coverage/html`)
+    println("Done! Open your browser at file://$dir/coverage/html/index.html")
+end
 
 clean_folder(".")
