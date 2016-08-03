9aefcc4638d3af3d27e1f6c9f50591f0eb861c50
Tim Besard <tim.besard@gmail.com>
2016-08-03 16:04:30 -0400

    Remove unnecessary convert for unsafe_string.

--- src/devices.jl
+++ src/devices.jl
@@ -38,7 +38,7 @@ function name(dev::CuDevice)
     @apicall(:cuDeviceGetName, (Ptr{Cchar}, Cint, CuDevice_t),
                                buf, buflen, dev)
     buf[end] = 0
-    return unsafe_string(Ptr{UInt8}(pointer(buf)))
+    return unsafe_string(pointer(buf))
 end
 
 "Get the amount of GPU memory (in bytes) of a CUDA device"
