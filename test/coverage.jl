06b3595fa792d21cd00dc4ada3188e04a7947758
Tim Besard <tim.besard@gmail.com>
2016-09-07 20:28:43 +0200

    Improve coverage script, use system Julia for submitting.

--- test/coverage.jl
+++ test/coverage.jl
@@ -1,37 +1,69 @@
 #!/usr/bin/env julia
 
-codecov = haskey(ENV, "USE_CODECOV")
-lcov    = haskey(ENV, "USE_LCOV")
-if !codecov && !lcov
-    codecov = true
+# This script will gather coverage information with the currently active julia,
+# and will process it with Coverage.jl using the system-wide Julia at /usr/bin
+# (this for compatibility reasons, but might break if .cov parsing changes).
+
+cmd = Base.julia_cmd()
+
+system_cmd = "/usr/bin/julia"
+system_env = copy(ENV)
+for (k,v) in system_env
+    if startswith(k, "JULIA_")
+        delete!(system_env, k)
+    end
 end
 
-using Coverage
+# make sure Coverage.jl is available on the clean Julia
+run(setenv(`$system_cmd -E "using Coverage"`, system_env))
+
+
+#
+# Measure
+#
 
 dir = dirname(@__FILE__)
 root = "$dir/../"
 cd(root)
 
-run(`julia --compilecache=no --inline=no --code-coverage=user "test/runtests.jl"`)
+run(`$cmd --compilecache=no --inline=no --code-coverage=user "test/runtests.jl"`)
 
 ENV["DEBUG"] = 1
-run(`julia --compilecache=no --inline=no --code-coverage=user "test/runtests.jl"`)
+run(`$cmd --compilecache=no --inline=no --code-coverage=user "test/runtests.jl"`)
 
 ENV["TRACE"] = 1
-run(`julia --compilecache=no --inline=no --code-coverage=user "test/runtests.jl"`)
+run(`$cmd --compilecache=no --inline=no --code-coverage=user "test/runtests.jl"`)
 
-coverage = process_folder()
 
-if codecov
-    ENV["REPO_TOKEN"] = "d8b27424-577e-4851-a490-9e5459b5d787"
-    Codecov.submit_token(coverage)
+#
+# Submit (with the system Julia)
+#
+
+let code = """
+    using Coverage
+
+    coverage = process_folder()
+
+    codecov = haskey(ENV, "USE_CODECOV")
+    lcov    = haskey(ENV, "USE_LCOV")
+    if !codecov && !lcov
+        codecov = true
+    end
+
+    if codecov
+        ENV["REPO_TOKEN"] = "d8b27424-577e-4851-a490-9e5459b5d787"
+        Codecov.submit_token(coverage)
+    end
+
+    if lcov
+        isdir("test/coverage") || mkdir("test/coverage")
+        LCOV.writefile("test/coverage/lcov.info", coverage)
+        run(`genhtml test/coverage/lcov.info -o test/coverage/html`)
+        println("Done! Open your browser at file://$dir/coverage/html/index.html")
+    end
+
+    clean_folder(".")
+    """
+
+    run(setenv(`$system_cmd -E "$code"`, system_env))
 end
-
-if lcov
-    isdir("test/coverage") || mkdir("test/coverage")
-    LCOV.writefile("test/coverage/lcov.info", coverage)
-    run(`genhtml test/coverage/lcov.info -o test/coverage/html`)
-    println("Done! Open your browser at file://$dir/coverage/html/index.html")
-end
-
-clean_folder(".")
