f93b57935225d7d67e076d143866cffa4d8b00b1
Tim Besard <tim.besard@gmail.com>
2016-10-27 17:08:50 +0200

    Also port execution test to testsets.

--- test/execution.jl
+++ test/execution.jl
@@ -1,83 +1,103 @@
-do_nothing() = return nothing
+@testset "execution" begin
 
-#
-# cufunction
-#
+############################################################################################
 
-@test_throws UndefVarError cufunction(undefined_kernel, ())
+@eval exec_dummy() = return nothing
 
-cufunction(dev, do_nothing, ())
-cufunction(dev, do_nothing, Tuple{})
+@testset "cufunction" begin
+    @test_throws UndefVarError cufunction(exec_undefined_kernel, ())
 
-# NOTE: other cases are going to be covered by tests below,
-#       as @cuda internally uses cufunction
+    cufunction(dev, exec_dummy, ())
+    cufunction(dev, exec_dummy, Tuple{})
 
-
-
-#
-# @cuda
-#
-
-@test_throws UndefVarError @cuda dev (1,1) undefined_kernel()
-
-# kernel dims
-@cuda dev (1,1) do_nothing()
-@test_throws ArgumentError @cuda dev (0,0) do_nothing()
-
-# shared memory
-@cuda dev (1,1,1) do_nothing()
-
-# streams
-s = CuStream()
-@cuda dev (1,1,1,s) do_nothing()
-destroy(s)
-
-# external kernel
-module KernelModule
-    export do_more_nothing
-    do_more_nothing() = return nothing
+    # NOTE: other cases are going to be covered by tests below,
+    #       as @cuda internally uses cufunction
 end
-@cuda dev (1,1) KernelModule.do_more_nothing()
-@eval begin
-    using KernelModule
-    @cuda dev (1,1) do_more_nothing()
+
+############################################################################################
+
+
+@testset "@cuda" begin
+
+@test_throws UndefVarError @cuda dev (1,1) exec_undefined_kernel()
+
+
+@testset "dimensions" begin
+    @cuda dev (1,1) exec_dummy()
+    @test_throws ArgumentError @cuda dev (0,0) exec_dummy()
 end
 
 
-## return values
-
-retint() = return 1
-@test_throws ErrorException @cuda dev (1,1) retint()
-
-function call_retint()
-    retint()
-    return nothing
+@testset "shared memory" begin
+    @cuda dev (1,1,1) exec_dummy()
 end
-@cuda dev (1,1) call_retint()
 
 
-## argument passing
+@testset "streams" begin
+    s = CuStream()
+    @cuda dev (1,1,1,s) exec_dummy()
+    destroy(s)
+end
+
+
+@testset "external kernels" begin
+    @eval module KernelModule
+        export exec_external_dummy
+        exec_external_dummy() = return nothing
+    end
+    @cuda dev (1,1) KernelModule.exec_external_dummy()
+    @eval begin
+        using KernelModule
+        @cuda dev (1,1) exec_external_dummy()
+    end
+end
+
+
+@testset "return values" begin
+    @eval exec_return_int_inner() = return 1
+    @test_throws ErrorException @cuda dev (1,1) exec_return_int_inner()
+
+    @eval function exec_return_int_outer()
+        exec_return_int_inner()
+        return nothing
+    end
+    @cuda dev (1,1) exec_return_int_outer()
+end
+
+
+@testset "calling device function" begin
+    @eval @noinline exec_devfun_child() = return nothing
+    @eval exec_devfun_parent() = exec_devfun_child()
+
+    @cuda dev (1,1) exec_devfun_parent()
+end
+
+end
+
+
+############################################################################################
+
+@testset "argument passing" begin
 
 dims = (16, 16)
 len = prod(dims)
 
-function ptr_copy(input, output)
-    i = (blockIdx().x-1) * blockDim().x + threadIdx().x
+@testset "manually allocated" begin
+    @eval function exec_pass_ptr(input, output)
+        i = (blockIdx().x-1) * blockDim().x + threadIdx().x
 
-    val = unsafe_load(input, i)
-    unsafe_store!(output, val, i)
+        val = unsafe_load(input, i)
+        unsafe_store!(output, val, i)
 
-    return nothing
-end
+        return nothing
+    end
 
-# manually allocated
-let
     input = round.(rand(Float32, dims) * 100)
 
     input_dev = CuArray(input)
     output_dev = CuArray(Float32, dims)
 
-    @cuda dev (1,len) ptr_copy(input_dev.ptr, output_dev.ptr)
+    @cuda dev (1,len) exec_pass_ptr(input_dev.ptr, output_dev.ptr)
     output = Array(output_dev)
     @test input ≈ output
 
@@ -85,61 +105,65 @@ let
     free(output_dev)
 end
 
-# scalar through single-value array
-function ptr_lastvalue(a, x)
-    i = (blockIdx().x-1) * blockDim().x + threadIdx().x
-    max = gridDim().x * blockDim().x
-    if i == max
-        val = unsafe_load(a, i)
-        unsafe_store!(x, val)
+
+@testset "scalar through single-value array" begin
+    @eval function exec_pass_scalar(a, x)
+        i = (blockIdx().x-1) * blockDim().x + threadIdx().x
+        max = gridDim().x * blockDim().x
+        if i == max
+            val = unsafe_load(a, i)
+            unsafe_store!(x, val)
+        end
+
+        return nothing
     end
 
-    return nothing
-end
-let
     arr = round.(rand(Float32, dims) * 100)
     val = Float32[0]
 
     arr_dev = CuArray(arr)
     val_dev = CuArray(val)
 
-    @cuda dev (1,len) ptr_lastvalue(arr_dev.ptr, val_dev.ptr)
+    @cuda dev (1,len) exec_pass_scalar(arr_dev.ptr, val_dev.ptr)
     @test arr[dims...] ≈ Array(val_dev)[1]
 
     free(val_dev)
     free(arr_dev)
 end
 
-# same, but using a device function
-@noinline function ptr_lastvalue_devfun(a, x)
-    i = (blockIdx().x-1) * blockDim().x + threadIdx().x
-    max = gridDim().x * blockDim().x
-    if i == max
-        val = lastvalue_devfun(a, i)
-        unsafe_store!(x, val)
+
+@testset "scalar through single-value array, using device function" begin
+    @eval @noinline function exec_pass_scalar_devfun(a, x)
+        i = (blockIdx().x-1) * blockDim().x + threadIdx().x
+        max = gridDim().x * blockDim().x
+        if i == max
+            val = exec_pass_scalar_devfun_child(a, i)
+            unsafe_store!(x, val)
+        end
+
+        return nothing
+    end
+    @eval function exec_pass_scalar_devfun_child(a, i)
+        return unsafe_load(a, i)
     end
 
-    return nothing
-end
-function lastvalue_devfun(a, i)
-    return unsafe_load(a, i)
-end
-let
     arr = round.(rand(Float32, dims) * 100)
     val = Float32[0]
 
     arr_dev = CuArray(arr)
     val_dev = CuArray(val)
 
-    @cuda dev (1,len) ptr_lastvalue_devfun(arr_dev.ptr, val_dev.ptr)
+    @cuda dev (1,len) exec_pass_scalar_devfun(arr_dev.ptr, val_dev.ptr)
     @test arr[dims...] ≈ Array(val_dev)[1]
 
     free(val_dev)
     free(arr_dev)
 end
 
-# bug: ghost type function parameters are elided by the compiler
-let
+
+@testset "ghost function parameters" begin
+    # bug: ghost type function parameters are elided by the compiler
+
     len = 60
     a = rand(Float32, len)
     b = rand(Float32, len)
@@ -148,15 +172,15 @@ let
     d_b = CuArray(b)
     d_c = CuArray(Float32, len)
 
-    immutable Ghost end
+    @eval immutable ExecGhost end
 
-    function map_inner(ghost, a, b, c)
+    @eval function exec_pass_ghost(ghost, a, b, c)
         i = (blockIdx().x-1) * blockDim().x + threadIdx().x
         unsafe_store!(c, unsafe_load(a,i)+unsafe_load(b,i), i)
 
         return nothing
     end
-    @cuda dev (1,len) map_inner(Ghost(), d_a.ptr, d_b.ptr, d_c.ptr)
+    @cuda dev (1,len) exec_pass_ghost(ExecGhost(), d_a.ptr, d_b.ptr, d_c.ptr)
 
     c = Array(d_c)
     @test a+b == c
@@ -166,9 +190,11 @@ let
     free(d_a)
 end
 
-# issue #7: tuples not passed by pointer
-let
-    function kernel7(keeps, out)
+
+@testset "tuples" begin
+    # issue #7: tuples not passed by pointer
+
+    @eval function exec_pass_tuples(keeps, out)
         if keeps[1]
             unsafe_store!(out, 1)
         else
@@ -180,15 +206,17 @@ let
     keeps = (true,)
     d_out = CuArray(Int, 1)
 
-    @cuda dev (1,1) kernel7(keeps, d_out.ptr)
+    @cuda dev (1,1) exec_pass_tuples(keeps, d_out.ptr)
     @test Array(d_out) == [1]
 
     free(d_out)
 end
 
-# issue #15: immutables not passed by pointer
-let
-    function kernel15(A, b)
+
+@testset "immutables" begin
+    # issue #15: immutables not passed by pointer
+
+    @eval function exec_pass_immutables(A, b)
         unsafe_store!(A, imag(b))
         nothing
     end
@@ -196,16 +224,14 @@ let
     A = CuArray(zeros(Float32, (1,)));
     x = Complex64(2,2)
 
-    @cuda dev (1, 1) kernel15(A.ptr, x)
+    @cuda dev (1, 1) exec_pass_immutables(A.ptr, x)
     @test Array(A) == Float32[imag(x)]
 
     free(A)
 end
 
-# issue: calling device function
-let
-    @noinline child() = return nothing
-    parent() = child()
+end
 
-    @cuda dev (1,1) parent()
+############################################################################################
+
 end
