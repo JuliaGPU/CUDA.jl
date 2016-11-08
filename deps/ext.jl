68b08128dc5c0a46a521adcfdfc723e12e3dd59f
Tim Besard <tim.besard@gmail.com>
2016-11-08 09:30:03 -0500

    Don't have build artifacts in the repo.

--- deps/ext.jl
+++
@@ -1,5 +0,0 @@
-# Toolchain properties
-const llvm_version = v"4.0.0-svn"
-const cuda_version = v"8.0.0"
-
-const toolchain_caps = VersionNumber[v"3.7.0",v"6.2.0",v"6.0.0",v"5.2.0",v"3.5.0",v"5.0.0",v"3.0.0",v"5.3.0",v"6.1.0",v"2.0.0",v"2.1.0",v"3.2.0"]
