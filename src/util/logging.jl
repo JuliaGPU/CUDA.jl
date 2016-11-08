3ffbde23c86742574997777cbb22b013b4201563
Tim Besard <tim.besard@gmail.com>
2016-11-08 12:04:45 -0500

    Logging: drop to raw I/O after atexit.

--- src/util/logging.jl
+++ src/util/logging.jl
@@ -1,7 +1,17 @@
-if false
-# debug version of `print_with_color` to print after I/O streams have been finalized
-Base.print_with_color(color::Union{Int, Symbol}, io::IO, msg::AbstractString...) =
+# I/O without libuv, for use after STDOUT is finalized
+raw_print(msg::AbstractString...) =
     ccall(:write, Cssize_t, (Cint, Cstring, Csize_t), 1, join(msg), length(join(msg)))
+raw_println(msg::AbstractString...) = raw_print(msg..., "\n")
+
+# safe version of `Base.print_with_color`, switching to raw I/O before finalizers are run
+# (see `atexit` in `__init_logging__`)
+const after_exit = Ref{Bool}(false)
+function safe_print_with_color(color::Union{Int, Symbol}, io::IO, msg::AbstractString...)
+    if after_exit[]
+        raw_print(msg...)
+    else
+        print_with_color(color, io, msg...)
+    end
 end
 
 const TRACE = haskey(ENV, "TRACE")
@@ -9,7 +19,7 @@ const TRACE = haskey(ENV, "TRACE")
 is set."
 @inline function trace(io::IO, msg...; prefix="TRACE: ", line=true)
     @static if TRACE
-        Base.print_with_color(:cyan, io, prefix, chomp(string(msg...)), line?"\n":"")
+        safe_print_with_color(:cyan, io, prefix, chomp(string(msg...)), line?"\n":"")
     end
 end
 @inline trace(msg...; kwargs...) = trace(STDERR, msg...; kwargs...)
@@ -19,7 +29,7 @@ const DEBUG = TRACE || haskey(ENV, "DEBUG")
 variable is set."
 @inline function debug(io::IO, msg...; prefix="DEBUG: ", line=true)
     @static if DEBUG
-        Base.print_with_color(:green, io, prefix, chomp(string(msg...)), line?"\n":"")
+        safe_print_with_color(:green, io, prefix, chomp(string(msg...)), line?"\n":"")
     end
 end
 @inline debug(msg...; kwargs...) = debug(STDERR, msg...; kwargs...)
@@ -72,4 +82,9 @@ function __init_logging__()
         debug("CUDAdrv.jl is running in debug mode, this will generate additional output")
         debug("Run with TRACE=1 to enable even more output")
     end
+
+    atexit(()->begin
+        debug("Dropping down to post-finalizer I/O")
+        after_exit[]=true
+    end)
 end
