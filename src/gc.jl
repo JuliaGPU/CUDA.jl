50e48fb0d52dd0b284f2de8a27ab23b32259e032
Tim Besard <tim.besard@gmail.com>
2016-11-17 19:35:05 +0100

    Properly document the GC finalization stuff.

--- src/gc.jl
+++ src/gc.jl
@@ -1,6 +1,4 @@
-# finalizers are run out-of-order disregarding field references between objects (see
-# JuliaLang/julia#3067), so we manually need to keep instances alive outside of the object
-# fields in order to prevent objects getting collected before their owners are
+# auxiliary infrastructure to cope with out-of-order finalization (JuliaLang/julia#3067)
 
 # For example, we have:
 #   type CuContext
@@ -14,15 +12,25 @@
 #   end
 #   finalize(arr::CuArray) = ccall(:cuMemFree, arr.ptr)
 #
-#  As finalizers are not guaranteed to run in order, the context might be finalized before
-#  the array, but doing so breaks the call to :cuMemFree as destroying the underlying
-#  context invalidates all resources.
+#   As finalizers are not guaranteed to run in order, the context might be finalized before
+#   the array, but doing so breaks the call to :cuMemFree as destroying the underlying
+#   context invalidates all resources.
+#
+# The functions in this file help in construction additional dependency chains, blocking
+# finalizers to be run before all child objects have been collected. Note that during
+# process exit, even those additional dependency chains are violated (see `can_finalize`).
 
 # NOTE: we use `pointer_from_objref` instead of `WeakRef` (either of both are required
 #       not to keep the owner artificially alive) because different objects can have
 #       identical hashes
 const finalizer_blocks = Dict{Ptr{Void},Any}() # owner => target to make block/unblock cheap
 
+"""
+Block finalization of all objects in `targets`, and register `owner` as the owning object.
+
+This function is meant to be called in the constructor of a child object, which needs to be
+finalized _before_ any parent object is finalized.
+"""
 function block_finalizer(owner::ANY, targets::ANY...)
     owner_id = Base.pointer_from_objref(owner)
     trace("Blocking finalization of ",
@@ -32,6 +40,11 @@ function block_finalizer(owner::ANY, targets::ANY...)
     finalizer_blocks[owner_id] = targets
 end
 
+"""
+Unblock finalization of all objects in `targets`, checking whether they were owned by `owner`.
+
+This function is meant to be called in the finalized of a child object.
+"""
 function unblock_finalizer(owner::ANY, targets::ANY...)
     owner_id = Base.pointer_from_objref(owner)
     trace("Unblocking finalization of ",
@@ -49,4 +62,11 @@ function unblock_finalizer(owner::ANY, targets::ANY...)
     delete!(finalizer_blocks, owner_id)
 end
 
+"""
+Check whether an object can be finalized, ie. whether it has not been blocked by any other object.
+
+This function is meant to be called in the finalized of the parent object, as during process
+exit Julia's garbage collector ignores the dependency chains set-up by `block_finalizer` and
+`unblock_finalizer`.
+"""
 can_finalize(target::ANY) = isempty(filter((owner,targets) -> target in targets, finalizer_blocks))
