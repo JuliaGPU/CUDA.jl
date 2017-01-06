58d84bd97217f611eee03ff3a3b8927413fc4151
Tim Besard <tim.besard@gmail.com>
2017-01-06 16:35:15 +0100

    Revert messed up example.

--- examples/vadd.jl
+++ examples/vadd.jl
@@ -1,16 +1,9 @@
 using CUDAdrv, CUDAnative
-
-@noinline function child(i)
-    if i < 10
-        return i*i
-    else
-        return (i-1)*(i+1)
-    end
-end
+using Base.Test
 
 function kernel_vadd(a, b, c)
     i = (blockIdx().x-1) * blockDim().x + threadIdx().x
-    c[i] = a[i] + child(b[i])
+    c[i] = a[i] + b[i]
 
     return nothing
 end
@@ -29,9 +22,6 @@ d_c = similar(d_a)
 len = prod(dims)
 @cuda (1,len) kernel_vadd(d_a, d_b, d_c)
 c = Array(d_c)
-
-CUDAnative.code_sass(kernel_vadd, Tuple{CuDeviceArray{Float32,2}, CuDeviceArray{Float32,2}, CuDeviceArray{Float32,2}})
-CUDAnative.code_ptx(child, Tuple{Float32})
-CUDAnative.code_sass(child, Tuple{Float32})
+@test a+b ≈ c
 
 destroy(ctx)
