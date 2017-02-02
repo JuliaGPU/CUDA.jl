4bf38e7a0349ab85e8716815eb31a1dba1ba7a48
Tim Besard <tim.besard@gmail.com>
2017-02-02 14:41:24 +0100

    Remove unused util function.

--- src/util.jl
+++ src/util.jl
@@ -2,15 +2,6 @@
 
 const dumpdir = Ref{String}()
 
-# Generate a temporary file with specific suffix
-function mkstemps(suffix::AbstractString)
-    b = joinpath(tempdir(), "tmpXXXXXX$suffix")
-    # NOTE: mkstemps modifies b, which should be a NULL-terminated string
-    p = ccall(:mkstemps, Int32, (Cstring, Cint), b, length(suffix))
-    systemerror(:mktemp, p == -1)
-    return (b, fdio(p, true))
-end
-
 function __init_util__()
     if TRACE
         # When in trace mode, we'll be dumping certain build artifacts to disk
