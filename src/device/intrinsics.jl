7adc6c42e757d7e5289c1587f8bf3d8e8d2735a1
Tim Besard <tim.besard@gmail.com>
2017-02-02 11:11:27 +0100

    Fix wording of at-cuprintf doc.
    Ref #35.

--- src/device/intrinsics.jl
+++ src/device/intrinsics.jl
@@ -149,7 +149,7 @@ Note that this is not a fully C-compliant `printf` implementation; see the CUDA
 documentation for supported options and inputs.
 
 Also beware that it is an untyped, and unforgiving `printf` implementation. Type widths need
-to match, eg. printing a Julia integer requires the `%ld` formatting string.
+to match, eg. printing a 64-bit Julia integer requires the `%ld` formatting string.
 
 """
 macro cuprintf(fmt::String, args...)
