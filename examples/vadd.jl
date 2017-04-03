9d5d4f84ae269149e0b84e49c8d3f7afdc65026f
Tim Besard <tim.besard@gmail.com>
2017-04-03 13:04:59 +0200

    Revert messed up example.

--- examples/vadd.jl
+++ examples/vadd.jl
@@ -1,12 +1,9 @@
 using CUDAdrv, CUDAnative
 using Base.Test
 
-const BLOCK_SIZE = 16
-
-function kernel_vadd(c)
-    shadow = @cuStaticSharedMem(Float32, (BLOCK_SIZE,))
-    tx = threadIdx().x
-    c[tx] = shadow[tx]
+function kernel_vadd(a, b, c)
+    i = (blockIdx().x-1) * blockDim().x + threadIdx().x
+    c[i] = a[i] + b[i]
 
     return nothing
 end
@@ -19,13 +16,12 @@ a = round.(rand(Float32, dims) * 100)
 b = round.(rand(Float32, dims) * 100)
 
 d_a = CuArray(a)
-matrix = CuArray{Float32}((BLOCK_SIZE, BLOCK_SIZE))
 d_b = CuArray(b)
 d_c = similar(d_a)
 
 len = prod(dims)
-CUDAnative.@code_llvm @cuda (1,len) kernel_vadd(d_c)
-# c = Array(d_c)
-# @test a+b ≈ c
+@cuda (1,len) kernel_vadd(d_a, d_b, d_c)
+c = Array(d_c)
+@test a+b ≈ c
 
 destroy(ctx)
