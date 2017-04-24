be1ede82ca211d190a0533a2681442d7e828afd6
Tim Besard <tim.besard@gmail.com>
2017-04-24 11:14:11 +0200

    Uniformize build script with other CUDA packages.

--- deps/build.jl
+++ deps/build.jl
@@ -1,58 +1,85 @@
-# Discover the CUDA and LLVM toolchains, and figure out which devices we can compile for
+using CUDAdrv
+using LLVM
 
-const ext = joinpath(@__DIR__, "ext.jl")
-try
-    using CUDAdrv
-    using LLVM
+# TODO: import from CUDAapi.jl
+import CUDAdrv: debug
 
-    # non-exported utility functions
-    import CUDAdrv: debug
 
-    # helper methods for querying version DBs
-    # (tool::VersionNumber => devices::Vector{VersionNumber})
-    search(db, predicate) =
-        Set(Base.Iterators.flatten(valvec for (key,valvec) in db if predicate(key)))
+## auxiliary routines
 
-    # figure out which targets are supported by LLVM
+# helper methods for querying version DBs
+# (tool::VersionNumber => devices::Vector{VersionNumber})
+search(db, predicate) =
+    Set(Base.Iterators.flatten(valvec for (key,valvec) in db if predicate(key)))
+
+# database of LLVM versions and the supported devices
+const llvm_db = [
+    v"3.2" => [v"2.0", v"2.1", v"3.0", v"3.5"],
+    v"3.5" => [v"5.0"],
+    v"3.7" => [v"3.2", v"3.7", v"5.2", v"5.3"],
+    v"3.9" => [v"6.0", v"6.1", v"6.2"]
+]
+
+# check support for the LLVM version
+function check_llvm()
+    llvm_version = LLVM.version()
+    info("Using LLVM $llvm_version")
+
     InitializeAllTargets()
     "nvptx" in LLVM.name.(collect(targets())) ||
         error("Your LLVM does not support the NVPTX back-end. Fix this, and rebuild LLVM.jl and CUDAnative.jl.")
 
-    # figure out which devices are supported by LLVM
-    llvm_version = LLVM.version()
-    info("Using LLVM $llvm_version")
-    const llvm_db = [
-        v"3.2" => [v"2.0", v"2.1", v"3.0", v"3.5"],
-        v"3.5" => [v"5.0"],
-        v"3.7" => [v"3.2", v"3.7", v"5.2", v"5.3"],
-        v"3.9" => [v"6.0", v"6.1", v"6.2"]
-    ]
-    llvm_version = LLVM.version()
     llvm_support = search(llvm_db, ver -> ver <= llvm_version)
     isempty(llvm_support) && error("LLVM $llvm_version does not support any compatible device")
 
-    # figure out which devices are supported by CUDA
+    return llvm_version, llvm_support
+end
+
+# database of CUDA versions and the supported devices
+const cuda_db = [
+    v"4.0" => [v"2.0", v"2.1"],
+    v"4.2" => [v"3.0"],
+    v"5.0" => [v"3.5"],
+    v"6.0" => [v"3.2", v"5.0"],
+    v"6.5" => [v"3.7"],
+    v"7.0" => [v"5.2"],
+    v"7.5" => [v"5.3"],
+    v"8.0" => [v"6.0", v"6.1", v"6.2"]
+]
+
+# check support for the CUDA version
+function check_cuda()
     cuda_version = CUDAdrv.version()
     info("Using CUDA $cuda_version")
-    const cuda_db = [
-        v"4.0" => [v"2.0", v"2.1"],
-        v"4.2" => [v"3.0"],
-        v"5.0" => [v"3.5"],
-        v"6.0" => [v"3.2", v"5.0"],
-        v"6.5" => [v"3.7"],
-        v"7.0" => [v"5.2"],
-        v"7.5" => [v"5.3"],
-        v"8.0" => [v"6.0", v"6.1", v"6.2"]
-    ]
+
     cuda_support = search(cuda_db, ver -> ver <= cuda_version)
     isempty(cuda_support) && error("CUDA $cuda_version does not support any compatible device")
 
-    # check if Julia's LLVM version matches ours
+    return cuda_version, cuda_support
+end
+
+# check support for the Julia version
+function check_julia(llvm_version)
     julia_llvm_version = VersionNumber(Base.libllvm_version)
     info("Using Julia's LLVM $julia_llvm_version")
+
     if julia_llvm_version != llvm_version
         error("LLVM $llvm_version incompatible with Julia's LLVM $julia_llvm_version")
     end
+
+    return julia_llvm_version
+end
+
+
+## main
+
+const ext = joinpath(@__DIR__, "ext.jl")
+
+function main()
+    # check version support
+    llvm_version, llvm_support = check_llvm()
+    cuda_version, cuda_support = check_cuda()
+    julia_llvm_version = check_julia(llvm_version)
 
     # check if we need to rebuild
     if isfile(ext)
@@ -82,6 +109,11 @@ try
             const supported_capabilities = $supported_capabilities
             """)
     end
+    nothing
+end
+
+try
+    main()
 catch ex
     # if anything goes wrong, wipe the existing ext.jl to prevent the package from loading
     rm(ext; force=true)
