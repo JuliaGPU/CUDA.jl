cee49dab1f11aa22f8476c2c457a8242dbc2d73c
Tim Besard <tim.besard@gmail.com>
2017-05-16 15:58:12 +0200

    Add example on pairwise vector processing.

---
+++ examples/pairwise.jl
@@ -0,0 +1,116 @@
+# calculate pairwise distance between every point in a vector
+
+using CUDAnative, CUDAdrv
+
+
+function haversine_cpu(lat1::Float32, lon1::Float32, lat2::Float32, lon2::Float32, radius::Float32)
+    c1 = cospi(lat1 / 180.0f0)
+    c2 = cospi(lat2 / 180.0f0)
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    d1 = sinpi(dlat / 360.0f0)
+    d2 = sinpi(dlon / 360.0f0)
+    t = d2 * d2 * c1 * c2
+    a = d1 * d1 + t
+    c = 2.0f0 * asin(min(1.0f0, sqrt(a)))
+    return radius * c
+end
+
+function pairwise_dist_cpu(lat::Vector{Float32}, lon::Vector{Float32})
+    # allocate
+    n = length(lat)
+    rowresult = Array{Float32}(n, n)
+    
+    # brute force fill in each cell
+    for i in 1:n, j in 1:n
+        @inbounds rowresult[i, j] = haversine_cpu(lat[i], lon[i], lat[j], lon[j] , 6372.8f0)
+    end
+    
+    return rowresult    
+end
+
+# from https://devblogs.nvidia.com/parallelforall/fast-great-circle-distance-calculation-cuda-c/
+function haversine_gpu(lat1::Float32, lon1::Float32, lat2::Float32, lon2::Float32, radius::Float32)
+    # XXX: need to prefix math intrinsics with CUDAnative
+    c1 = CUDAnative.cospi(lat1 / 180.0f0)
+    c2 = CUDAnative.cospi(lat2 / 180.0f0)
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    d1 = CUDAnative.sinpi(dlat / 360.0f0)
+    d2 = CUDAnative.sinpi(dlon / 360.0f0)
+    t = d2 * d2 * c1 * c2
+    a = d1 * d1 + t
+    c = 2.0f0 * CUDAnative.asin(CUDAnative.min(1.0f0, CUDAnative.sqrt(a)))
+    return radius * c
+end
+
+# _very_ naive/inefficient/ad-hoc pairwise kernel
+# can be made much more efficient and generic
+function pairwise_dist_kernel(lat_ptr::Ptr{Float32}, lon_ptr::Ptr{Float32},
+                              rowresult_ptr::Ptr{Float32}, n)
+    lat = CuDeviceArray(n, lat_ptr)
+    lon = CuDeviceArray(n, lon_ptr)
+    rowresult = CuDeviceArray((n, n), rowresult_ptr)
+
+    i = (blockIdx().x-1) * blockDim().x + threadIdx().x
+    j = (blockIdx().y-1) * blockDim().y + threadIdx().y
+
+    if i <= n && j <= n
+        # store to shared memory
+        shmem = @cuDynamicSharedMem(Float32, 2*blockDim().x + 2*2*blockDim().x)
+        if threadIdx().y == 1
+            shmem[threadIdx().x] = lat[i]
+            shmem[blockDim().x + threadIdx().x] = lon[i]
+        end
+        if threadIdx().x == 1
+            shmem[2*blockDim().x + threadIdx().y] = lat[j]
+            shmem[2*blockDim().x + blockDim().y + threadIdx().y] = lon[j]
+        end
+        sync_threads()
+
+        # load from shared memory
+        lat_i = shmem[threadIdx().x]
+        lon_i = shmem[blockDim().x + threadIdx().x]
+        lat_j = shmem[2*blockDim().x + threadIdx().y]
+        lon_j = shmem[2*blockDim().x + blockDim().y + threadIdx().y]
+
+        @inbounds rowresult[i, j] = haversine_gpu(lat_i, lon_i, lat_j, lon_j, 6372.8f0)
+    end
+end
+
+function pairwise_dist_gpu(lat::Vector{Float32}, lon::Vector{Float32})
+    # upload
+    lat_gpu = CuArray(lat)
+    lon_gpu = CuArray(lon)
+
+    # allocate
+    n = length(lat)
+    rowresult_gpu = CuArray{Float32}(n, n)
+
+    # calculate launch configuration
+    threads = (32, 32)  # XXX: hardcoded
+    blocks = ceil.(Int, n ./ threads)
+
+    # calculate size of dynamic shared memory
+    shmem = 2 * sum(threads) * sizeof(Float32)
+
+    # XXX: pass arrays as pointer to avoid synchronization / global ptr loads
+    @cuda (blocks, threads, shmem) pairwise_dist_kernel(pointer(lat_gpu), pointer(lon_gpu),
+                                                        pointer(rowresult_gpu), n)
+
+    return Array(rowresult_gpu)
+end
+
+
+const dev = CuDevice(0)
+const ctx = CuContext(dev)
+
+# generate reasonable data
+const n = 10000
+const lat = rand(Float32, n) .* 45
+const lon = rand(Float32, n) .* -120
+
+using Base.Test
+@test pairwise_dist_cpu(lat, lon) ≈ pairwise_dist_gpu(lat, lon)
+
+destroy(ctx)
