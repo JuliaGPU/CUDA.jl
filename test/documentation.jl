c8c66ef3a00b4d22b91380b57dec6effcc0c7b37
Tim Besard <tim.besard@gmail.com>
2017-06-08 15:12:22 +0200

    Pass Julia cmdline opts when running examples.

--- test/documentation.jl
+++ test/documentation.jl
@@ -3,7 +3,8 @@
 out = Pipe()
 result = cd(joinpath(dirname(@__DIR__), "docs")) do
     withenv("TEST"=>true) do
-        success(pipeline(`$(Base.julia_cmd()) --color=$(Base.have_color?"yes":"no") make.jl`; stdout=out, stderr=out))
+        cmd = julia_cmd(`make.jl`)
+        success(pipeline(cmd; stdout=out, stderr=out))
     end
 end
 close(out.in)
