2f13431842cd725b1261130e337a24fe12f13795
Tim Besard <tim.besard@gmail.com>
2017-06-24 01:18:27 +0200

    CuPrimaryContext can be immutable.

--- src/context/primary.jl
+++ src/context/primary.jl
@@ -13,7 +13,7 @@ Create a primary CUDA context for a given device.
 Each primary context is unique per device and is shared with CUDA runtime API. It is meant
 for interoperability with (applications using) the runtime API.
 """
-type CuPrimaryContext
+immutable CuPrimaryContext
     dev::CuDevice
 
     function CuPrimaryContext(dev::CuDevice)
