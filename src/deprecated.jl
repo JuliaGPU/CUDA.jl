8ad446335847fe73babd8bdc35b49723388ce32e
Tim Besard <tim.besard@gmail.com>
2017-07-29 10:53:12 +0200

    Add a deprecation for DevicePtr.

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,9 +1,10 @@
 # Deprecated functionality
 
-export devcount, list_devices, destroy, reset
+import Base: @deprecate_binding
 
 @deprecate devcount() length(devices())
 @deprecate list_devices() map(println, devices())
 @deprecate destroy(ctx::CuContext) destroy!(ctx)
 @deprecate CuPrimaryContext(devnum::Int) CuPrimaryContext(CuDevice(devnum))
 @deprecate reset(pctx::CuPrimaryContext) unsafe_reset!(pctx)
+@deprecate_binding DevicePtr Ptr
