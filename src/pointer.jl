1bf273b05de835464267b429802d1c42f9b50580
Tim Besard <tim.besard@gmail.com>
2017-08-01 12:43:36 +0200

    Reorganize pointer code cfr. CUDAnative.

--- src/pointer.jl
+++ src/pointer.jl
@@ -9,9 +9,32 @@
     (::Type{OwnedPtr{T}}){T}(ptr::Ptr{T}, ctx::CuContext) = new{T}(ptr,ctx)
 end
 
-function Base.:(==)(a::OwnedPtr, b::OwnedPtr)
-    return a.ctx == b.ctx && a.ptr == b.ptr
-end
+
+## getters
+
+Base.pointer(p::OwnedPtr) = p.ptr
+
+Base.isnull{T}(p::OwnedPtr{T}) = (p.ptr == C_NULL)
+Base.eltype{T}(::Type{OwnedPtr{T}}) = T
+
+
+## conversions
+
+# between Ptr and OwnedPtr
+## simple conversions disallowed
+Base.convert{T}(::Type{Ptr{T}}, p::OwnedPtr{T}) = throw(InexactError())
+Base.convert{T}(::Type{OwnedPtr{T}}, p::Ptr{T}) = throw(InexactError())
+## unsafe ones are allowed
+Base.unsafe_convert{T}(::Type{Ptr{T}}, p::OwnedPtr) = Ptr{T}(pointer(p))
+
+# between pointers of different types
+Base.convert{T}(::Type{OwnedPtr{T}}, p::OwnedPtr) =
+    OwnedPtr{T}(reinterpret(Ptr{T}, pointer(p)), p.ctx)
+
+
+## limited pointer arithmetic & comparison
+
+Base.:(==)(a::OwnedPtr, b::OwnedPtr) = a.ctx == b.ctx && pointer(a) == pointer(b)
 
 Base.isless(x::OwnedPtr, y::OwnedPtr) = Base.isless(pointer(x), pointer(y))
 Base.:(-)(x::OwnedPtr, y::OwnedPtr) = pointer(x) - pointer(y)
@@ -19,20 +42,3 @@ Base.:(-)(x::OwnedPtr, y::OwnedPtr) = pointer(x) - pointer(y)
 Base.:(+){T}(x::OwnedPtr{T}, y::Integer) = OwnedPtr{T}(pointer(x) + y, x.ctx)
 Base.:(-){T}(x::OwnedPtr{T}, y::Integer) = OwnedPtr{T}(pointer(x) - y, x.ctx)
 Base.:(+)(x::Integer, y::OwnedPtr) = y + x
-
-# simple conversions between Ptr and OwnedPtr are disallowed
-Base.convert{T}(::Type{Ptr{T}}, p::OwnedPtr{T}) = throw(InexactError())
-Base.convert{T}(::Type{OwnedPtr{T}}, p::Ptr{T}) = throw(InexactError())
-
-# unsafe ones are allowed
-Base.unsafe_convert{T}(::Type{Ptr{T}}, p::OwnedPtr) = Ptr{T}(p.ptr)
-
-# conversion between pointers of different types
-Base.convert{T}(::Type{OwnedPtr{T}}, p::OwnedPtr) =
-    OwnedPtr{T}(reinterpret(Ptr{T}, p.ptr), p.ctx)
-
-# some convenience methods (which are already defined for Ptr{T},
-# but due to the disallowed conversions we can't use those)
-Base.isnull{T}(p::OwnedPtr{T}) = (p.ptr == C_NULL)
-Base.eltype{T}(::Type{OwnedPtr{T}}) = T
-Base.pointer(p::OwnedPtr) = p.ptr
