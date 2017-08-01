5e6de7ecf1cbc648c3b2293349dcc3f12c335986
Mike J Innes <mike.j.innes@gmail.com>
2017-08-01 19:50:46 +0100

    gpu implementation

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -3,22 +3,26 @@ Base.Broadcast._containertype(::Type{<:CuArray}) = CuArray
 using Base.Cartesian
 using Base.Broadcast: newindex, _broadcast_getindex
 
-@generated function _broadcast!(f, C::AbstractArray, keeps::K, Idefaults::ID, A::AT, Bs::BT, ::Type{Val{N}}) where {K,ID,AT,BT,N}
-    nargs = N + 1
+@generated function broadcast_kernel(f, C::AbstractArray, keeps::K, Idefaults::ID, A::AT, Bs::BT) where {K,ID,AT,BT}
+    N = length(Bs.parameters)+1
     quote
-        $(Expr(:meta, :inline))
         A_1 = A
-        @nexprs $N i-> A_{i+1} = Bs[i]
-        @nexprs $nargs i -> keep_i = keeps[i]
-        @nexprs $nargs i -> Idefault_i = Idefaults[i]
-        for i = 1:length(C)
-            I = CartesianIndex(ind2sub(C, i))
-            @nexprs $nargs i -> I_i = newindex(I, keep_i, Idefault_i)
-            @nexprs $nargs i -> @inbounds val_i = _broadcast_getindex(A_i, I_i)
-            result = @ncall $nargs f val
-            @inbounds C[I] = result
-        end
+        @nexprs $(N-1) i-> A_{i+1} = Bs[i]
+        @nexprs $N i -> keep_i = keeps[i]
+        @nexprs $N i -> Idefault_i = Idefaults[i]
+
+        i = (blockIdx().x-1) * blockDim().x + threadIdx().x
+        I = CartesianIndex(ind2sub(C, i))
+        @nexprs $N i -> I_i = newindex(I, keep_i, Idefault_i)
+        @nexprs $N i -> @inbounds val_i = _broadcast_getindex(A_i, I_i)
+        result = @ncall $N f val
+        @inbounds C[I] = result
     end
+end
+
+@inline function _broadcast!(f, C::AbstractArray, keeps, Idefaults, A, Bs)
+    @cuda (1, length(C)) broadcast_kernel(f, todevice(C), keeps, Idefaults, todevice(A), todevice.(Bs))
+    return C
 end
 
 using Base.Broadcast: map_newindexer, _broadcast_eltype, broadcast_indices,
@@ -32,16 +36,16 @@ Base.Broadcast.broadcast_indices(::Type{CuArray}, A) = indices(A)
     isleaftype(T) || error("Broadcast output type $T is not concrete")
     C = similar(CuArray{T}, shape)
     keeps, Idefaults = map_newindexer(shape, A, Bs)
-    _broadcast!(f, C, keeps, Idefaults, A, Bs, Val{N})
-    return C
+    _broadcast!(f, C, keeps, Idefaults, A, Bs)
 end
+
+# Called by Base broadcasting mechanisms (in place and out of place)
 
 @inline function Base.Broadcast.broadcast_c!(f, ::Type{CuArray}, ::Type, C, A, Bs::Vararg{Any,N}) where N
     shape = indices(C)
     @boundscheck check_broadcast_indices(shape, A, Bs...)
     keeps, Idefaults = map_newindexer(shape, A, Bs)
-    _broadcast!(f, C, keeps, Idefaults, A, Bs, Val{N})
-    return C
+    _broadcast!(f, C, keeps, Idefaults, A, Bs)
 end
 
 @inline function Base.Broadcast.broadcast_c(f, ::Type{CuArray}, A, Bs...)
