ed302ca9a268ba4c544da750d4eba6ebe53725d1
Mike J Innes <mike.j.innes@gmail.com>
2017-08-22 21:45:42 +0100

    cublas cleanup

--- src/blas.jl
+++ src/blas.jl
@@ -1,8 +1,6 @@
 using CUBLAS
 
-CuFloatArray{T<:CUBLAS.CublasFloat,N} = CuArray{T,N}
+CUBLASArray{T<:CUBLAS.CublasFloat,N} = CuArray{T,N}
 
-# TODO: reduce the conversion overhead
-
-Base.LinAlg.A_mul_B!(c::CuFloatArray, a::CuFloatArray, b::CuFloatArray) =
-  CuArray(A_mul_B!(CUBLAS.CuArray.(promote(c, a, b))...))
+Base.LinAlg.A_mul_B!(c::CUBLASArray, a::CUBLASArray, b::CUBLASArray) =
+  CuArray(A_mul_B!(CUDAdrv.CuArray.(promote(c, a, b))...))
