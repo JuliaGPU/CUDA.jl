8b7dfb021a116ad9aff0597e0f8a99cb753f57fd
Mike J Innes <mike.j.innes@gmail.com>
2017-08-23 11:35:58 +0100

    style fixes

--- src/blas.jl
+++ src/blas.jl
@@ -8,5 +8,5 @@ Base.LinAlg.A_mul_B!(c::CUBLASArray, a::CUBLASArray, b::CUBLASArray) =
 Base.LinAlg.A_mul_Bt!(c::CUBLASArray, a::CUBLASArray, b::CUBLASArray) =
   CuArray(A_mul_Bt!(CUDAdrv.CuArray.(promote(c, a, b))...))
 
-  Base.LinAlg.At_mul_B!(c::CUBLASArray, a::CUBLASArray, b::CUBLASArray) =
+Base.LinAlg.At_mul_B!(c::CUBLASArray, a::CUBLASArray, b::CUBLASArray) =
   CuArray(At_mul_B!(CUDAdrv.CuArray.(promote(c, a, b))...))
--- src/dnn.jl
+++ src/dnn.jl
@@ -3,8 +3,9 @@ import NNlib: softmax!
 
 const CUDNNFloat = Union{Float16,Float32,Float64}
 CUDNNArray{T<:CUDNNFloat,N} = CuArray{T,N}
-CUDNNVector{T<:CUDNNFloat} = CuArray{T,1}
-CUDNNMatrix{T<:CUDNNFloat} = CuArray{T,2}
+
+CUDNNVector{T} = CUDNNArray{T,1}
+CUDNNMatrix{T} = CUDNNArray{T,2}
 CUDNNVecOrMat{T} = Union{CUDNNVector{T},CUDNNMatrix{T}}
 
 function softmax!(out::CUDNNVecOrMat, xs::CUDNNVecOrMat)
