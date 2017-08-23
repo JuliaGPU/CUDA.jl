7713e2428d75d33ec9b8ec823581b64720c2ec49
Mike J Innes <mike.j.innes@gmail.com>
2017-08-23 12:05:57 +0100

    cleaner tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,10 @@ using Base.Test
 
 CuArrays.allowslow(false)
 
+function testf(f, xs...)
+  @test collect(f(cu.(xs)...)) ≈ collect(f(xs...))
+end
+
 @testset "CuArrays" begin
 
 @testset "Array" begin
@@ -11,74 +15,55 @@ CuArrays.allowslow(false)
   @test size(xs) == (2, 3)
   @test collect(CuArray([1 2; 3 4])) == [1 2; 3 4]
   @test collect(cu[1, 2, 3]) == [1, 2, 3]
+  @test collect(cu([1, 2, 3])) == [1, 2, 3]
 end
 
 @testset "Indexing" begin
-  xs = CuArray(rand(2, 3))
-  # For some reason, this fails during Pkg.test() only...
-  @test collect(xs[1:2, 2]) == collect(xs)[1:2, 2]
-  @test collect(xs[[2, 1], :]) == collect(xs)[[2, 1], :]
+  testf(x -> x[1:2, 2], rand(2,3))
+  testf(x -> x[[2,1], :], rand(2,3))
 end
 
 @testset "PermuteDims" begin
-  xs = CuArray(rand(2, 3))
-  @test collect(permutedims(xs, (2, 1))) == permutedims(collect(xs), (2, 1))
-  xs = CuArray(rand(4, 5, 6))
-  @test collect(permutedims(xs, (2, 1, 3))) == permutedims(collect(xs), (2, 1, 3))
+  testf(x -> permutedims(x, (2, 1)), rand(2, 3))
+  testf(x -> permutedims(x, (2, 1, 3)), rand(4, 5, 6))
 end
 
 @testset "Concat" begin
-  xs = CuArray(rand(3, 3))
-  ys = CuArray(rand(3, 3))
-  @test collect(hcat(xs, ys)) == hcat(collect(xs), collect(ys))
-  @test collect(vcat(xs, ys)) == vcat(collect(xs), collect(ys))
+  testf(hcat, rand(3, 3), rand(3, 3))
+  testf(vcat, rand(3, 3), rand(3, 3))
 end
 
 @testset "Broadcast" begin
-  @test collect(fill!(CuArray(2,2), 1)) == [1 1; 1 1]
-
-  xs = CuArray(rand(2, 3))
-  ys = CuArray(rand(2, 3))
-  @test collect(map(+, xs, ys)) ≈ map(+, collect(xs), collect(ys))
-
-  @test collect(CuArrays.sin.(xs)) ≈ sin.(collect(xs))
-  ys = CuArray(rand(2, 1))
-  @test collect(xs .+ ys) ≈ collect(xs) .+ collect(ys)
-
-  zs = CuArray(rand(2, 3))
-  zs .= xs .+ ys
-  @test collect(zs) ≈ collect(xs .+ ys)
+  testf((x)       -> fill!(x, 1),  rand(3,3))
+  testf((x, y)    -> map(+, x, y), rand(2, 3), rand(2, 3))
+  testf((x)       -> sin.(x),      rand(2, 3))
+  testf((x, y)    -> x .+ y,       rand(2, 3), rand(1, 3))
+  testf((z, x, y) -> z .= x .+ y,  rand(2, 3), rand(2, 3), rand(2))
 end
 
 @testset "Reduce" begin
-  xs = CuArray(rand(2, 3))
-  @test collect(sum(xs, 1)) ≈ sum(collect(xs), 1)
-  @test collect(sum(xs, 2)) ≈ sum(collect(xs), 2)
-  @test collect(sum(x -> x^2, xs, 1)) ≈ sum(x -> x^2, collect(xs), 1)
-  @test collect(prod(xs, 1)) ≈ prod(collect(xs), 1)
+  testf(x -> sum(x, 1), rand(2, 3))
+  testf(x -> sum(x, 2), rand(2, 3))
+  testf(x -> sum(x -> x^2, x, 1), rand(2, 3))
+  testf(x -> prod(x, 2), rand(2, 3))
 
-  @test sum(xs) ≈ sum(collect(xs))
-  @test prod(xs) ≈ prod(collect(xs))
+  testf(x -> sum(x), rand(2, 3))
+  testf(x -> prod(x), rand(2, 3))
 end
 
 @testset "BLAS" begin
-  xs = cu(rand(5, 5))
-  ys = cu(rand(5))
-  @test collect(xs * xs) ≈ collect(xs) * collect(xs)
-  @test collect(xs * ys) ≈ collect(xs) * collect(ys)
-  xs = cu(rand(5, 5))
-  ys = cu(rand(5, 5))
-  @test collect(A_mul_Bt(xs, ys)) ≈ A_mul_Bt(collect(xs), collect(ys))
-  @test collect(At_mul_B(xs, ys)) ≈ At_mul_B(collect(xs), collect(ys))
+  testf(*, rand(5, 5), rand(5, 5))
+  testf(*, rand(5, 5), rand(5))
+  testf(A_mul_Bt, rand(5, 5), rand(5, 5))
+  testf(At_mul_B, rand(5, 5), rand(5, 5))
 end
 
 using NNlib: softmax, ∇softmax
 
 @testset "NNlib" begin
   for dims in [(5,5), (5,)]
-    xs, Δ = cu(rand(dims)), cu(rand(dims))
-    @test collect(softmax(xs)) ≈ softmax(collect(xs))
-    @test collect(∇softmax(Δ, xs)) ≈ ∇softmax(collect(Δ), collect(xs))
+    testf(softmax, rand(dims))
+    testf(∇softmax, rand(dims), rand(dims))
   end
 end
 
