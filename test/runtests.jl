c0ed858f19341e3c9f40265df96a6ac0d79f14bb
Tim Besard <tim.besard@gmail.com>
2017-08-28 15:05:09 +0200

    Add small test suite.

---
+++ test/runtests.jl
@@ -0,0 +1,28 @@
+using CUDAapi
+using Base.Test
+
+
+## logging
+
+@trace("")
+
+@debug("")
+
+@logging_ccall(:time, :time, Cint, ())
+
+logging_run(`true`)
+
+
+## properties
+
+CUDAapi.gcc_for_cuda(v"8.0")
+CUDAapi.devices_for_cuda(v"8.0")
+CUDAapi.devices_for_llvm(v"5.0")
+
+
+## discovery
+
+toolkit = find_toolkit()
+find_driver()
+find_binary("nvcc", toolkit)
+find_library("cudart", toolkit)
