992c1a09f29dd7be5f8cb143a992fbcce3e1f337
Tim Besard <tim.besard@gmail.com>
2017-09-22 15:46:40 +0200

    Finalize sync_warp.

--- deps/build.jl
+++ deps/build.jl
@@ -121,6 +121,10 @@ function main()
     config[:cuobjdump] = find_binary("cuobjdump", toolkit_path)
     config[:ptxas] = find_binary("ptxas", toolkit_path)
 
+    if config[:cuda_version] >= v"9.0-" && VERSION < v"0.7.0-DEV.1"
+        warn("CUDA 9.0 is only supported on Julia 0.7; see https://github.com/JuliaGPU/CUDAnative.jl/pull/107")
+    end
+
 
     ## (re)generate ext.jl
 
--- src/device/intrinsics/warp_shuffle.jl
+++ src/device/intrinsics/warp_shuffle.jl
@@ -20,12 +20,9 @@ for (name, mode, mask) in ((:shfl_up,   :up,   UInt32(0x00)),
 
     @eval begin
         export $name
-        @inline function $name(val::UInt32, srclane::Integer, width::Integer=$ws)
-            val = ccall($"$intrinsic", llvmcall, UInt32,
-                        (UInt32, UInt32, UInt32), val, convert(UInt32, srclane), $pack_expr)
-            sync_warp()
-            return val
-        end
+        @inline $name(val::UInt32, srclane::Integer, width::Integer=$ws) =
+            ccall($"$intrinsic", llvmcall, UInt32,
+                  (UInt32, UInt32, UInt32), val, convert(UInt32, srclane), $pack_expr)
     end
 end
 
