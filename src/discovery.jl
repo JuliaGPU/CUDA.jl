f499955e5e9e96a46a93939b401724f9712586e2
Tim Besard <tim.besard@gmail.com>
2017-10-20 13:49:39 +0200

    Add functionality from CUDArt to discover a host toolchain.

--- src/discovery.jl
+++ src/discovery.jl
@@ -1,7 +1,10 @@
-export find_library, find_binary, find_toolkit, find_driver
+export find_library, find_binary,
+       find_driver, find_toolkit, find_toolchain
 
 
-# library names
+# names
+
+const nvcc = "nvcc"
 
 const libcuda = Compat.Sys.iswindows() ? "nvcuda" : "cuda"
 const libnvml = Compat.Sys.iswindows() ? "nvml" : "nvidia-ml"
@@ -88,57 +91,6 @@ end
 
 # CUDA-specific
 
-function find_toolkit()
-    # figure out locations
-    dirs = ["/usr/lib/nvidia-cuda-toolkit",
-            "/usr/local/cuda",
-            "/opt/cuda"]
-    ## look for environment variables (taking priority over default values)
-    envvars = ["CUDA_PATH", "CUDA_HOME", "CUDA_ROOT"]
-    envvars_set = filter(var -> haskey(ENV, var), envvars)
-    if length(envvars_set) > 0
-        envvals = unique(map(var->ENV[var], envvars_set))
-        if length(envvals) > 1
-            warn("Multiple CUDA environment variables set to different values: $(join(envvars_set, ", ", " and "))")
-        end
-        unshift!(dirs, envvals...)
-    end
-    ## look for the runtime library (in the case LD_LIBRARY_PATH points to the installation)
-    try
-        libcudart_path = find_library("cudart")
-        dir = dirname(libcudart_path)
-        if ismatch(r"^lib(32|64)?$", basename(dir))
-            dir = dirname(dir)
-        end
-        push!(dirs, dir)
-    catch ex
-        isa(ex, ErrorException) || rethrow(ex)
-    end
-    ## look for the compiler binary (in the case PATH points to the installation)
-    try
-        nvcc_path = find_binary("nvcc")
-        dir = dirname(nvcc_path)
-        if ismatch(r"^bin(32|64)?$", basename(dir))
-            dir = dirname(dir)
-        end
-        push!(dirs, dir)
-    catch ex
-        isa(ex, ErrorException) || rethrow(ex)
-    end
-
-    dirs = filter(isdir, unique(dirs))
-    if length(dirs) > 1
-        warn("Found multiple CUDA toolkit installations: ", join(dirs, ", ", " and "))
-    elseif isempty(dirs)
-        error("Could not find CUDA toolkit; specify using CUDA_(dir|HOME|ROOT) environment variable")
-    end
-
-    # select
-    dir = first(dirs)
-    @debug("Using CUDA toolkit at $dir")
-    return dir
-end
-
 function find_driver()
     # figure out locations
     dirs = String[]
@@ -177,4 +129,146 @@ function find_driver()
     dir = first(dirs)
     @debug("Using CUDA driver at $dir")
     return dir
+end
+
+function find_toolkit()
+    # figure out locations
+    dirs = ["/usr/lib/nvidia-cuda-toolkit",
+            "/usr/local/cuda",
+            "/opt/cuda"]
+    ## look for environment variables (taking priority over default values)
+    envvars = ["CUDA_PATH", "CUDA_HOME", "CUDA_ROOT"]
+    envvars_set = filter(var -> haskey(ENV, var), envvars)
+    if length(envvars_set) > 0
+        envvals = unique(map(var->ENV[var], envvars_set))
+        if length(envvals) > 1
+            warn("Multiple CUDA environment variables set to different values: $(join(envvars_set, ", ", " and "))")
+        end
+        unshift!(dirs, envvals...)
+    end
+    ## look for the runtime library (in the case LD_LIBRARY_PATH points to the installation)
+    try
+        libcudart_path = find_library("cudart")
+        dir = dirname(libcudart_path)
+        if ismatch(r"^lib(32|64)?$", basename(dir))
+            dir = dirname(dir)
+        end
+        push!(dirs, dir)
+    catch ex
+        isa(ex, ErrorException) || rethrow(ex)
+    end
+    ## look for the compiler binary (in the case PATH points to the installation)
+    try
+        nvcc_path = find_binary(nvcc)
+        dir = dirname(nvcc_path)
+        if ismatch(r"^bin(32|64)?$", basename(dir))
+            dir = dirname(dir)
+        end
+        push!(dirs, dir)
+    catch ex
+        isa(ex, ErrorException) || rethrow(ex)
+    end
+
+    dirs = filter(isdir, unique(dirs))
+    if length(dirs) > 1
+        warn("Found multiple CUDA toolkit installations: ", join(dirs, ", ", " and "))
+    elseif isempty(dirs)
+        error("Could not find CUDA toolkit; specify using CUDA_(dir|HOME|ROOT) environment variable")
+    end
+
+    # select
+    dir = first(dirs)
+    @debug("Using CUDA toolkit at $dir")
+    return dir
+end
+
+type Toolchain
+    version::VersionNumber
+    nvcc::String
+    flags::Vector{String}
+end
+function find_toolchain(toolkit_path)
+    nvcc_path = find_binary(nvcc, toolkit_path)
+
+    # parse the nvcc version
+    re = r"release (?<major>\d+).(?<minor>\d+)"
+    m = match(re, readstring(`$nvcc_path --version`))
+    m != nothing || error("Could not get version from nvcc")
+    version = VersionNumber(parse(Int, m[:major]), parse(Int, m[:minor]))
+    @debug("Looking for toolchain supported by CUDA $version")
+
+    # find a suitable host compiler
+    flags = [ "--compiler-bindir" ]
+    if !(is_windows() || is_apple())
+        # Unix-like platforms: find compatible GCC binary
+
+        # find the maximally supported version of gcc
+        gcc_range = gcc_for_cuda(version)
+        @trace("CUDA $version supports GCC $gcc_range")
+
+        # enumerate possible names for the gcc binary
+        # NOTE: this is coarse, and might list invalid, non-existing versions
+        gcc_names = [ "gcc" ]
+        for major in 3:7
+            push!(gcc_names, "gcc-$major")
+            for minor in 0:9
+                push!(gcc_names, "gcc-$major.$minor")
+                push!(gcc_names, "gcc$major$minor")
+            end
+        end
+
+        # find the binary
+        gcc_possibilities = []
+        for gcc_name in gcc_names
+            # check if the binary exists
+            gcc_path = try
+                find_binary(gcc_name)
+            catch ex
+                isa(ex, ErrorException) || rethrow(ex)
+                continue
+            end
+
+            # parse the GCC version string
+            verstring = chomp(readlines(`$gcc_path --version`)[1])
+            m = match(Regex("^$gcc_name \\(.*\\) ([0-9.]+)"), verstring)
+            if m === nothing
+                warn("Could not parse GCC version info (\"$verstring\"), skipping this compiler.")
+                continue
+            end
+            gcc_ver = VersionNumber(m.captures[1])
+            @trace("Found GCC $gcc_ver at $gcc_path")
+
+            if in(gcc_ver, gcc_range)
+                push!(gcc_possibilities, (gcc_path, gcc_ver))
+            end
+        end
+
+        if length(gcc_possibilities) == 0
+            error("Could not find a suitable host compiler (your CUDA v$version needs GCC <= $(get(gcc_maxver))).")
+        end
+        sort!(gcc_possibilities; rev=true, lt=(a, b) -> a[2]<b[2])
+        gcc_path, gcc_ver = gcc_possibilities[1]
+        @debug("Using GCC $gcc_ver at $gcc_path")
+        push!(flags, gcc_path)
+    elseif is_windows()
+        # Windows: just use cl.exe
+
+        vc_versions = ["VS140COMNTOOLS", "VS120COMNTOOLS", "VS110COMNTOOLS", "VS100COMNTOOLS"]
+        !any(x -> haskey(ENV, x), vc_versions) && error("Compatible Visual Studio installation cannot be found; Visual Studio 2015, 2013, 2012, or 2010 is required.")
+        vs_cmd_tools_dir = ENV[vc_versions[first(find(x -> haskey(ENV, x), vc_versions))]]
+        cl_path = joinpath(dirname(dirname(dirname(vs_cmd_tools_dir))), "VC", "bin", Sys.WORD_SIZE == 64 ? "amd64" : "", "cl.exe")
+
+        @debug("Using Visual Studio compiler at $cl_path")
+        push!(flags, cl_path)
+    elseif is_apple()
+        # GCC is no longer supported on MacOS so let's just use clang
+        # TODO: proper version matching, etc
+
+        clang_path = find_binary("clang")
+
+        @trace("Using Clang at $clang_path")
+        push!(flags, clang_path)
+    end
+
+    return Toolchain(version, nvcc_path, flags)
 end
