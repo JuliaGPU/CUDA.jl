6d882763955fd8591fe355b173e4eee993debb83
Tim Besard <tim.besard@gmail.com>
2017-12-13 15:16:38 +0100

    Flip VersionRange inclusivity.

--- src/compatibility.jl
+++ src/compatibility.jl
@@ -16,7 +16,7 @@ struct VersionRange
     upper::VersionNumber
 end
 
-Base.in(v::VersionNumber, r::VersionRange) = (v >= r.lower && v < r.upper)
+Base.in(v::VersionNumber, r::VersionRange) = (v >= r.lower && v <= r.upper)
 
 Base.colon(a::VersionNumber, b::VersionNumber) = VersionRange(a, b)
 
@@ -32,13 +32,13 @@ const highest = v"999"
 
 # Source: CUDA/include/host_config.h or include/crt/host_config.h on 9.0+
 const cuda_gcc_db = Dict(
-    v"5.5" => lowest:v"4.9-",   # (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 8)) && #error
-    v"6.0" => lowest:v"4.9-",   # (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 8)) && #error
-    v"6.5" => lowest:v"4.9-",   # (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 8)) && #error
-    v"7.0" => lowest:v"4.10-",  # (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 9)) && #error
-    v"7.5" => lowest:v"4.10-",  # (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 9)) && #error
-    v"8.0" => lowest:v"6-",     # (__GNUC__ > 5)                                          && #error
-    v"9.0" => lowest:v"7-"      # (__GNUC__ > 6)                                          && #error
+    v"5.5" => lowest:v"4.8",    # (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 8)) && #error
+    v"6.0" => lowest:v"4.8",    # (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 8)) && #error
+    v"6.5" => lowest:v"4.8",    # (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 8)) && #error
+    v"7.0" => lowest:v"4.9",    # (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 9)) && #error
+    v"7.5" => lowest:v"4.9",    # (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 9)) && #error
+    v"8.0" => lowest:v"5",      # (__GNUC__ > 5)                                          && #error
+    v"9.0" => lowest:v"6"       # (__GNUC__ > 6)                                          && #error
 )
 
 function gcc_for_cuda(ver::VersionNumber)
@@ -73,12 +73,12 @@ const cuda_msvc_db = Dict(
 # - https://en.wikipedia.org/wiki/CUDA#GPUs_supported
 # - ptxas |& grep -A 10 '\--gpu-name'
 const dev_cuda_db = Dict(
-    v"1.0" => lowest:v"7.0",
-    v"1.1" => lowest:v"7.0",
-    v"1.2" => lowest:v"7.0",
-    v"1.3" => lowest:v"7.0",
-    v"2.0" => lowest:v"9.0",
-    v"2.1" => lowest:v"9.0",
+    v"1.0" => lowest:v"6.5",
+    v"1.1" => lowest:v"6.5",
+    v"1.2" => lowest:v"6.5",
+    v"1.3" => lowest:v"6.5",
+    v"2.0" => lowest:v"8.0",
+    v"2.1" => lowest:v"8.0",
     v"3.0" => v"4.2":highest,
     v"3.2" => v"6.0":highest,
     v"3.5" => v"5.0":highest,
@@ -180,8 +180,8 @@ end
 
 # Source: LLVM/lib/Target/NVPTX/NVPTX.td
 const isa_llvm_db = Dict(
-    v"3.0" => v"3.2":v"3.6",
-    v"3.1" => v"3.2":v"3.6",
+    v"3.0" => v"3.2":v"3.5",
+    v"3.1" => v"3.2":v"3.5",
     v"3.2" => v"3.5":highest,
     v"4.0" => v"3.5":highest,
     v"4.1" => v"3.7":highest,
