33ce878e67d28c54254416156f5c75f4a7283e96
Tim Besard <tim.besard@gmail.com>
2018-01-08 14:47:28 +0100

    Add a CUSOLVERError type.

--- src/solver/CUSOLVER.jl
+++ src/solver/CUSOLVER.jl
@@ -9,37 +9,7 @@ import Base.one
 import Base.zero
 
 include("libcusolver_types.jl")
-
-function statusmessage( status )
-    if status == CUSOLVER_STATUS_SUCCESS
-        return "cusolver success"
-    elseif status == CUSOLVER_STATUS_NOT_INITIALIZED
-        return "cusolver not initialized"
-    elseif status == CUSOLVER_STATUS_ALLOC_FAILED
-        return "cusolver allocation failed"
-    elseif status == CUSOLVER_STATUS_INVALID_VALUE
-        return "cusolver invalid value"
-    elseif status == CUSOLVER_STATUS_ARCH_MISMATCH
-        return "cusolver architecture mismatch"
-    elseif status == CUSOLVER_STATUS_EXECUTION_FAILED
-        return "cusolver execution failed"
-    elseif status == CUSOLVER_STATUS_INTERNAL_ERROR
-        return "cusolver internal error"
-    elseif status == CUSOLVER_STATUS_MATRIX_TYPE_NOT_SUPPORTED
-        return "cusolver matrix type not supported"
-    end
-end
-
-function statuscheck( status )
-    if status == CUSOLVER_STATUS_SUCCESS
-        return nothing
-    end
-    warn("CUSOLVER error triggered from:")
-    Base.show_backtrace(STDOUT, backtrace())
-    println()
-    throw(statusmessage(status))
-end
-
+include("error.jl")
 include("libcusolver.jl")
 
 const cusolverDnhandle = cusolverDnHandle_t[0]
