15008e88f6fc60e8e4fae2e4c77346b43e968345
Mike J Innes <mike.j.innes@gmail.com>
2018-01-09 15:44:09 +0000

    more descriptors

--- src/dnn/helpers.jl
+++ src/dnn/helpers.jl
@@ -17,9 +17,7 @@ juliaDataType(a)=(a==CUDNN_DATA_HALF ? Float16 :
 # Descriptors
 
 mutable struct TD; ptr; end
-
 free(td::TD) = cudnnDestroyTensorDescriptor(td.ptr)
-
 Base.unsafe_convert(::Type{cudnnTensorDescriptor_t}, td::TD)=td.ptr
 
 function TD(a::CuArray, dims=ndims(a))
@@ -28,6 +26,46 @@ function TD(a::CuArray, dims=ndims(a))
     cudnnCreateTensorDescriptor(d)
     cudnnSetTensorNdDescriptor(d[1], cudnnDataType(a), length(sz), sz, st)
     this = TD(d[1])
+    finalizer(this, free)
+    return this
+end
+
+mutable struct FD; ptr; end
+free(fd::FD)=cudnnDestroyFilterDescriptor(fd.ptr)
+Base.unsafe_convert(::Type{cudnnFilterDescriptor_t}, fd::FD)=fd.ptr
+
+function FD(a::CuArray, dims=ndims(a), format=CUDNN_TENSOR_NCHW)
+    # The only difference of a FilterDescriptor is no strides.
+    (sz, st) = tensorsize(a, dims)
+    d = cudnnFilterDescriptor_t[0]
+    cudnnCreateFilterDescriptor(d)
+    CUDNN_VERSION >= 5000 ?
+        cudnnSetFilterNdDescriptor(d[1], cudnnDataType(a), format, length(sz), sz) :
+    CUDNN_VERSION >= 4000 ?
+        cudnnSetFilterNdDescriptor_v4(d[1], cudnnDataType(a), format, length(sz), sz) :
+        cudnnSetFilterNdDescriptor(d[1], cudnnDataType(a), length(sz), sz)
+    this = FD(d[1])
+    finalizer(this, free)
+    return this
+end
+
+mutable struct CD; ptr; end
+free(cd::CD) = cudnnDestroyConvolutionDescriptor(cd.ptr)
+Base.unsafe_convert(::Type{cudnnConvolutionDescriptor_t}, cd::CD)=cd.ptr
+
+function cdsize(w, nd)
+    isa(w, Integer) ? Cint[fill(w,nd)...] :
+    length(w)!=nd ? error("Dimension mismatch") :
+    Cint[reverse(w)...]
+end
+
+function CD(T, N, padding, stride, upscale, mode)
+    cd = cudnnConvolutionDescriptor_t[0]
+    cudnnCreateConvolutionDescriptor(cd)
+    CUDNN_VERSION >= 4000 ? cudnnSetConvolutionNdDescriptor(cd[1],N,cdsize(padding,N),cdsize(stride,N),cdsize(upscale,N),mode,cudnnDataType(T)) :
+    CUDNN_VERSION >= 3000 ? cudnnSetConvolutionNdDescriptor_v3(cd[1],N,cdsize(padding,N),cdsize(stride,N),cdsize(upscale,N),mode,cudnnDataType(T)) :
+    cudnnSetConvolutionNdDescriptor(cd[1],N,cdsize(padding,N),cdsize(stride,N),cdsize(upscale,N),mode)
+    this = CD(cd[1])
     finalizer(this, free)
     return this
 end
--- src/dnn/libcudnn.jl
+++ src/dnn/libcudnn.jl
@@ -30,6 +30,50 @@ function cudnnDestroyTensorDescriptor(tensorDesc)
     @check ccall((:cudnnDestroyTensorDescriptor,libcudnn),cudnnStatus_t,(cudnnTensorDescriptor_t,),tensorDesc)
 end
 
+function cudnnCreateFilterDescriptor(filterDesc)
+    @check ccall((:cudnnCreateFilterDescriptor,libcudnn),cudnnStatus_t,(Ptr{cudnnFilterDescriptor_t},),filterDesc)
+end
+
+function cudnnSetFilterNdDescriptor(filterDesc,dataType,nbDims,filterDimA)
+    @check ccall((:cudnnSetFilterNdDescriptor,libcudnn),cudnnStatus_t,(cudnnFilterDescriptor_t,cudnnDataType_t,Cint,Ptr{Cint}),filterDesc,dataType,nbDims,filterDimA)
+end
+
+function cudnnSetFilterNdDescriptor(filterDesc,dataType,format,nbDims,filterDimA)
+    @check ccall((:cudnnSetFilterNdDescriptor,libcudnn),cudnnStatus_t,(cudnnFilterDescriptor_t,cudnnDataType_t,cudnnTensorFormat_t,Cint,Ptr{Cint}),filterDesc,dataType,format,nbDims,filterDimA)
+end
+
+function cudnnSetFilterNdDescriptor_v4(filterDesc,dataType,format,nbDims,filterDimA)
+    @check ccall((:cudnnSetFilterNdDescriptor_v4,libcudnn),cudnnStatus_t,(cudnnFilterDescriptor_t,cudnnDataType_t,cudnnTensorFormat_t,Cint,Ptr{Cint}),filterDesc,dataType,format,nbDims,filterDimA)
+end
+
+function cudnnGetFilterNdDescriptor(filterDesc,nbDimsRequested,dataType,nbDims,filterDimA)
+    @check ccall((:cudnnGetFilterNdDescriptor,libcudnn),cudnnStatus_t,(cudnnFilterDescriptor_t,Cint,Ptr{cudnnDataType_t},Ptr{Cint},Ptr{Cint}),filterDesc,nbDimsRequested,dataType,nbDims,filterDimA)
+end
+
+function cudnnGetFilterNdDescriptor_v4(filterDesc,nbDimsRequested,dataType,format,nbDims,filterDimA)
+    @check ccall((:cudnnGetFilterNdDescriptor_v4,libcudnn),cudnnStatus_t,(cudnnFilterDescriptor_t,Cint,Ptr{cudnnDataType_t},Ptr{cudnnTensorFormat_t},Ptr{Cint},Ptr{Cint}),filterDesc,nbDimsRequested,dataType,format,nbDims,filterDimA)
+end
+
+function cudnnDestroyFilterDescriptor(filterDesc)
+    @check ccall((:cudnnDestroyFilterDescriptor,libcudnn),cudnnStatus_t,(cudnnFilterDescriptor_t,),filterDesc)
+end
+
+function cudnnCreateConvolutionDescriptor(convDesc)
+    @check ccall((:cudnnCreateConvolutionDescriptor,libcudnn),cudnnStatus_t,(Ptr{cudnnConvolutionDescriptor_t},),convDesc)
+end
+
+function cudnnSetConvolutionNdDescriptor(convDesc,arrayLength,padA,filterStrideA,upscaleA,mode,dataType)
+    @check ccall((:cudnnSetConvolutionNdDescriptor,libcudnn),cudnnStatus_t,(cudnnConvolutionDescriptor_t,Cint,Ptr{Cint},Ptr{Cint},Ptr{Cint},cudnnConvolutionMode_t,cudnnDataType_t),convDesc,arrayLength,padA,filterStrideA,upscaleA,mode,dataType)
+end
+
+function cudnnGetConvolutionNdDescriptor(convDesc,arrayLengthRequested,arrayLength,padA,strideA,upscaleA,mode,dataType)
+    @check ccall((:cudnnGetConvolutionNdDescriptor,libcudnn),cudnnStatus_t,(cudnnConvolutionDescriptor_t,Cint,Ptr{Cint},Ptr{Cint},Ptr{Cint},Ptr{Cint},Ptr{cudnnConvolutionMode_t},Ptr{cudnnDataType_t}),convDesc,arrayLengthRequested,arrayLength,padA,strideA,upscaleA,mode,dataType)
+end
+
+function cudnnDestroyConvolutionDescriptor(convDesc)
+    @check ccall((:cudnnDestroyConvolutionDescriptor,libcudnn),cudnnStatus_t,(cudnnConvolutionDescriptor_t,),convDesc)
+end
+
 function cudnnSoftmaxForward(handle,algo,mode,alpha,xDesc,x,beta,yDesc,y)
     @check ccall((:cudnnSoftmaxForward,libcudnn),cudnnStatus_t,(cudnnHandle_t,cudnnSoftmaxAlgorithm_t,cudnnSoftmaxMode_t,Ptr{Void},cudnnTensorDescriptor_t,Ptr{Void},Ptr{Void},cudnnTensorDescriptor_t,Ptr{Void}),handle,algo,mode,alpha,xDesc,x,beta,yDesc,y)
 end
