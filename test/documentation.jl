142f672ad3c118340cb14e5afc2d7f8afc09233d
Tim Besard <tim.besard@gmail.com>
2018-01-28 13:28:41 +0100

    Don't check doctest output (fix #79).

--- test/documentation.jl
+++ test/documentation.jl
@@ -1,23 +1,9 @@
 @testset "documentation" begin
 
-out = Pipe()
-result = cd(joinpath(dirname(@__DIR__), "docs")) do
+cd(joinpath(dirname(@__DIR__), "docs")) do
     withenv("TEST"=>true) do
-        cmd = julia_cmd(`make.jl`)
-        success(pipeline(cmd; stdout=out, stderr=out))
+        run(julia_cmd(`make.jl`))
     end
-end
-close(out.in)
-
-output = readstring(out)
-println(output)
-
-if !result
-    error("error making documentation")
-end
-
-if contains(output, "Test Error")
-    error("error running doctests")
 end
 
 end
