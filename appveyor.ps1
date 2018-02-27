d98c0d9cd7951f6ddd14e4f0e92610fc646d357a
Tim Besard <tim.besard@gmail.com>
2018-02-27 09:00:56 +0100

    Simplify AppVeyor config.

---
+++ appveyor.ps1
@@ -0,0 +1,22 @@
+# Julia
+
+$julia_installers = @{}
+$julia_installers.Add('0.6',     'https://julialang-s3.julialang.org/bin/winnt/x64/0.6/julia-0.6-latest-win64.exe')
+$julia_installers.Add('nightly', 'https://julialangnightlies-s3.julialang.org/bin/winnt/x64/julia-latest-win64.exe')
+
+$julia_installer = $julia_installers.Get_Item($env:JULIA)
+
+appveyor DownloadFile $julia_installer -FileName julia.exe
+julia.exe /S /D=C:\julia
+
+
+# CUDA
+
+$cuda_installers = @{}
+$cuda_installers.Add('8.0', 'https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe')
+$cuda_installers.Add('9.0', 'https://developer.nvidia.com/compute/cuda/9.0/prod/local_installers/cuda_9.0.176_windows-exe')
+
+$cuda_installer = $cuda_installers.Get_Item($env:CUDA)
+
+appveyor DownloadFile $cuda_installer -FileName cuda.exe
+cuda.exe -s "compiler_$env:CUDA" "cudart_$env:CUDA"
