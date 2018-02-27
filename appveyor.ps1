a340804a25dd330fa55c448b587e030c344a76b4
Tim Besard <tim.besard@gmail.com>
2018-02-27 10:03:23 +0100

    Invoke-WebRequest is slow.

--- appveyor.ps1
+++ appveyor.ps1
@@ -9,7 +9,7 @@ $julia_installers.Add('0.6',     'https://julialang-s3.julialang.org/bin/winnt/x
 $julia_installers.Add('nightly', 'https://julialangnightlies-s3.julialang.org/bin/winnt/x64/julia-latest-win64.exe')
 
 $julia_installer = $julia_installers.Get_Item($env:JULIA)
-Invoke-WebRequest $julia_installer -OutFile julia.exe
+Start-FileDownload $julia_installer -FileName julia.exe
 
 .\julia.exe /S /D=C:\julia
 
@@ -21,6 +21,6 @@ $cuda_installers.Add('8.0', 'https://developer.nvidia.com/compute/cuda/8.0/prod/
 $cuda_installers.Add('9.0', 'https://developer.nvidia.com/compute/cuda/9.0/prod/local_installers/cuda_9.0.176_windows-exe')
 
 $cuda_installer = $cuda_installers.Get_Item($env:CUDA)
-Invoke-WebRequest $cuda_installer -OutFile cuda.exe
+Start-FileDownload $cuda_installer -FileName cuda.exe
 
 .\cuda.exe -s "compiler_$env:CUDA" "cudart_$env:CUDA"
