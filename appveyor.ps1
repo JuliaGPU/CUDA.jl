ccd313a6a0305d66859727257b3721ff6bfde551
Tim Besard <tim.besard@gmail.com>
2018-02-27 09:17:19 +0100

    Clean-ups.

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,3 +1,6 @@
+Set-PSDebug -Trace 1
+
+
 # Julia
 
 $julia_installers = @{}
@@ -5,9 +8,10 @@ $julia_installers.Add('0.6',     'https://julialang-s3.julialang.org/bin/winnt/x
 $julia_installers.Add('nightly', 'https://julialangnightlies-s3.julialang.org/bin/winnt/x64/julia-latest-win64.exe')
 
 $julia_installer = $julia_installers.Get_Item($env:JULIA)
+Invoke-WebRequest $julia_installer -OutFile julia.exe
 
-appveyor DownloadFile $julia_installer -FileName julia.exe
 julia.exe /S /D=C:\julia
+Remove-Item julia.exe
 
 
 # CUDA
@@ -17,6 +21,7 @@ $cuda_installers.Add('8.0', 'https://developer.nvidia.com/compute/cuda/8.0/prod/
 $cuda_installers.Add('9.0', 'https://developer.nvidia.com/compute/cuda/9.0/prod/local_installers/cuda_9.0.176_windows-exe')
 
 $cuda_installer = $cuda_installers.Get_Item($env:CUDA)
+Invoke-WebRequest $cuda_installer -OutFile cuda.exe
 
-appveyor DownloadFile $cuda_installer -FileName cuda.exe
 cuda.exe -s "compiler_$env:CUDA" "cudart_$env:CUDA"
+Remove-Item cuda.exe
