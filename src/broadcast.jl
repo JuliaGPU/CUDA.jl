7b1da954b093ab65f18d0450c3e4f46f738ebe15
Mike J Innes <mike.j.innes@gmail.com>
2018-03-01 15:06:46 +0000

    tanh fix

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -112,9 +112,14 @@ using DiffRules
 
 for f in libdevice
   if haskey(DiffRules.DEFINED_DIFFRULES, (:Base,f,1))
+    f == :tanh && continue
     diffrule = DiffRules.DEFINED_DIFFRULES[(:Base,f,1)]
     DiffRules.DEFINED_DIFFRULES[(:CUDAnative,f,1)] =
       (args...) -> replace_device(diffrule(args...))
     eval(unary_dual_definition(:CUDAnative, f))
   end
 end
+
+DiffRules.DEFINED_DIFFRULES[(:CUDAnative, :tanh, 1)] = x ->
+  replace_device(:(1-tanh(x)^2))
+eval(unary_dual_definition(:CUDAnative, :tanh))
