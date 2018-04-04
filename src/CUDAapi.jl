7575fa038f5614fb14914f2cb289e3a02c4678b3
Tim Besard <tim.besard@gmail.com>
2018-04-04 09:20:13 +0200

    Use MicroLogging, and be compatible with precompilation.

--- src/CUDAapi.jl
+++ src/CUDAapi.jl
@@ -3,10 +3,26 @@ __precompile__()
 module CUDAapi
 
 using Compat
-VERSION >= v"0.7.0-DEV.3382" && using Libdl
+if VERSION >= v"0.7-"
+    using Libdl
+    using Logging
+else
+    using MicroLogging
+end
 
 include("util.jl")
 include("compatibility.jl")
 include("discovery.jl")
+
+function __init__()
+    DEBUG = parse(Bool, get(ENV, "DEBUG", "false"))
+    if DEBUG
+        if VERSION >= v"0.7-"
+            global_logger(ConsoleLogger(global_logger().stream, Logging.Debug))
+        else
+            configure_logging(min_level=:debug)
+        end
+    end
+end
 
 end
