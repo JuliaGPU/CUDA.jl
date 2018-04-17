cf5e8a7e8c188eb34de01977929c74f7a99efcd5
Tim Besard <tim.besard@gmail.com>
2018-04-17 11:43:42 +0200

    Fix colon deprecation.

--- src/util.jl
+++ src/util.jl
@@ -7,7 +7,13 @@ end
 
 Base.in(v::VersionNumber, r::VersionRange) = (v >= r.lower && v <= r.upper)
 
-Base.colon(a::VersionNumber, b::VersionNumber) = VersionRange(a, b)
+@static if VERSION >= v"0.7.0-DEV.4003"
+    import Base.(:)
+    (:)(a::VersionNumber, b::VersionNumber) = VersionRange(a, b)
+else
+    import Base.colon
+    colon(a::VersionNumber, b::VersionNumber) = VersionRange(a, b)
+end
 
 Base.intersect(v::VersionNumber, r::VersionRange) =
     v < r.lower ? (r.lower:v) :
