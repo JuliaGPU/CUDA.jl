9ba4441a2059d9cf8a543e20f9e0083d0c5d2945
Tim Besard <tim.besard@gmail.com>
2018-04-23 14:56:42 +0200

    Simplify examples test runner.

--- test/examples.jl
+++ test/examples.jl
@@ -1,30 +1,25 @@
 @testset "examples" begin
 
-function find_sources(dir)
-    sources = String[]
-    for entry in readdir(dir)
-        path = joinpath(dir, entry)
-        if isdir(path)
-            append!(sources, find_sources(path))
-        elseif endswith(path, ".jl")
-            push!(sources, path)
+function find_sources(path::String, sources=String[])
+    if isdir(path)
+        for entry in readdir(path)
+            find_sources(joinpath(path, entry), sources)
         end
+    elseif endswith(path, ".jl")
+        push!(sources, path)
     end
-    return sources
+    sources
 end
 
 examples_dir = joinpath(@__DIR__, "..", "examples")
 examples = find_sources(examples_dir)
 filter!(file -> readline(file) != "# EXCLUDE FROM TESTING", examples)
 
-for example in examples
-    id = relpath(example, examples_dir)
-    @eval begin
-        @testset $id begin
-            file = $example
-            cmd = julia_cmd(`$file`)
-            @test success(pipeline(cmd, stderr=stderr))
-        end
+cd(examples_dir) do
+    examples = relpath.(examples, Ref(examples_dir))
+    @testset for example in examples
+        cmd = julia_cmd(`$example`)
+        @test success(pipeline(cmd, stderr=stderr))
     end
 end
 
