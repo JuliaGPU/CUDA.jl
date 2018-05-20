21d03e5a1d33ff5e2383a0385fe129129386a8fa
Tim Besard <tim.besard@gmail.com>
2018-05-20 13:36:15 +0200

    Fix cuprintf example.

--- examples/hello_world.jl
+++ examples/hello_world.jl
@@ -1,7 +1,7 @@
 using CUDAdrv, CUDAnative
 
 hello_world() =
-    @cuprintf("Greetings from block %u, thread %u!\n", blockIdx().x, threadIdx().x)
+    @cuprintf("Greetings from block %ld, thread %ld!\n", Int64(blockIdx().x), Int64(threadIdx().x))
 
 @cuda blocks=2 threads=2 hello_world()
 synchronize()
