08c3585d7bf90e788e56f1f61975518e33e4d992
Tim Besard <tim.besard@gmail.com>
2018-05-22 09:32:47 +0200

    Update CI.

--- appveyor.ps1
+++ appveyor.ps1
@@ -20,6 +20,7 @@ $cuda_installers = @{}
 $cuda_installers.Add('8.0', 'https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe')
 $cuda_installers.Add('9.0', 'https://developer.nvidia.com/compute/cuda/9.0/prod/local_installers/cuda_9.0.176_windows-exe')
 $cuda_installers.Add('9.1', 'https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_windows')
+$cuda_installers.Add('9.2', 'https://developer.nvidia.com/compute/cuda/9.2/Prod/local_installers/cuda_9.2.88_windows')
 
 $cuda_installer = $cuda_installers.Get_Item($env:CUDA)
 Start-FileDownload $cuda_installer -FileName cuda.exe
