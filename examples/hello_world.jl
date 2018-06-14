d999905dcf6eb6c060c3a50408aefd405326d3b5
floswald <florian.oswald@gmail.com>
2018-06-14 10:34:26 +0200

    fixed different formatting string for windows

--- examples/hello_world.jl
+++ examples/hello_world.jl
@@ -1,7 +1,11 @@
 using CUDAdrv, CUDAnative
 
-hello_world() =
-    @cuprintf("Greetings from block %ld, thread %ld!\n", Int64(blockIdx().x), Int64(threadIdx().x))
-
+if Sys.iswindows()
+	hello_world() =
+   	@cuprintf("Greetings from block %lld, thread %lld!\n", Int64(blockIdx().x), Int64(threadIdx().x))
+else
+	hello_world() =
+    	@cuprintf("Greetings from block %ld, thread %ld!\n", Int64(blockIdx().x), Int64(threadIdx().x))
+end
 @cuda blocks=2 threads=2 hello_world()
 synchronize()
