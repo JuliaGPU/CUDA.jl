1dbcf0780c8b742f6f0b7c9983b0ff6816ca589e
Andreas Noack <andreasnoackjensen@gmail.com>
2018-08-13 10:45:18 -0400

    Copy callback logic from CUBLAS to CUSOLVER intialization

--- src/solver/CUSOLVER.jl
+++ src/solver/CUSOLVER.jl
@@ -1,5 +1,8 @@
 module CUSOLVER
 
+using CUDAdrv: CuContext, CuDevice
+using CUDAnative
+
 using ..CuArrays
 const cudaStream_t = Ptr{Nothing}
 
@@ -14,13 +17,33 @@ include("libcusolver_types.jl")
 include("error.jl")
 include("libcusolver.jl")
 
+const libcusolver_handles_dense = Dict{CuContext,cusolverDnHandle_t}()
 const libcusolver_handle_dense = Ref{cusolverDnHandle_t}()
 
 function __init__()
-  configured || return
+    configured || return
 
-  cusolverDnCreate(libcusolver_handle_dense)
-  atexit(() -> cusolverDnDestroy(libcusolver_handle_dense[]))
+    # initialize the library when we switch devices
+    callback = (dev::CuDevice, ctx::CuContext) -> begin
+        libcusolver_handle_dense[] = get!(libcusolver_handles_dense, ctx) do
+            @debug "Initializing CUSOLVER for $dev"
+            handle = Ref{cusolverDnHandle_t}()
+            cusolverDnCreate(handle)
+            handle[]
+        end
+    end
+    push!(CUDAnative.device!_listeners, callback)
+
+    # deinitialize when exiting
+    atexit() do
+        libcusolver_handle_dense[] = C_NULL
+
+        for (ctx, handle) in libcusolver_handles_dense
+            if CUDAdrv.isvalid(ctx)
+                cusolverDnDestroy(handle)
+            end
+        end
+    end
 end
 
 include("dense.jl")
