fe193923f6808d14d1c24bbd5817e0849930ae05
Mike Innes <mike.j.innes@gmail.com>
2018-08-20 15:25:48 +0100

    transpose fix

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -10,7 +10,7 @@ end
 # to variants that are valid on the GPU, as an example we need to convert CuArray to CuDeviceArray
 cudaconvert(bc::Broadcasted{Style}) where Style = Broadcasted{Style}(bc.f, map(cudaconvert, bc.args), bc.axes)
 cudaconvert(ex::Extruded) = Extruded(cudaconvert(ex.x), ex.keeps, ex.defaults)
-cudaconvert(x::LinearAlgebra.Transpose{<:Any,<:CuArray}) = LinearAlgebra.Transpose(cudaconvert(x.vec))
+cudaconvert(x::LinearAlgebra.Transpose{<:Any,<:CuArray}) = LinearAlgebra.Transpose(cudaconvert(x.parent))
 
 # Ref{CuArray} is invalid for GPU codegen
 # see https://github.com/JuliaGPU/CUDAnative.jl/issues/223
