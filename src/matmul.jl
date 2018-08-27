ec99debeca3d01b3b942bb105ad945e4acbdcb86
Tim Besard <tim.besard@gmail.com>
2018-08-27 14:29:21 +0200

    Add fallback for lmul and rmul.

--- src/matmul.jl
+++ src/matmul.jl
@@ -1,5 +1,6 @@
 using LinearAlgebra
 
+
 function generic_matmatmul!(C::AbstractVecOrMat{R}, A::AbstractVecOrMat{T}, B::AbstractVecOrMat{S}) where {T,S,R}
     if size(A,2) != size(B,1)
         throw(DimensionMismatch("matrix A has dimensions $(size(A)), matrix B has dimensions $(size(B))"))
@@ -45,3 +46,27 @@ LinearAlgebra.mul!(C::CuVecOrMat, A::LinearAlgebra.Transpose{<:Any, <:CuVecOrMat
 LinearAlgebra.mul!(C::CuVecOrMat, A::LinearAlgebra.Adjoint{<:Any, <:CuVecOrMat}, B::LinearAlgebra.Transpose{<:Any, <:CuVecOrMat}) = generic_matmatmul!(C, A, B)
 LinearAlgebra.mul!(C::CuVecOrMat, A::LinearAlgebra.Adjoint{<:Any, <:CuVecOrMat}, B::LinearAlgebra.Adjoint{<:Any, <:CuVecOrMat}) = generic_matmatmul!(C, A, B)
 LinearAlgebra.mul!(C::CuVecOrMat, A::LinearAlgebra.Transpose{<:Any, <:CuVecOrMat}, B::LinearAlgebra.Transpose{<:Any, <:CuVecOrMat}) = generic_matmatmul!(C, A, B)
+
+
+function generic_rmul!(X::CuArray, s::Number)
+    function kernel(X, s)
+        i = (blockIdx().x-1) * blockDim().x + threadIdx().x
+        @inbounds X[i] *= s
+    end
+    @cuda blocks=length(X) kernel(X, s)
+    X
+end
+
+LinearAlgebra.rmul!(A::CuArray, b::Number) = generic_rmul!(A, b)
+
+
+function generic_lmul!(s::Number, X::CuArray)
+    function kernel(s, X)
+        i = (blockIdx().x-1) * blockDim().x + threadIdx().x
+        @inbounds X[i] = s*X[i]
+    end
+    @cuda blocks=length(X) kernel(s, X)
+    X
+end
+
+LinearAlgebra.lmul!(a::Number, B::CuArray) = generic_lmul!(a, B)
