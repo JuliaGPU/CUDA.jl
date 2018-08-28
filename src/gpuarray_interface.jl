550546a1e735cdfba39508c5dae030cc8cfe8af8
Tim Besard <tim.besard@gmail.com>
2018-08-28 10:53:52 +0200

    Clean-ups.

--- src/gpuarray_interface.jl
+++ src/gpuarray_interface.jl
@@ -25,11 +25,8 @@ GPUArrays.blasbuffer(x::CuArray) = x
 """
 Blocks until all operations are finished on `A`
 """
-function GPUArrays.synchronize(A::CuArray)
-    # fallback is a noop, for backends not needing synchronization. This
-    # makes it easier to write generic code that also works for AbstractArrays
+GPUArrays.synchronize(A::CuArray) =
     CUDAdrv.synchronize()
-end
 
 for (i, sym) in enumerate((:x, :y, :z))
     for (f, fcu) in (
@@ -50,26 +47,21 @@ GPUArrays.is_gpu(dev::CUDAdrv.CuDevice) = true
 GPUArrays.name(dev::CUDAdrv.CuDevice) = string("CU ", CUDAdrv.name(dev))
 GPUArrays.threads(dev::CUDAdrv.CuDevice) = CUDAdrv.attribute(dev, CUDAdrv.MAX_THREADS_PER_BLOCK)
 
-function GPUArrays.blocks(dev::CUDAdrv.CuDevice)
-    (
-        CUDAdrv.attribute(dev, CUDAdrv.MAX_BLOCK_DIM_X),
-        CUDAdrv.attribute(dev, CUDAdrv.MAX_BLOCK_DIM_Y),
-        CUDAdrv.attribute(dev, CUDAdrv.MAX_BLOCK_DIM_Z),
-    )
-end
+GPUArrays.blocks(dev::CUDAdrv.CuDevice) =
+    (CUDAdrv.attribute(dev, CUDAdrv.MAX_BLOCK_DIM_X),
+     CUDAdrv.attribute(dev, CUDAdrv.MAX_BLOCK_DIM_Y),
+     CUDAdrv.attribute(dev, CUDAdrv.MAX_BLOCK_DIM_Z))
 
 GPUArrays.free_global_memory(dev::CUDAdrv.CuDevice) = CUDAdrv.Mem.info()[1]
 GPUArrays.global_memory(dev::CUDAdrv.CuDevice) = CUDAdrv.totalmem(dev)
 GPUArrays.local_memory(dev::CUDAdrv.CuDevice) = CUDAdrv.attribute(dev, CUDAdrv.TOTAL_CONSTANT_MEMORY)
 
-
-function GPUArrays._gpu_call(::CuArrayBackend, f, A, args::Tuple, blocks_threads::Tuple{T, T}) where T <: NTuple{N, Integer} where N
+function GPUArrays._gpu_call(::CuArrayBackend, f, A, args::Tuple,
+                             blocks_threads::Tuple{T, T}) where {N, T <: NTuple{N, Integer}}
     blk, thr = blocks_threads
-    @info "$f, $(map(typeof, args))"
     @cuda blocks=blk threads=thr f(CuKernelState(), args...)
 end
 
 # Save reinterpret and reshape implementation use this in GPUArrays
-function GPUArrays.unsafe_reinterpret(::Type{T}, A::CuArray{ET}, size::NTuple{N, Integer}) where {T, ET, N}
+GPUArrays.unsafe_reinterpret(::Type{T}, A::CuArray, size::NTuple{N, Integer}) where {T, N} =
     CuArray{T, N}(A.buf, size)
-end
