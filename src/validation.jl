205819633940aebcdfe6ee9851ee9b3229171c70
Tim Besard <tim.besard@gmail.com>
2018-08-29 09:06:18 +0200

    Provide compile traces without relying on inlining.

--- src/validation.jl
+++ src/validation.jl
@@ -59,25 +59,47 @@ function Base.showerror(io::IO, err::InvalidIRError)
 end
 
 # generate a pseudo-backtrace from LLVM IR instruction debug information
-function backtrace(inst)
+function backtrace(inst, bt = StackTraces.StackFrame[])
     name = Ref{Cstring}()
     filename = Ref{Cstring}()
     line = Ref{Cuint}()
     col = Ref{Cuint}()
 
+    # look up the debug information from the current instruction
     depth = 0
-    bt = StackTraces.StackFrame[]
-    VERSION >= v"0.7.0-alpha.37" || return bt
     while LLVM.API.LLVMGetSourceLocation(LLVM.ref(inst), depth, name, filename, line, col) == 1
         frame = StackTraces.StackFrame(replace(unsafe_string(name[]), r";$"=>""), unsafe_string(filename[]), line[])
         push!(bt, frame)
         depth += 1
     end
 
-    # wrapping the kernel _does_ trigger a new debug info frame,
-    # so just get rid of the wrapper frame.
-    if !isempty(bt) && last(bt).func == :KernelWrapper
-        pop!(bt)
+    # move up the call chain
+    f = LLVM.parent(LLVM.parent(inst))
+    callers = uses(f)
+    if isempty(callers)
+        # wrapping the kernel _does_ trigger a new debug info frame,
+        # so just get rid of the wrapper frame.
+        if !isempty(bt) && last(bt).func == :KernelWrapper
+            pop!(bt)
+        end
+    else
+        # figure out the call sites of this instruction
+        call_sites = unique(callers) do call
+            # there could be multiple calls, originating from the same source location
+            md = metadata(user(call))
+            if haskey(md, LLVM.MD_dbg)
+                md[LLVM.MD_dbg]
+            else
+                nothing
+            end
+        end
+
+        if length(call_sites) > 1
+            frame = StackTraces.StackFrame("multiple call sites", "unknown", 0)
+            push!(bt, frame)
+        elseif length(call_sites) == 1
+            backtrace(user(first(call_sites)), bt)
+        end
     end
 
     bt
