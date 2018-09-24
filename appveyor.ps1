926784f03fe59bc988f1ae6cb5e85ae8a29f45b3
Tim Besard <tim.besard@gmail.com>
2018-09-24 07:04:37 +0200

    Add CUDA 10 to the Windows CI roster.

--- appveyor.ps1
+++ appveyor.ps1
@@ -8,6 +8,7 @@ $cuda_installers.Add('8.0', 'https://developer.nvidia.com/compute/cuda/8.0/prod/
 $cuda_installers.Add('9.0', 'https://developer.nvidia.com/compute/cuda/9.0/prod/local_installers/cuda_9.0.176_windows-exe')
 $cuda_installers.Add('9.1', 'https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_windows')
 $cuda_installers.Add('9.2', 'https://developer.nvidia.com/compute/cuda/9.2/Prod/local_installers/cuda_9.2.88_windows')
+$cuda_installers.Add('10.0', 'https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_411.31_win10')
 
 $cuda_installer = $cuda_installers.Get_Item($env:cuda_version)
 Start-FileDownload $cuda_installer -FileName cuda.exe
@@ -17,6 +18,7 @@ $cuda_components.Add('8.0', @("compiler_8.0", "cudart_8.0"))
 $cuda_components.Add('9.0', @("compiler_9.0", "cudart_9.0"))
 $cuda_components.Add('9.1', @("nvcc_9.1", "cudart_9.1"))
 $cuda_components.Add('9.2', @("nvcc_9.2", "cudart_9.2"))
+$cuda_components.Add('10.0', @("nvcc_10.0", "cudart_10.0"))
 
 $components = $cuda_components.Get_Item($env:cuda_version)
 .\cuda.exe -s @components | Out-Null
