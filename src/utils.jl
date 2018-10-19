dcd0db90eafd8dfb5046d2178a8e9b342b21e7d0
Tim Besard <tim.besard@gmail.com>
2018-10-19 11:17:38 +0200

    Remove unused logging functions.

--- src/utils.jl
+++ src/utils.jl
@@ -1,35 +1,3 @@
-# logging
-
-using Logging
-
-# fatal versions to `@error`, including a safe version for compile-time errors
-# (to be used instead of `error`, which _emits_ an error)
-macro fatal(ex...)
-    esc(quote
-        @error $(ex...)
-        error("Fatal error occurred")
-    end)
-end
-
-# define safe loggers for use in generated functions (where task switches are not allowed)
-for level in [:trace, :debug, :info, :warn, :error, :fatal]
-    @eval begin
-        macro $(Symbol("safe_$level"))(ex...)
-            macrocall = :(@placeholder $(ex...))
-            # NOTE: `@placeholder` in order to avoid hard-coding @__LINE__ etc
-            macrocall.args[1] = Symbol($"@$level")
-            quote
-                old_logger = global_logger()
-                global_logger(Logging.ConsoleLogger(Core.stderr, old_logger.min_level))
-                ret = $(esc(macrocall))
-                global_logger(old_logger)
-                ret
-            end
-        end
-    end
-end
-
-
 # device capability handling
 
 # select the highest capability that is supported by both the toolchain and device
