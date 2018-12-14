b796c433055b4a3bb50493c4a0a5f5fc0975da59
Katharine Hyatt <khyatt@flatironinstitute.org>
2018-12-14 15:25:51 -0500

    Fix wrapper and tests

--- test/solver.jl
+++ test/solver.jl
@@ -191,17 +191,25 @@ k = 1
     end
     
     @testset "gesvdj!" begin
-        A              = rand(elty,m,n)
-        d_A            = CuArray(A)
-        d_U, d_S, d_V  = CUSOLVER.gesvdj!('V',1,d_A)
-        h_S            = collect(d_S)
-        h_U            = collect(d_U)
-        h_V            = collect(d_V)
-        F              = svd(A, full=true)
-        @test_broken abs.(h_U'h_U) ≈ Matrix(one(elty)*I, m, m) atol=1e-6
-        @test abs.(h_U[:,1:n]'F.U[:,1:n]) ≈ Matrix(one(elty)*I, n, n)
-        @test h_S ≈ svdvals(A)
-        @test abs.(h_V*h_V') ≈ Matrix(one(elty)*I, n, n)
+        for nm in ((m, n), (n, m))
+            a, b           = nm
+            A              = rand(elty,a,b)
+            d_A            = CuArray(A)
+            d_U, d_S, d_V  = CUSOLVER.gesvdj!('V',1,d_A)
+            h_S            = collect(d_S)
+            h_U            = collect(d_U)
+            h_V            = collect(d_V)
+            F              = svd(A, full=true)
+            s              = min(a, b)
+            t              = max(a, b)
+            @test size(h_V) == (b, s) 
+            @test size(h_U) == (a, s) 
+            tol = elty == Float32 || elty == ComplexF32 ? 1e-4 : 1e-8
+            @test abs.(h_U'*h_U) ≈ Matrix(one(elty)*I, s, s) rtol=tol
+            @test abs.(h_U[:,1:s]'F.U[:,1:s]) ≈ Matrix(one(elty)*I, s, s)
+            @test h_S ≈ svdvals(A)
+            @test abs.(h_V'*h_V) ≈ Matrix(one(elty)*I, s, s)
+        end
     end
 
     @testset "syevd!" begin
