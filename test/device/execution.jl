3b84869f19aa9b460023694061a5398e2847c0b1
Tim Besard <tim.besard@gmail.com>
2018-12-23 10:30:38 +0100

    CI system throws a different error...

--- test/device/execution.jl
+++ test/device/execution.jl
@@ -501,20 +501,23 @@ script = """
 
 let (code, out, err) = julia_script(script, `-g0`)
     @test code == 1
-    @test occursin("ERROR: CUDA error: an illegal instruction was encountered", err)
+    @test occursin("ERROR: CUDA error: an illegal instruction was encountered", err) ||
+          occursin("ERROR: CUDA error: unspecified launch failure", err)
     @test isempty(out)
 end
 
 let (code, out, err) = julia_script(script, `-g1`)
     @test code == 1
-    @test occursin("ERROR: CUDA error: an illegal instruction was encountered", err)
+    @test occursin("ERROR: CUDA error: an illegal instruction was encountered", err) ||
+          occursin("ERROR: CUDA error: unspecified launch failure", err)
     @test occursin(r"ERROR: a .+ exception occurred during kernel execution", out)
     @test occursin(r"Run Julia on debug level 2 for device stack traces", out)
 end
 
 let (code, out, err) = julia_script(script, `-g2`)
     @test code == 1
-    @test occursin("ERROR: CUDA error: an illegal instruction was encountered", err)
+    @test occursin("ERROR: CUDA error: an illegal instruction was encountered", err) ||
+          occursin("ERROR: CUDA error: unspecified launch failure", err)
     @test occursin(r"ERROR: a .+ exception occurred during kernel execution", out)
     @test occursin("[1] Type at float.jl", out)
     @test occursin("[2] kernel at none:2", out)
