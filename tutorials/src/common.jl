b914cfeb7237a17ab9c0026dc87674f47dc08ffb
Tim Besard <tim.besard@gmail.com>
2019-01-03 19:32:52 +0100

    Pass along --project args.

--- tutorials/src/common.jl
+++ tutorials/src/common.jl
@@ -1,13 +1,14 @@
 # function to run a Julia script outside of the current environment
 function script(code; wrapper=``, args=``)
+    if Base.JLOptions().project != C_NULL
+        args = `$args --project=$(unsafe_string(Base.JLOptions().project))`
+    end
     mktemp() do path, io
         write(io, code)
         flush(io)
-        withenv("JULIA_LOAD_PATH" => join(LOAD_PATH, ':')) do
-            cmd = `$wrapper $(Base.julia_cmd()) $args $path`
-            # redirect stderr to stdout to have it picked up by Weave.jl
-            run(pipeline(ignorestatus(cmd), stderr=stdout))
-        end
+        cmd = `$wrapper $(Base.julia_cmd()) $args $path`
+        # redirect stderr to stdout to have it picked up by Weave.jl
+        run(pipeline(ignorestatus(cmd), stderr=stdout))
     end
     nothing
 end
