0b5250b5dc37d9fb01e45f4a74b7623f2bbda650
Tim Besard <tim.besard@gmail.com>
2019-01-03 11:32:37 +0100

    Pkg.dev outside of the main package usage.

--- deps/build.jl
+++ deps/build.jl
@@ -21,15 +21,6 @@ end
 
 ## main
 
-# development often happens in lockstep with other packages,
-# so check-out the master branch of those packages.
-using Pkg
-if haskey(ENV, "GITLAB_CI")
-  for package in ("CUDAapi", "GPUArrays", "CUDAnative", "NNlib", "CUDAdrv")
-    Pkg.add(PackageSpec(name=package, rev="master"))
-  end
-end
-
 config_path = joinpath(@__DIR__, "ext.jl")
 const previous_config_path = config_path * ".bak"
 
