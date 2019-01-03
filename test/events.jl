17a1c2364cc12803588d5b5cdc0bb0cbb72f8e84
Tim Besard <tim.besard@gmail.com>
2019-01-03 10:13:26 +0100

    Wrap the CuEvent flags enum.

--- test/events.jl
+++ test/events.jl
@@ -4,14 +4,21 @@ let
     start = CuEvent()
     stop = CuEvent()
     @test start != stop
+
     record(start)
     record(stop)
     synchronize(stop)
+
     @test elapsed(start, stop) > 0
-    @test (CUDAdrv.@elapsed begin
-        end) > 0
-    @test (CUDAdrv.@elapsed CuDefaultStream() begin
-        end) > 0
 end
+
+@test (CUDAdrv.@elapsed begin
+    end) > 0
+
+@test (CUDAdrv.@elapsed CuDefaultStream() begin
+    end) > 0
+
+CuEvent(CUDAdrv.EVENT_BLOCKING_SYNC)
+CuEvent(CUDAdrv.EVENT_BLOCKING_SYNC | CUDAdrv.EVENT_DISABLE_TIMING)
 
 end
