06949158038874bd1c6b37027d9f43e2edf30259
David Sanders <dpsanders@gmail.com>
2019-02-19 14:04:39 -0500

    Add reference for the algorithm

--- src/accumulate.jl
+++ src/accumulate.jl
@@ -1,10 +1,12 @@
 
+# Implements the Hillis--Steele algorithm using global memory
+# See algorithm 1 at https://en.wikipedia.org/wiki/Prefix_sum#Parallel_algorithm
+
 function accumulate!(op::Function, vout::CuVector{T}, v::CuVector) where T
 
-    vin = T.(v)   # convert to type T
+    vin = convert.(T, v)  # convert to type T
 
     Δ = 1   # Δ = 2^d
-
     n = ceil(Int, log2(length(v)))
 
     num_threads = 256
@@ -15,7 +17,6 @@ function accumulate!(op::Function, vout::CuVector{T}, v::CuVector) where T
         @cuda blocks=num_blocks threads=num_threads _partial_accumulate!(op, vout, vin, Δ)
 
         vin, vout = vout, vin
-
         Δ *= 2
     end
 
