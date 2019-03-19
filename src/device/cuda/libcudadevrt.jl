ce29c10c9d6f4f89f7c95d41e343f0f672c5eff4
Tim Besard <tim.besard@gmail.com>
2019-03-19 13:04:38 +0100

    Use existing at-wrap macro.

--- src/device/cuda/libcudadevrt.jl
+++ src/device/cuda/libcudadevrt.jl
@@ -15,26 +15,4 @@ and should not be called from the host.
 `device_synchronize` acts as a synchronization point for
 child grids in the context of dynamic parallelism.
 """
-@generated function device_synchronize()
-    T_int32 = LLVM.Int32Type(JuliaContext())
-
-    # create function
-    param_types = LLVMType[]
-    llvm_f, _ = create_function(T_int32, param_types)
-    mod = LLVM.parent(llvm_f)
-
-    # generate IR
-    Builder(JuliaContext()) do builder
-        entry = BasicBlock(llvm_f, "entry", JuliaContext())
-        position!(builder, entry)
-
-        f_typ = LLVM.FunctionType(T_int32, param_types)
-        f = LLVM.Function(mod, "cudaDeviceSynchronize", f_typ)
-        LLVM.linkage!(f, LLVM.API.LLVMExternalLinkage)
-
-        val = call!(builder, f)
-        ret!(builder, val)
-    end
-
-    call_function(llvm_f, Cint, Tuple{})
-end
+@inline device_synchronize() = @wrap cudaDeviceSynchronize()::i32
