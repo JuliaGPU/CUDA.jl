6fb665bca27ccf9f686f81880137c7419eea1147
Tim Besard <tim.besard@gmail.com>
2019-03-26 14:52:24 +0100

    Use CuArrays for vadd example.

--- examples/vadd.jl
+++ examples/vadd.jl
@@ -1,4 +1,4 @@
-using CUDAdrv
+using CUDAdrv, CuArrays
 
 using Test
 
@@ -11,19 +11,15 @@ vadd = CuFunction(md, "kernel_vadd")
 dims = (3,4)
 a = round.(rand(Float32, dims) * 100)
 b = round.(rand(Float32, dims) * 100)
-c = similar(a)
 
-d_a = Mem.alloc(Mem.Device, sizeof(a))
-d_b = Mem.alloc(Mem.Device, sizeof(a))
-d_c = Mem.alloc(Mem.Device, sizeof(c))
-
-copyto!(d_a, pointer(a), sizeof(a))
-copyto!(d_b, pointer(b), sizeof(b))
+d_a = CuArray(a)
+d_b = CuArray(b)
+d_c = similar(d_a)
 
 len = prod(dims)
 cudacall(vadd, Tuple{CuPtr{Cfloat},CuPtr{Cfloat},CuPtr{Cfloat}}, d_a, d_b, d_c; threads=len)
 
-copyto!(pointer(c), d_c, sizeof(c))
+c = Array(d_c)
 
 @test a+b ≈ c
 
