a538b0a22bd78b5bbb0b01e5ec1e515756242759
Elliot Saba <staticfloat@gmail.com>
2019-04-01 13:27:41 -0700

    Fix embarrassing typo

--- src/dnn/compat.jl
+++ src/dnn/compat.jl
@@ -1,13 +1,13 @@
 # Compatibility shims until users upgrade to new NNlib format
 function conv!(y::CuArray{T}, x::CuArray{T}, w::CuArray{T}; pad=0, stride=1, flipkernel=0, dilation=1, kwargs...) where {T<:CUDNNFloat}
     cdims = DenseConvDims(x, w; padding=pad, stride=stride, flipkernel=flipkernel, dilation=dilation)
-    return conv!(y, x, w, cdims, kwargs...)
+    return conv!(y, x, w, cdims; kwargs...)
 end
 
 function ∇conv_filter!(dw::CuArray{T}, dy::CuArray{T}, x::CuArray{T}; pad=0, stride=1, flipkernel=0, dilation=1, kwargs...) where {T<:CUDNNFloat}
     cdims = DenseConvDims(x, dw; padding=pad, stride=stride, flipkernel=flipkernel, dilation=dilation)
     # NOTE!!!  This compat shim re-arranges the argument order!
-    return ∇conv_filter!(dw, x, dy, cdims, kwargs...)
+    return ∇conv_filter!(dw, x, dy, cdims; kwargs...)
 end
 
 function maxpool!(y::CuArray{T}, x::CuArray{T}, k; pad=map(_->0,k), stride=k) where {T<:CUDNNFloat}
