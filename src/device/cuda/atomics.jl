006e6e31be3afc7c548af2d4c66e111f6e39226b
Tim Besard <tim.besard@gmail.com>
2019-04-10 16:59:15 +0200

    Fixes.

--- src/device/cuda/atomics.jl
+++ src/device/cuda/atomics.jl
@@ -1,8 +1,24 @@
 # Atomic Functions (B.12)
 
 # TODO:
-# - _system and _block versions (see CUDA programming guide)
+# - scoped atomics: _system and _block versions (see CUDA programming guide, sm_60+)
+#   https://github.com/Microsoft/clang/blob/86d4513d3e0daa4d5a29b0b1de7c854ca15f9fe5/test/CodeGen/builtins-nvptx.c#L293
 # - atomic_cas!
+
+
+#
+# LLVM
+#
+
+# common arithmetic operations on integers using LLVM instructions
+#
+# > 8.6.6. atomicrmw Instruction
+# >
+# > nand is not supported. The other keywords are supported for i32 and i64 types, with the
+# > following restrictions.
+# >
+# > - The pointer must be either a global pointer, a shared pointer, or a generic pointer
+# >   that points to either the global address space or the shared address space.
 
 @generated function llvm_atomic(::Val{binop}, ptr::DevicePtr{T,A}, val::T, ::Val{ordering}) where
                                {binop, T, A, ordering}
@@ -46,15 +62,6 @@ const binops = Dict(
 const aquire = LLVM.API.LLVMAtomicOrderingAcquire
 const aquire_release = LLVM.API.LLVMAtomicOrderingAcquireRelease
 
-# common arithmetic operations on integers using LLVM instructions
-#
-# > 8.6.6. atomicrmw Instruction
-# >
-# > nand is not supported. The other keywords are supported for i32 and i64 types, with the
-# > following restrictions.
-# >
-# > - The pointer must be either a global pointer, a shared pointer, or a generic pointer
-# >   that points to either the global address space or the shared address space.
 for T in (Int32, Int64, UInt32, UInt64)
     ops = [:xchg, :add, :sub, :and, :or, :xor, :max, :min]
 
@@ -74,21 +81,48 @@ for T in (Int32, Int64, UInt32, UInt64)
     end
 end
 
+
+#
+# NVVM
+#
+
 # floating-point operations using NVVM intrinsics
-# TODO: LLVM supports _some_ atomic_rmw ops with floating-point types
-#       does NVPTX support those? why use NVVM intrinsics?
+
 for A in (AS.Generic, AS.Global, AS.Shared)
     # declare float @llvm.nvvm.atomic.load.add.f32.p0f32(float* address, float val)
     # declare float @llvm.nvvm.atomic.load.add.f32.p1f32(float addrspace(1)* address, float val)
     # declare float @llvm.nvvm.atomic.load.add.f32.p3f32(float addrspace(3)* address, float val)
+    #
+    # FIXME: these only works on sm_60+, but we can't verify that for now
     # declare double @llvm.nvvm.atomic.load.add.f64.p0f64(double* address, double val)
     # declare double @llvm.nvvm.atomic.load.add.f64.p1f64(double addrspace(1)* address, double val)
     # declare double @llvm.nvvm.atomic.load.add.f64.p3f64(double addrspace(3)* address, double val)
     for T in (Float32, Float64)
         nb = sizeof(T)*8
         intr = "llvm.nvvm.atomic.load.add.f$nb.p$(convert(Int, A))f$nb"
-        @eval @inline atomic_add!(ptr::DevicePtr{$T,$A}, val::$T) =
-            ccall($intr, llvmcall, $T, (DevicePtr{$T,$A}, $T), ptr, val)
+
+        if VERSION >= v"999" # FIXME: JuliaLang/julia#31624
+            @eval @inline atomic_add!(ptr::DevicePtr{$T,$A}, val::$T) =
+                ccall($intr, llvmcall, $T, (DevicePtr{$T,$A}, $T), ptr, val)
+        else
+            import Base.Sys: WORD_SIZE
+            if T == Float32
+                T_val = "float"
+            else
+                T_val = "double"
+            end
+            if A == AS.Generic
+                T_ptr = "$(T_val)*"
+            else
+                T_ptr = "$(T_val) addrspace($(convert(Int, A)))*"
+            end
+            @eval @inline atomic_add!(ptr::DevicePtr{$T,$A}, val::$T) = Base.llvmcall(
+                $("declare $T_val @$intr($T_ptr, $T_val)",
+                  "%ptr = inttoptr i$WORD_SIZE %0 to $T_ptr
+                   %rv = call $T_val @$intr($T_ptr %ptr, $T_val %1)
+                   ret $T_val %rv"), $T,
+                Tuple{DevicePtr{$T,$A}, $T}, ptr, val)
+        end
     end
 
     # declare i32 @llvm.nvvm.atomic.load.inc.32.p0i32(i32* address, i32 val)
@@ -101,9 +135,25 @@ for A in (AS.Generic, AS.Global, AS.Shared)
     for T in (Int32,), op in (:inc, :dec)
         nb = sizeof(T)*8
         intr = "llvm.nvvm.atomic.load.$op.$nb.p$(convert(Int, A))i$nb"
-
         fn = Symbol("atomic_$(op)!")
-        @eval @inline $fn(ptr::DevicePtr{$T,$A}, val::$T) =
-            ccall($intr, llvmcall, $T, (DevicePtr{$T,$A}, $T), ptr, val)
+
+        if VERSION >= v"999" # FIXME: JuliaLang/julia#31624
+            @eval @inline $fn(ptr::DevicePtr{$T,$A}, val::$T) =
+                ccall($intr, llvmcall, $T, (DevicePtr{$T,$A}, $T), ptr, val)
+        else
+            import Base.Sys: WORD_SIZE
+            T_val = "i32"
+            if A == AS.Generic
+                T_ptr = "$(T_val)*"
+            else
+                T_ptr = "$(T_val) addrspace($(convert(Int, A)))*"
+            end
+            @eval @inline $fn(ptr::DevicePtr{$T,$A}, val::$T) = Base.llvmcall(
+                $("declare $T_val @$intr($T_ptr, $T_val)",
+                  "%ptr = inttoptr i$WORD_SIZE %0 to $T_ptr
+                   %rv = call $T_val @$intr($T_ptr %ptr, $T_val %1)
+                   ret $T_val %rv"), $T,
+                Tuple{DevicePtr{$T,$A}, $T}, ptr, val)
+        end
     end
 end
