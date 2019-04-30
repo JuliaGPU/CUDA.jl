fb74333a762609dc6f931af4687bd21d45fd5d77
Tim Besard <tim.besard@gmail.com>
2019-04-30 16:57:11 +0200

    Wrap in testsets to prevent failure from halting the tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,29 +3,21 @@ using CUDAapi
 using Test
 
 
-## library types (Accessibility and `@enum` behaviour)
+@testset "library types" begin
+    @test CUDAapi.PATCH_LEVEL == CUDAapi.libraryPropertyType(2)
+    @test CUDAapi.C_32U == CUDAapi.cudaDataType(13)
+end
 
-@test CUDAapi.PATCH_LEVEL == CUDAapi.libraryPropertyType(2)
-@test CUDAapi.C_32U == CUDAapi.cudaDataType(13)
+@testset "properties" begin
+    @test !CUDAapi.gcc_supported(v"5.0", v"5.5")
+    @test CUDAapi.gcc_supported(v"5.0", v"8.0")
+    CUDAapi.devices_for_cuda(v"8.0")
+    CUDAapi.devices_for_llvm(v"5.0")
+    CUDAapi.isas_for_cuda(v"8.0")
+    CUDAapi.isas_for_llvm(v"5.0")
+end
 
-
-## properties
-
-@test !CUDAapi.gcc_supported(v"5.0", v"5.5")
-@test CUDAapi.gcc_supported(v"5.0", v"8.0")
-CUDAapi.devices_for_cuda(v"8.0")
-CUDAapi.devices_for_llvm(v"5.0")
-CUDAapi.isas_for_cuda(v"8.0")
-CUDAapi.isas_for_llvm(v"5.0")
-
-
-## discovery
-
-# generic
-find_binary([Sys.iswindows() ? "CHKDSK" : "true"])
-find_library([Sys.iswindows() ? "NTDLL" : "c"])
-
-# CUDA
+# helper macro to test for non-nothingness
 macro test_something(ex)
     quote
         rv = $(ex)
@@ -34,21 +26,29 @@ macro test_something(ex)
     end
 end
 
-dirs = find_toolkit()
-@test !isempty(dirs)
+@testset "discovery" begin
+    find_binary([Sys.iswindows() ? "CHKDSK" : "true"])
+    find_library([Sys.iswindows() ? "NTDLL" : "c"])
 
-if !isempty(dirs)
+    dirs = find_toolkit()
+    @test !isempty(dirs)
+
     ver = find_toolkit_version(dirs)
 
-    @test_something find_cuda_binary("nvcc", dirs)
-    @test_something find_cuda_library("cudart", dirs)
-    @test_something find_cuda_library("nvtx", dirs)
-    @test_something find_libdevice([v"3.0"], dirs)
-    @test_something find_libcudadevrt(dirs)
-    @test_something find_host_compiler()
-    @test_something find_host_compiler(ver)
-    @test_something find_toolchain(dirs)
-    @test_something find_toolchain(dirs, ver)
+    @testset "CUDA tools and libraries" begin
+        @test_something find_cuda_binary("nvcc", dirs)
+        @test_something find_cuda_library("cudart", dirs)
+        @test_something find_cuda_library("nvtx", dirs)
+        @test_something find_libdevice([v"3.0"], dirs)
+        @test_something find_libcudadevrt(dirs)
+        @test_something find_toolchain(dirs)
+        @test_something find_toolchain(dirs, ver)
+    end
+
+    @testset "host tools and libraries" begin
+        @test_something find_host_compiler()
+        @test_something find_host_compiler(ver)
+    end
 
     if haskey(ENV, "CI")
         # CI deals with plenty of CUDA versions, which makes discovery tricky.
