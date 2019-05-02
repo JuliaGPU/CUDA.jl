ace0bf971924816fb382f6889d0c8270085e69bf
Tim Besard <tim.besard@gmail.com>
2019-05-02 11:39:26 +0200

    Fix imports.

--- src/tensor/CUTENSOR.jl
+++ src/tensor/CUTENSOR.jl
@@ -1,17 +1,23 @@
 module CUTENSOR
 
-import CUDAdrv: CUDAdrv, CuContext, CuStream_t, CuPtr, PtrOrCuPtr, CU_NULL
 import CUDAapi
 
+import CUDAdrv: CUDAdrv, CuContext, CuStream_t, CuPtr, PtrOrCuPtr, CU_NULL
+
+using ..CuArrays
+using ..CuArrays: libcutensor, active_context
+
 using Libdl
-using CuArrays
 Libdl.dlopen(CuArrays.CUBLAS.libcublas, RTLD_NOW | RTLD_DEEPBIND | RTLD_GLOBAL)
 Libdl.dlopen(libcutensor, RTLD_LAZY | RTLD_DEEPBIND | RTLD_GLOBAL)
-using CuArrays: active_context
 
 using LinearAlgebra
+
+export CuTensor
+
 include("libcutensor_types.jl")
 include("error.jl")
+
 const _handles = Dict{CuContext,cutensorHandle_t}()
 const _handle = Ref{cutensorHandle_t}(C_NULL)
 
@@ -27,11 +33,10 @@ function handle()
     end
     return _handle[]
 end
+
 include("libcutensor.jl")
 include("highlevel.jl")
 include("wrappers.jl")
-
-export CuTensor
 
 version() = VersionNumber(cutensorGetProperty(CUDAapi.MAJOR_VERSION),
                           cutensorGetProperty(CUDAapi.MINOR_VERSION),
