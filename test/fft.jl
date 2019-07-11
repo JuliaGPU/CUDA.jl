6b92908e5037e5c54264e7066a066f42961acf64
Tim Besard <tim.besard@gmail.com>
2019-07-11 12:09:02 +0200

    Clean-up FFT tests.

--- test/fft.jl
+++ test/fft.jl
@@ -2,6 +2,8 @@
 
 using CuArrays.CUFFT
 
+using CUDAdrv
+
 using FFTW
 
 # notes:
@@ -15,8 +17,10 @@ N4 = 8
 MYRTOL = 1e-5
 MYATOL = 1e-8
 
-# out-of-place
-function dotest1(X::AbstractArray{T,N}) where {T <: Complex,N}
+
+## complex
+
+function out_of_place(X::AbstractArray{T,N}) where {T <: Complex,N}
     fftw_X = fft(X)
     d_X = CuArray(X)
     p = plan_fft(d_X)
@@ -35,7 +39,114 @@ function dotest1(X::AbstractArray{T,N}) where {T <: Complex,N}
     @test isapprox(Z, X, rtol = MYRTOL, atol = MYATOL)
 end
 
-function dotest1(X::AbstractArray{T,N}) where {T <: Real,N}
+function in_place(X::AbstractArray{T,N}) where {T <: Complex,N}
+    fftw_X = fft(X)
+    d_X = CuArray(X)
+    p = plan_fft!(d_X)
+    p * d_X
+    Y = collect(d_X)
+    @test isapprox(Y, fftw_X, rtol = MYRTOL, atol = MYATOL)
+
+    pinv = plan_ifft!(d_X)
+    pinv * d_X
+    Z = collect(d_X)
+    @test isapprox(Z, X, rtol = MYRTOL, atol = MYATOL)
+end
+
+function batched(X::AbstractArray{T,N},region) where {T <: Complex,N}
+    fftw_X = fft(X,region)
+    d_X = CuArray(X)
+    p = plan_fft(d_X,region)
+    d_Y = p * d_X
+    Y = collect(d_Y)
+    @test isapprox(Y, fftw_X, rtol = MYRTOL, atol = MYATOL)
+
+    pinv = plan_ifft(d_Y,region)
+    d_Z = pinv * d_Y
+    Z = collect(d_Z)
+    @test isapprox(Z, X, rtol = MYRTOL, atol = MYATOL)
+end
+
+@testset for T in [ComplexF32, ComplexF64]
+
+@testset "1D" begin
+    dims = (N1,)
+    X = rand(T, dims)
+    out_of_place(X)
+end
+@testset "1D inplace" begin
+    dims = (N1,)
+    X = rand(T, dims)
+    in_place(X)
+end
+
+@testset "2D" begin
+    dims = (N1,N2)
+    X = rand(T, dims)
+    out_of_place(X)
+end
+@testset "2D inplace" begin
+    dims = (N1,N2)
+    X = rand(T, dims)
+    in_place(X)
+end
+
+@testset "Batch 1D" begin
+    dims = (N1,N2)
+    X = rand(T, dims)
+    batched(X,1)
+
+    dims = (N1,N2)
+    X = rand(T, dims)
+    batched(X,2)
+
+    dims = (N1,N2)
+    X = rand(T, dims)
+    batched(X,(1,2))
+end
+
+@testset "3D" begin
+    dims = (N1,N2,N3)
+    X = rand(T, dims)
+    out_of_place(X)
+end
+
+@testset "3D inplace" begin
+    dims = (N1,N2,N3)
+    X = rand(T, dims)
+    in_place(X)
+end
+
+@testset "Batch 2D (in 3D)" begin
+    dims = (N1,N2,N3)
+    for region in [(1,2),(2,3),(1,3)]
+        X = rand(T, dims)
+        batched(X,region)
+    end
+
+    X = rand(T, dims)
+    @test_throws ArgumentError batched(X,(3,1))
+end
+
+@testset "Batch 2D (in 4D)" begin
+    dims = (N1,N2,N3,N4)
+    for region in [(1,2),(1,4),(3,4)]
+        X = rand(T, dims)
+        batched(X,region)
+    end
+    for region in [(1,3),(2,3),(2,4)]
+        X = rand(T, dims)
+        @test_throws ArgumentError batched(X,region)
+    end
+
+end
+
+end
+
+
+## real
+
+function out_of_place(X::AbstractArray{T,N}) where {T <: Real,N}
     fftw_X = rfft(X)
     d_X = CuArray(X)
     p = plan_rfft(d_X)
@@ -59,39 +170,7 @@ function dotest1(X::AbstractArray{T,N}) where {T <: Real,N}
     @test isapprox(W, Y, rtol = MYRTOL, atol = MYATOL)
 end
 
-# in-place
-function dotest2(X::AbstractArray{T,N}) where {T <: Complex,N}
-    fftw_X = fft(X)
-    d_X = CuArray(X)
-    p = plan_fft!(d_X)
-    p * d_X
-    Y = collect(d_X)
-    @test isapprox(Y, fftw_X, rtol = MYRTOL, atol = MYATOL)
-
-    pinv = plan_ifft!(d_X)
-    pinv * d_X
-    Z = collect(d_X)
-    @test isapprox(Z, X, rtol = MYRTOL, atol = MYATOL)
-end
-
-# no inplace rfft for now
-
-# batch transforms
-function dotest3(X::AbstractArray{T,N},region) where {T <: Complex,N}
-    fftw_X = fft(X,region)
-    d_X = CuArray(X)
-    p = plan_fft(d_X,region)
-    d_Y = p * d_X
-    Y = collect(d_Y)
-    @test isapprox(Y, fftw_X, rtol = MYRTOL, atol = MYATOL)
-
-    pinv = plan_ifft(d_Y,region)
-    d_Z = pinv * d_Y
-    Z = collect(d_Z)
-    @test isapprox(Z, X, rtol = MYRTOL, atol = MYATOL)
-end
-
-function dotest3(X::AbstractArray{T,N},region) where {T <: Real,N}
+function batched(X::AbstractArray{T,N},region) where {T <: Real,N}
     fftw_X = rfft(X,region)
     d_X = CuArray(X)
     p = plan_rfft(d_X,region)
@@ -105,174 +184,113 @@ function dotest3(X::AbstractArray{T,N},region) where {T <: Real,N}
     @test isapprox(Z, X, rtol = MYRTOL, atol = MYATOL)
 end
 
+@testset for T in [Float32, Float64]
 
-@testset "FFT" for (rtype,ctype) in [(Float32,ComplexF32), (Float64,ComplexF64)]
-
-@testset "1D FFT" begin
-    dims = (N1,)
-    X = rand(ctype, dims)
-    dotest1(X)
-end
-@testset "1D inplace FFT" begin
-    dims = (N1,)
-    X = rand(ctype, dims)
-    dotest2(X)
+@testset "1D" begin
+    X = rand(T, N1)
+    out_of_place(X)
 end
 
-@testset "2D FFT" begin
+@testset "Batch 1D" begin
     dims = (N1,N2)
-    X = rand(ctype, dims)
-    dotest1(X)
-end
-@testset "2D inplace FFT" begin
-    dims = (N1,N2)
-    X = rand(ctype, dims)
-    dotest2(X)
-end
-
-@testset "Batch 1D FFT" begin
-    dims = (N1,N2)
-    X = rand(ctype, dims)
-    dotest3(X,1)
+    X = rand(T, dims)
+    batched(X,1)
 
     dims = (N1,N2)
-    X = rand(ctype, dims)
-    dotest3(X,2)
+    X = rand(T, dims)
+    batched(X,2)
 
     dims = (N1,N2)
-    X = rand(ctype, dims)
-    dotest3(X,(1,2))
+    X = rand(T, dims)
+    batched(X,(1,2))
 end
 
-@testset "3D FFT" begin
-    dims = (N1,N2,N3)
-    X = rand(ctype, dims)
-    dotest1(X)
-end
-@testset "3D inplace FFT" begin
-    dims = (N1,N2,N3)
-    X = rand(ctype, dims)
-    dotest2(X)
+@testset "2D" begin
+    X = rand(T, N1,N2)
+    out_of_place(X)
 end
 
-@testset "Batch 2D FFT (in 3D)" begin
+@testset "Batch 2D (in 3D)" begin
     dims = (N1,N2,N3)
     for region in [(1,2),(2,3),(1,3)]
-        X = rand(ctype, dims)
-        dotest3(X,region)
+        X = rand(T, dims)
+        batched(X,region)
     end
 
-    X = rand(ctype, dims)
-    @test_throws ArgumentError dotest3(X,(3,1))
+    X = rand(T, dims)
+    @test_throws ArgumentError batched(X,(3,1))
 end
 
-@testset "Batch 2D FFT (in 4D)" begin
+@testset "Batch 2D (in 4D)" begin
     dims = (N1,N2,N3,N4)
     for region in [(1,2),(1,4),(3,4)]
-        X = rand(ctype, dims)
-        dotest3(X,region)
+        X = rand(T, dims)
+        batched(X,region)
     end
     for region in [(1,3),(2,3),(2,4)]
-        X = rand(ctype, dims)
-        @test_throws ArgumentError dotest3(X,region)
-    end
-
-end
-
-@testset "1D real FFT" begin
-    X = rand(rtype, N1)
-    dotest1(X)
-end
-
-@testset "Batch 1D real FFT" begin
-    dims = (N1,N2)
-    X = rand(rtype, dims)
-    dotest3(X,1)
-
-    dims = (N1,N2)
-    X = rand(rtype, dims)
-    dotest3(X,2)
-
-    dims = (N1,N2)
-    X = rand(rtype, dims)
-    dotest3(X,(1,2))
-end
-
-@testset "2D real FFT" begin
-    X = rand(rtype, N1,N2)
-    dotest1(X)
-end
-
-@testset "Batch 2D real FFT (in 3D)" begin
-    dims = (N1,N2,N3)
-    for region in [(1,2),(2,3),(1,3)]
-        X = rand(rtype, dims)
-        dotest3(X,region)
-    end
-
-    X = rand(rtype, dims)
-    @test_throws ArgumentError dotest3(X,(3,1))
-end
-
-@testset "Batch 2D real FFT (in 4D)" begin
-    dims = (N1,N2,N3,N4)
-    for region in [(1,2),(1,4),(3,4)]
-        X = rand(rtype, dims)
-        dotest3(X,region)
-    end
-    for region in [(1,3),(2,3),(2,4)]
-        X = rand(rtype, dims)
-        @test_throws ArgumentError dotest3(X,region)
+        X = rand(T, dims)
+        @test_throws ArgumentError batched(X,region)
     end
 end
 
-@testset "3D real FFT" begin
-    X = rand(rtype, N1, N2, N3)
-    dotest1(X)
+@testset "3D" begin
+    X = rand(T, N1, N2, N3)
+    out_of_place(X)
 end
 
-end # testset FFT
+end
 
-# integer array arguments
-function dotest5(X::AbstractArray{T,N}) where {T <: Complex,N}
+
+## complex integer
+
+function out_of_place(X::AbstractArray{T,N}) where {T <: Complex{<:Integer},N}
     fftw_X = fft(X)
     d_X = CuArray(X)
     p = plan_fft(d_X)
     d_Y = p * d_X
     Y = collect(d_Y)
     @test isapprox(Y, fftw_X, rtol = MYRTOL, atol = MYATOL)
+
     d_Y = fft(d_X)
     Y = collect(d_Y)
     @test isapprox(Y, fftw_X, rtol = MYRTOL, atol = MYATOL)
 end
 
-function dotest5(X::AbstractArray{T,N}) where {T <: Real,N}
+@testset for T in [Complex{Int32}, Complex{Int64}]
+
+@testset "1D" begin
+    dims = (N1,)
+    X = rand(T, dims)
+    out_of_place(X)
+end
+
+end
+
+
+## real integer
+
+function out_of_place(X::AbstractArray{T,N}) where {T <: Integer,N}
     fftw_X = rfft(X)
     d_X = CuArray(X)
     p = plan_rfft(d_X)
     d_Y = p * d_X
     Y = collect(d_Y)
     @test isapprox(Y, fftw_X, rtol = MYRTOL, atol = MYATOL)
+
     d_Y = rfft(d_X)
     Y = collect(d_Y)
     @test isapprox(Y, fftw_X, rtol = MYRTOL, atol = MYATOL)
 end
 
-@testset "Int FFT" for (rtype,ctype) in [(Int32,Complex{Int32}), (Int64,Complex{Int64})]
+@testset for T in [Int32, Int64]
 
-@testset "1D FFT" begin
-    dims = (N1,)
-    X = rand(ctype, dims)
-    dotest5(X)
+@testset "1D" begin
+    X = rand(T, N1)
+    out_of_place(X)
 end
 
-@testset "1D real FFT" begin
-    X = rand(rtype, N1)
-    dotest5(X)
 end
 
-
-end # testset int FFT
 
 @testset "streams" begin
     X = rand(N1)
@@ -280,5 +298,6 @@ end # testset int FFT
     p = plan_fft(d_X)
     CUFFT.set_stream(p, CUDAdrv.CuDefaultStream())
 end
+
 
 end
