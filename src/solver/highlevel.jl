a82e1c4cdd157b251d8ffe8e2923a4dce79ea8a6
Tim Besard <tim.besard@gmail.com>
2019-09-27 10:07:07 +0200

    Use CuArrays.zeros instead of broadcasting.

--- src/solver/highlevel.jl
+++ src/solver/highlevel.jl
@@ -54,7 +54,7 @@ LinearAlgebra.lmul!(trA::Transpose{T,<:CuQRPackedQ{T,S}}, B::CuVecOrMat{T}) wher
     ormqr!('L', 'T', parent(trA).factors, parent(trA).τ, B)
 
 function Base.getindex(A::CuQRPackedQ{T, S}, i::Integer, j::Integer) where {T, S}
-    x = CuArray{T}(undef, size(A, 2)) .= 0
+    x = CuArrays.zeros(T, size(A, 2))
     x[j] = 1
     lmul!(A, x)
     return _getindex(x, i)
