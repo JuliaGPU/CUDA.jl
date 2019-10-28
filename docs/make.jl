ddea32998fc2a078f232ac9443a01b98e134a6a5
Tim Besard <tim.besard@gmail.com>
2019-10-28 10:14:46 +0100

    Fix repo root.

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,26 @@ using Documenter, Literate
 using CUDAapi, CUDAdrv, CUDAnative, CuArrays
 
 @info "Building Literate.jl documentation"
+function set_repo_root(content)
+    if haskey(ENV, "GITLAB_CI")
+        commit = ENV["CI_COMMIT_REF_NAME"]
+        repo_root_url = "https://github.com/JuliaGPU/CUDA.jl/blob/$(commit)"
+        return replace(content, "@__REPO_ROOT_URL__" => repo_root_url)
+    else
+        return content
+    end
+end
 cd(joinpath(@__DIR__, "src")) do
-    Literate.markdown("tutorials/introduction.jl", "tutorials")
+    withenv("TRAVIS_REPO_SLUG" => "JuliaGPU/CUDA.jl") do
+        Literate.markdown("tutorials/introduction.jl", "tutorials"; postprocess=set_repo_root)
+    end
 end
 
 @info "Generating Documenter.jl site"
 makedocs(
     sitename = "CUDA.jl",
     authors = "Tim Besard",
+    repo = "https://github.com/JuliaGPU/CUDA.jl/blob/{commit}{path}#{line}",
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true"
     ),
