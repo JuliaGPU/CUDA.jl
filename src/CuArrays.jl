8279318eadac63cd2ae55ea53774bdaa8cc73b87
Tim Besard <tim.besard@gmail.com>
2019-10-30 09:32:54 +0100

    Export submodules.

--- src/CuArrays.jl
+++ src/CuArrays.jl
@@ -5,6 +5,7 @@ using CUDAapi, CUDAdrv, CUDAnative
 using GPUArrays
 
 export CuArray, CuVector, CuMatrix, CuVecOrMat, cu
+export CUBLAS, CUSPARSE, CUSOLVER, CUFFT, CURAND, CUDNN, CUTENSOR
 
 import LinearAlgebra
 
