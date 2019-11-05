c46e8f44fbebe8fe62e8d474e341268f6cb14064
Jarrett Revels <jarrettrevels@gmail.com>
2019-11-05 16:08:16 -0500

    make CuIterator mutable instead of immutable with Ref fields

--- src/iterator.jl
+++ src/iterator.jl
@@ -1,30 +1,29 @@
-struct CuIterator{B}
+mutable struct CuIterator{B}
     batches::B
-    initial_pool_size::Ref{Int}
-    pool::Ref{Mem.DeviceBuffer}
-end
-
-function CuIterator(batches, initial_pool_size::Integer=0)
-    return CuIterator(batches, Ref(initial_pool_size), Ref{Mem.DeviceBuffer}())
+    initial_pool_size::Int
+    pool::Mem.DeviceBuffer
+    function CuIterator(batches, initial_pool_size=0)
+        return new{typeof(batches)}(batches, initial_pool_size)
+    end
 end
 
 function Base.iterate(c::CuIterator, state...)
     item = iterate(c.batches, state...)
     if item === nothing
-        Mem.free(c.pool[])
+        isdefined(c, :pool) && Mem.free(c.pool)
         return nothing
     end
     batch, next_state = item
     required_pool_size = sum(sizeof, batch)
     if isempty(state)
-        c.initial_pool_size[] = max(required_pool_size, c.initial_pool_size[])
-        c.pool[] = Mem.alloc(Mem.DeviceBuffer, c.initial_pool_size[])
-    elseif required_pool_size > sizeof(c.pool[])
-        Mem.free(c.pool[])
-        c.initial_pool_size[] = required_pool_size
-        c.pool[] = Mem.alloc(Mem.DeviceBuffer, required_pool_size)
+        c.initial_pool_size = max(required_pool_size, c.initial_pool_size)
+        c.pool = Mem.alloc(Mem.DeviceBuffer, c.initial_pool_size)
+    elseif required_pool_size > sizeof(c.pool)
+        Mem.free(c.pool)
+        c.initial_pool_size = required_pool_size
+        c.pool = Mem.alloc(Mem.DeviceBuffer, required_pool_size)
     end
-    pool = c.pool[]
+    pool = c.pool
     offset = 0
     cubatch = map(batch) do array
         @assert array isa AbstractArray
