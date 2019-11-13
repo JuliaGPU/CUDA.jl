231059ade0c4a3813b2282a88c3e1147dbd59113
Tim Besard <tim.besard@gmail.com>
2019-11-13 09:57:35 +0100

    Print a message if the package fails to load.

--- src/CuArrays.jl
+++ src/CuArrays.jl
@@ -60,85 +60,91 @@ has_cudnn() = libraries["cudnn"] !== nothing && CUDNN.libcudnn !== nothing
 has_cutensor() = libraries["cutensor"] !== nothing && CUTENSOR.libcutensor !== nothing
 
 function __init__()
-    # discover libraries
-    toolkit = find_toolkit()
-    for name in ("cublas", "cusparse", "cusolver", "cufft", "curand", "cudnn", "cutensor")
-        mod = getfield(CuArrays, Symbol(uppercase(name)))
-        lib = Symbol("lib$name")
-        path = find_cuda_library(name, toolkit)
-        libraries[name] = path
-        if path !== nothing
-            dir = dirname(path)
-            if !(dir in Libdl.DL_LOAD_PATH)
-                push!(Libdl.DL_LOAD_PATH, dir)
+    silent = parse(Bool, get(ENV, "JULIA_CUDA_SILENT", "false"))
+    verbose = parse(Bool, get(ENV, "JULIA_CUDA_VERBOSE", "false"))
+    precompiling = ccall(:jl_generating_output, Cint, ()) != 0
+
+    # if any dependent GPU package failed, expect it to have logged an error and bail out
+    if !CUDAdrv.functional() || !CUDAnative.functional()
+        verbose && @warn "CuArrays.jl did not initialize because CUDAdrv.jl or CUDAnative.jl failed to"
+        return
+    end
+
+    # if any dependent GPU package failed, expect it to have logged an error and bail out
+    CUDAdrv.functional() || return
+    CUDAnative.functional() || return
+
+    precompiling = ccall(:jl_generating_output, Cint, ()) != 0
+    try
+        # discover libraries
+        toolkit = find_toolkit()
+        for name in ("cublas", "cusparse", "cusolver", "cufft", "curand", "cudnn", "cutensor")
+            mod = getfield(CuArrays, Symbol(uppercase(name)))
+            lib = Symbol("lib$name")
+            path = find_cuda_library(name, toolkit)
+            libraries[name] = path
+            if path !== nothing
+                dir = dirname(path)
+                if !(dir in Libdl.DL_LOAD_PATH)
+                    push!(Libdl.DL_LOAD_PATH, dir)
+                end
+            end
+        end
+
+        # library dependencies
+        CUBLAS.version()
+        CUSPARSE.version()
+        CUSOLVER.version()
+        CUFFT.version()
+        CURAND.version()
+        # CUDNN and CUTENSOR are optional
+
+        # library compatibility
+        if has_cutensor()
+            ver = CUTENSOR.version()
+            if ver.major != 0 || ver.minor != 2
+                error("CuArrays.jl only supports CUTENSOR 0.2")
+            end
+        end
+
+        # package integrations
+        @require ForwardDiff="f6369f11-7733-5829-9624-2563aa707210" include("forwarddiff.jl")
+
+        # update the active context when we switch devices
+        callback = (::CuDevice, ctx::CuContext) -> begin
+            active_context[] = ctx
+
+            # wipe the active handles
+            CUBLAS._handle[] = C_NULL
+            CUBLAS._xt_handle[] = C_NULL
+            CUSOLVER._dense_handle[] = C_NULL
+            CUSOLVER._sparse_handle[] = C_NULL
+            CUSPARSE._handle[] = C_NULL
+            CURAND._generator[] = nothing
+            CUDNN._handle[] = C_NULL
+            CUTENSOR._handle[] = C_NULL
+        end
+        push!(CUDAnative.device!_listeners, callback)
+
+        # a device might be active already
+        existing_ctx = CUDAdrv.CuCurrentContext()
+        if existing_ctx !== nothing
+            active_context[] = existing_ctx
+        end
+
+        __init_memory__()
+    catch ex
+        # don't actually fail to keep the package loadable
+        silent = parse(Bool, get(ENV, "JULIA_CUDA_SILENT", "false"))
+        if !silent && !precompiling
+            verbose = parse(Bool, get(ENV, "JULIA_CUDA_VERBOSE", "false"))
+            if verbose
+                @error "CuArrays.jl failed to initialize" exception=(ex, catch_backtrace())
+            else
+                @info "CuArrays.jl failed to initialized and will be unavailable (set JULIA_CUDA_SILENT or JULIA_CUDA_VERBOSE to silence or expand this message)"
             end
         end
     end
-
-    try
-        # barrier to avoid compiling `ccall`s to unavailable libraries
-        inferencebarrier(__hidden_init__)()
-        __initialized__[] = true
-    catch ex
-        # don't actually fail to keep the package loadable
-        @debug("CuArrays.jl failed to initialize; the package will not be functional.",
-               exception=(ex, catch_backtrace()))
-    end
-end
-
-if VERSION >= v"1.3.0-DEV.35"
-    using Base: inferencebarrier
-else
-    inferencebarrier(@nospecialize(x)) = Ref{Any}(x)[]
-end
-
-function __hidden_init__()
-    # package dependencies
-    CUDAdrv.functional() || error("CUDAdrv.jl is not functional")
-    CUDAnative.functional() || error("CUDAnative.jl is not functional")
-
-    # library dependencies
-    CUBLAS.version()
-    CUSPARSE.version()
-    CUSOLVER.version()
-    CUFFT.version()
-    CURAND.version()
-    # CUDNN and CUTENSOR are optional
-
-    # library compatibility
-    if has_cutensor()
-        ver = inferencebarrier(CUTENSOR.version)()
-        if ver.major != 0 || ver.minor != 2
-            error("CuArrays.jl only supports CUTENSOR 0.2")
-        end
-    end
-
-    # package integrations
-    @require ForwardDiff="f6369f11-7733-5829-9624-2563aa707210" include("forwarddiff.jl")
-
-    # update the active context when we switch devices
-    callback = (::CuDevice, ctx::CuContext) -> begin
-        active_context[] = ctx
-
-        # wipe the active handles
-        CUBLAS._handle[] = C_NULL
-        CUBLAS._xt_handle[] = C_NULL
-        CUSOLVER._dense_handle[] = C_NULL
-        CUSOLVER._sparse_handle[] = C_NULL
-        CUSPARSE._handle[] = C_NULL
-        CURAND._generator[] = nothing
-        CUDNN._handle[] = C_NULL
-        CUTENSOR._handle[] = C_NULL
-    end
-    push!(CUDAnative.device!_listeners, callback)
-
-    # a device might be active already
-    existing_ctx = CUDAdrv.CuCurrentContext()
-    if existing_ctx !== nothing
-        active_context[] = existing_ctx
-    end
-
-    __init_memory__()
 end
 
 end # module
