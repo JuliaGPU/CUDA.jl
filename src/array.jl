fea9d56ef21b9c72496a61c55a6b0b5cadfd6f7a
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2019-11-29 10:05:44 +0800

    Allow cu to operate on Array types

--- src/array.jl
+++ src/array.jl
@@ -286,6 +286,7 @@ end
 ## utilities
 
 cu(xs) = adapt(CuArray{Float32}, xs)
+cu(::Type{Array{T,N}}) where {T,N} = CuArray{T,N}
 Base.getindex(::typeof(cu), xs...) = CuArray([xs...])
 
 zeros(T::Type, dims...) = fill!(CuArray{T}(undef, dims...), 0)
