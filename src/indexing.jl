c80f1de462cfd01cf54ab2603de576c4915305b8
Tim Besard <tim.besard@gmail.com>
2020-01-29 10:16:12 +0100

    Remove cat functionality already covered by GPUArrays.

--- src/indexing.jl
+++ src/indexing.jl
@@ -25,50 +25,7 @@ macro cuindex(A)
   end
 end
 
-@generated function nindex(i::T, ls::NTuple{N,T}) where {N,T}
-  na = one(i)
-  quote
-    Base.@_inline_meta
-    $(foldr((n, els) -> :(i ≤ ls[$n] ? ($n, i) : (i -= ls[$n]; $els)), :($na, $na), one(i):i(N)))
-  end
-end
-
-@inline function catindex(dim, I::NTuple{N}, shapes) where N
-  @inbounds x, i = nindex(I[dim], getindex.(shapes, dim))
-  x, ntuple(n -> n == dim ? i : I[n], Val{N})
-end
 # COV_EXCL_STOP
-
-function growdims(dim, x)
-  if ndims(x) >= dim
-    x
-  else
-    reshape(x, size.((x,), 1:dim)...)
-  end
-end
-
-function _cat(dim, dest, xs...)
-  function kernel(dim, dest, xs)
-    I = @cuindex dest
-    @inbounds n, I′ = catindex(dim, Int.(I), size.(xs))
-    @inbounds dest[I...] = xs[n][I′...]
-    return
-  end
-  xs = growdims.(dim, xs)
-  blk, thr = cudims(dest)
-  @cuda blocks=blk threads=thr kernel(dim, dest, xs)
-  return dest
-end
-
-function Base.cat_t(dims::Integer, T::Type, x::CuArray, xs::CuArray...)
-  catdims = Base.dims2cat(dims)
-  shape = Base.cat_shape(catdims, (), size.((x, xs...))...)
-  dest = Base.cat_similar(x, T, shape)
-  _cat(dims, dest, x, xs...)
-end
-
-Base.vcat(xs::CuArray...) = cat(xs..., dims=1)
-Base.hcat(xs::CuArray...) = cat(xs..., dims=2)
 
 
 ## non-asserting indexing
