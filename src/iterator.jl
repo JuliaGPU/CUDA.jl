b75367bef24032fc8debcc4c7c28c44a6e18868e
Jarrett Revels <jarrettrevels@gmail.com>
2020-02-26 13:59:00 -0500

    simpler implementation that reuses existing CuArrays memory pool

--- src/iterator.jl
+++ src/iterator.jl
@@ -1,37 +1,15 @@
 mutable struct CuIterator{B}
     batches::B
-    initial_pool_size::Int
-    pool::Mem.DeviceBuffer
-    function CuIterator(batches, initial_pool_size=0)
-        return new{typeof(batches)}(batches, initial_pool_size)
-    end
+    previous::Any
+    CuIterator(batches) = new{typeof(batches)}(batches)
 end
 
 function Base.iterate(c::CuIterator, state...)
     item = iterate(c.batches, state...)
-    if item === nothing
-        isdefined(c, :pool) && Mem.free(c.pool)
-        return nothing
-    end
+    isdefined(c, :previous) && foreach(unsafe_free!, c.previous)
+    item === nothing && return nothing
     batch, next_state = item
-    required_pool_size = sum(sizeof, batch)
-    if isempty(state)
-        c.initial_pool_size = max(required_pool_size, c.initial_pool_size)
-        c.pool = Mem.alloc(Mem.DeviceBuffer, c.initial_pool_size)
-    elseif required_pool_size > sizeof(c.pool)
-        Mem.free(c.pool)
-        c.initial_pool_size = required_pool_size
-        c.pool = Mem.alloc(Mem.DeviceBuffer, required_pool_size)
-    end
-    pool = c.pool
-    offset = 0
-    cubatch = map(batch) do array
-        @assert array isa AbstractArray
-        ptr = Base.unsafe_convert(CuPtr{eltype(array)}, pool.ptr + offset)
-        cuarray = unsafe_wrap(CuArray, ptr, size(array); own=false);
-        copyto!(cuarray, array)
-        offset += sizeof(array)
-        return cuarray
-    end
+    cubatch = map(x -> adapt(CuArray, x), batch)
+    c.previous = cubatch
     return cubatch, next_state
 end
