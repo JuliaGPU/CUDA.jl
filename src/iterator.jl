3e43c05c2d1811286b2addcf5c5ce9219c54fd8e
Jarrett Revels <jarrettrevels@gmail.com>
2020-02-26 21:36:20 -0500

    switch to simpler implementation + docs

--- src/iterator.jl
+++ src/iterator.jl
@@ -1,3 +1,15 @@
+"""
+    CuIterator(batches)
+
+Return a `CuIterator` that can iterate through the provided `batches` via `Base.iterate`.
+
+Upon each iteration, the current `batch` is adapted to the GPU (via `map(x -> adapt(CuArray, x), batch)`)
+and the previous iteration is marked as freeable from GPU memory (via `unsafe_free!`).
+
+This abstraction is useful for batching data into GPU memory in a manner that
+allows old iterations to potentially be freed (or marked as reusable) earlier
+than they otherwise would via CuArray's internal polling mechanism.
+"""
 mutable struct CuIterator{B}
     batches::B
     previous::Any
