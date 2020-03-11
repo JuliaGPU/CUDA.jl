259ed9f5907e7afe00bf835f9e571433c3c6434f
Jarrett Revels <jarrettrevels@gmail.com>
2020-03-11 12:48:04 -0400

    fix test

--- test/iterator.jl
+++ test/iterator.jl
@@ -4,12 +4,12 @@
     max_ndims = 3
     sizes = 20:50
     rand_shape = () -> rand(sizes, rand(1:max_ndims))
-    batches = [[rand(Float32, rand_shape()...) for _ in rand(1:max_batch_items)] for _ in 1:batch_count]
+    batches = [[rand(Float32, rand_shape()...) for _ in 1:rand(1:max_batch_items)] for _ in 1:batch_count]
     cubatches = CuIterator(batch for batch in batches) # ensure generators are accepted
     previous_cubatch = missing
     for (batch, cubatch) in zip(batches, cubatches)
         @test ismissing(previous_cubatch) || all(x -> x.freed, previous_cubatch)
-        @test batch == cubatch
+        @test batch == Array.(cubatch)
         @test all(x -> x isa CuArray, cubatch)
         previous_cubatch = cubatch
     end
