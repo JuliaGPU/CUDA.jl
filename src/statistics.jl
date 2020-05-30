845788bb7137f89504a886a88bd38798fea7bb93
Tim Besard <tim.besard@gmail.com>
2020-05-30 17:32:09 +0200

    Fix Statistics.mean.

--- src/statistics.jl
+++ src/statistics.jl
@@ -11,3 +11,9 @@ Statistics._std(A::CuArray, corrected::Bool, mean, dims) =
 
 Statistics._std(A::CuArray, corrected::Bool, mean, ::Colon) =
     Base.sqrt.(Statistics.var(A; corrected=corrected, mean=mean, dims=:))
+
+# Revert https://github.com/JuliaLang/Statistics.jl/pull/25
+Statistics._mean(A::CuArray, ::Colon)    = sum(A) / length(A)
+Statistics._mean(f, A::CuArray, ::Colon) = sum(f, A) / length(A)
+Statistics._mean(A::CuArray, dims)    = mean!(Base.reducedim_init(t -> t/2, +, A, dims), A)
+Statistics._mean(f, A::CuArray, dims) = sum(f, A, dims=dims) / mapreduce(i -> size(A, i), *, unique(dims); init=1)
--- test/statistics.jl
+++ test/statistics.jl
@@ -3,12 +3,20 @@ using Statistics
 @testset "std" begin
     @test testf(std, rand(10))
     @test testf(std, rand(10,1,2))
-    @test testf(std, rand(10,1,2), corrected=true)
-    @test testf(std, rand(10,1,2), dims=1)
+    @test testf(std, rand(10,1,2); corrected=true)
+    @test testf(std, rand(10,1,2); dims=1)
 end
+
 @testset "var" begin
     @test testf(var, rand(10))
     @test testf(var, rand(10,1,2))
-    @test testf(var, rand(10,1,2), corrected=true)
-    @test testf(var, rand(10,1,2), dims=1)
+    @test testf(var, rand(10,1,2); corrected=true)
+    @test testf(var, rand(10,1,2); dims=1)
+end
+
+@testset "mean" begin
+    @test testf(mean, rand(2,2))
+    @test testf(mean, rand(2,2); dims=2)
+    @test testf(x->mean(sin, x), rand(2,2))
+    @test testf(x->mean(sin, x; dims=2), rand(2,2))
 end
