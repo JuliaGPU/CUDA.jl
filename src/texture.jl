77be342cc878fee585a39ff1583e01ca1601c4aa
Tim Besard <tim.besard@gmail.com>
2020-06-08 18:53:34 +0200

    Rework indexing and constructors to align more with CUDA.
    Requires users to reinterpret arrays if, e.g., working with ColorTypes.

--- src/texture.jl
+++ src/texture.jl
@@ -2,72 +2,26 @@
 
 export alias_type
 
-const _type_to_cuarrayformat_dict = Dict{DataType,CUarray_format}(
-    UInt8   => CU_AD_FORMAT_UNSIGNED_INT8,
-    UInt16  => CU_AD_FORMAT_UNSIGNED_INT16,
-    UInt32  => CU_AD_FORMAT_UNSIGNED_INT32,
-    Int8    => CU_AD_FORMAT_SIGNED_INT8,
-    Int16   => CU_AD_FORMAT_SIGNED_INT16,
-    Int32   => CU_AD_FORMAT_SIGNED_INT32,
-    Float16 => CU_AD_FORMAT_HALF,
-    Float32 => CU_AD_FORMAT_FLOAT,
-)
-
-@inline _alias_type_to_nchan_and_format(::Type{T}) where {T} =
-    error("Type `$T` is not a valid alias type for a \"CUDA array\" (texture memory) format")
-
-for (T, cuarrayformat) in _type_to_cuarrayformat_dict
-    @eval @inline _alias_type_to_nchan_and_format(::Type{$T})  = 1, $cuarrayformat, $T
-    @eval @inline _alias_type_to_nchan_and_format(::Type{NTuple{2,$T}}) = 2, $cuarrayformat, $T
-    @eval @inline _alias_type_to_nchan_and_format(::Type{NTuple{4,$T}}) = 4, $cuarrayformat, $T
-end
-
-@inline _assert_alias_size(::Type{T}, ::Type{Ta}) where {T,Ta} =
-    sizeof(T) == sizeof(Ta) || throw(DimensionMismatch("Julia type `$T` cannot be aliased to the \"CUDA array\" (texture memory) format `$Ta`: sizes in bytes do not match"))
-
-for (T, _) in _type_to_cuarrayformat_dict
-    @eval @inline alias_type(::Type{$T}) = $T
-    for N in (2, 4)
-        @eval @inline alias_type(::Type{NT}) where {NT <: NTuple{$N,$T}} = NT
+function Base.convert(::Type{CUarray_format}, T::Type)
+    if T === UInt8
+        return CU_AD_FORMAT_UNSIGNED_INT8
+    elseif T === UInt16
+        return CU_AD_FORMAT_UNSIGNED_INT16
+    elseif T === UInt32
+        return CU_AD_FORMAT_UNSIGNED_INT32
+    elseif T === Int8
+        return CU_AD_FORMAT_SIGNED_INT8
+    elseif T === Int16
+        return CU_AD_FORMAT_SIGNED_INT16
+    elseif T === Int32
+        return CU_AD_FORMAT_SIGNED_INT32
+    elseif T === Float16
+        return CU_AD_FORMAT_HALF
+    elseif T === Float32
+        return CU_AD_FORMAT_FLOAT
+    else
+        throw(ArgumentError("CUDA does not support texture arrays for element type $T."))
     end
-end
-
-function alias_type(T::Type)
-    err = "An alias from the type `$T` to a \"CUDA array\" (texture memory) format could not be inferred"
-    isprimitivetype(T) && error("Primitive type `$T` does not have a defined alias to a \"CUDA array\" (texture memory) format.")
-    isbitstype(T) || error("$err: Type is not `isbitstype`.")
-    Te = nothing
-    N = 0
-    datatypes = DataType[T]
-    while !isempty(datatypes)
-        for Ti in fieldtypes(pop!(datatypes))
-            if isprimitivetype(Ti)
-                Ti = alias_type(Ti)
-                typeof(Ti) == DataType ||
-                    error("$err: Composed of primitive type with no alias.")
-                if !isprimitivetype(Ti)
-                    push!(datatypes, Ti)
-                    break
-                end
-                if Te == nothing
-                    Te = Ti
-                    N = 1
-                elseif Te == Ti
-                    N += 1
-                else
-                    error("$err: Incompatible elements.")
-                end
-            else
-                push!(datatypes, Ti)
-            end
-        end
-    end
-    Ta = NTuple{N,Te}
-    sizeof(T) == sizeof(Ta) ||
-        error("$err: Inferred alias type and original type have different byte sizes.")
-    in(N, (1, 2, 4)) ||
-        error("$err: Incompatible number of elements ($N, but only 1, 2 or 4 supported).")
-    return Ta
 end
 
 
@@ -76,22 +30,20 @@ end
 export CuTextureArray
 
 """
-    CuTextureArray{T,N}
+    CuTextureArray{T,N,C}
 
 Type to handle CUDA arrays which are opaque device memory buffers optimized for texture
 fetching. The only way to initialize the content of this objects is by copying from host or
 device arrays using the constructor or `copyto!` calls.
 """
-mutable struct CuTextureArray{T,N}
+mutable struct CuTextureArray{T,N,C}
     handle::CUarray
     dims::Dims{N}
 
     ctx::CuContext
 
-    function CuTextureArray{T,N}(dims::Dims{N}) where {T,N}
-        Ta = alias_type(T)
-        _assert_alias_size(T, Ta)
-        nchan, format = _alias_type_to_nchan_and_format(Ta)
+    function CuTextureArray{T,N,C}(::UndefInitializer, dims::Dims{N}) where {T,N,C}
+        format = convert(CUarray_format, T)
 
         if N == 2
             width, height = dims
@@ -122,13 +74,13 @@ mutable struct CuTextureArray{T,N}
             height, # Height::Csize_t
             depth, # Depth::Csize_t
             format, # Format::CUarray_format
-            UInt32(nchan), # NumChannels::UInt32
+            UInt32(C), # NumChannels::UInt32
             0))
 
         handle_ref = Ref{CUarray}()
         cuArray3DCreate(handle_ref, allocateArray_ref)
 
-        t = new{T,N}(handle_ref[], dims, context())
+        t = new{T,N,C}(handle_ref[], dims, context())
         finalizer(unsafe_destroy!, t)
         return t
     end
@@ -142,31 +94,62 @@ end
 
 Base.unsafe_convert(::Type{CUarray}, t::CuTextureArray) = t.handle
 
-CuTextureArray{T}(n::Int) where {T} = CuTextureArray{T,1}((n,))
-CuTextureArray{T}(nx::Int, ny::Int) where {T} = CuTextureArray{T,2}((nx, ny))
-CuTextureArray{T}(nx::Int, ny::Int, nz::Int) where {T} = CuTextureArray{T,3}((nx, ny, nz))
 
-Base.eltype(tm::CuTextureArray{T,N}) where {T,N} = T
+## array interface
+
+Base.eltype(tm::CuTextureArray{T,N,1}) where {T,N} = T
+Base.eltype(tm::CuTextureArray{T,N,C}) where {T,N,C} = NTuple{C,T}
+
+Base.elsize(tm::CuTextureArray) = sizeof(eltype(tm))
 Base.size(tm::CuTextureArray) = tm.dims
 Base.length(tm::CuTextureArray) = prod(size(tm))
 Base.sizeof(tm::CuTextureArray) = sizeof(eltype(tm)) * length(tm)
 
 
-### Memory transfer
+## interop with other arrays
 
-function Base.copyto!(dst::CuTextureArray{T,1}, src::Array{T,1}) where {T}
+packed_type(T,C) = C==1 ? T : NTuple{C,T}
+
+@inline function CuTextureArray{T,N,C}(xs::AbstractArray{<:Any,N}) where {T,N,C}
+  A = CuTextureArray{T,N,C}(undef, size(xs))
+  copyto!(A, convert(Array{packed_type(T,C)}, xs))
+  return A
+end
+
+@inline function CuTextureArray{T,N,C}(xs::CuArray{<:Any,N}) where {T,N,C}
+  A = CuTextureArray{T,N,C}(undef, size(xs))
+  copyto!(A, convert(CuArray{packed_type(T,C)}, xs))
+  return A
+end
+
+# idempotency
+CuTextureArray{T,N,C}(xs::CuTextureArray{T,N,C}) where {T,N,C} = xs
+
+CuTextureArray(A::AbstractArray{T,N}) where {T,N} = CuTextureArray{T,N,1}(A)
+CuTextureArray(A::AbstractArray{NTuple{C,T},N}) where {T,N,C} = CuTextureArray{T,N,C}(A)
+
+
+## memory operations
+
+function Base.copyto!(dst::CuTextureArray{T,1,C}, src::Array{<:Any,1}) where {T,C}
+    eltype(src) === packed_type(T,C) ||
+        throw(ArgumentError("Invalid source element type for $C-channel $T: expected $(packed_type(T,C)), got $(eltype(src))"))
     size(dst) == size(src) || throw(DimensionMismatch("source and destination sizes must match"))
     cuMemcpyHtoA(dst, 0, src, sizeof(dst))
     return dst
 end
 
-function Base.copyto!(dst::CuTextureArray{T,1}, src::CuArray{T,1}) where {T}
+function Base.copyto!(dst::CuTextureArray{T,1,C}, src::CuArray{<:Any,1}) where {T,C}
+    eltype(src) === packed_type(T,C) ||
+        throw(ArgumentError("Invalid source element type for $C-channel $T: expected $(packed_type(T,C)), got $(eltype(src))"))
     size(dst) == size(src) || throw(DimensionMismatch("source and destination sizes must match"))
     cuMemcpyDtoA(dst, 0, src, sizeof(dst))
     return dst
 end
 
-function Base.copyto!(dst::CuTextureArray{T,2}, src::Union{Array{T,2}, CuArray{T,2}}) where {T}
+function Base.copyto!(dst::CuTextureArray{T,2,C}, src::Union{Array{<:Any,2}, CuArray{<:Any,2}}) where {T,C}
+    eltype(src) === packed_type(T,C) ||
+        throw(ArgumentError("Invalid source element type for $C-channel $T: expected $(packed_type(T,C)), got $(eltype(src))"))
     size(dst) == size(src) || throw(DimensionMismatch("source and destination sizes must match"))
     isdevmem = isa(src, CuArray)
     copy_ref = Ref(CUDA_MEMCPY2D(
@@ -184,14 +167,16 @@ function Base.copyto!(dst::CuTextureArray{T,2}, src::Union{Array{T,2}, CuArray{T
         0, # dstDevice::CUdeviceptr
         dst.handle, # dstArray::CUarray
         0, # dstPitch::Csize_t
-        dst.dims[1] * sizeof(T), # WidthInBytes::Csize_t
+        dst.dims[1] * sizeof(eltype(dst)), # WidthInBytes::Csize_t
         dst.dims[2], # Height::Csize_t
     ))
     cuMemcpy2D(copy_ref)
     return dst
 end
 
-function Base.copyto!(dst::CuTextureArray{T,3}, src::Union{Array{T,3}, CuArray{T,3}}) where {T}
+function Base.copyto!(dst::CuTextureArray{T,3,C}, src::Union{Array{<:Any,3}, CuArray{<:Any,3}}) where {T,C}
+    eltype(src) === packed_type(T,C) ||
+        throw(ArgumentError("Invalid source element type for $C-channel $T: expected $(packed_type(T,C)), got $(eltype(src))"))
     size(dst) == size(src) || throw(DimensionMismatch("source and destination sizes must match"))
     isdevmem = isa(src, CuArray)
     copy_ref = Ref(CUDA_MEMCPY3D(0, # srcXInBytes::Csize_t
@@ -203,7 +188,7 @@ function Base.copyto!(dst::CuTextureArray{T,3}, src::Union{Array{T,3}, CuArray{T
         isdevmem ? pointer(src) : 0, # srcDevice::CUdeviceptr
         0, # srcArray::CUarray
         0, # reserved0::Ptr{Cvoid}
-        size(src, 1) * sizeof(T), # srcPitch::Csize_t
+        size(src, 1) * sizeof(eltype(dst)), # srcPitch::Csize_t
         size(src, 2), # srcHeight::Csize_t
         0, # dstXInBytes::Csize_t
         0, # dstY::Csize_t
@@ -216,18 +201,12 @@ function Base.copyto!(dst::CuTextureArray{T,3}, src::Union{Array{T,3}, CuArray{T
         0, # reserved1::Ptr{Cvoid}
         0, # dstPitch::Csize_t
         0, # dstHeight::Csize_t
-        dst.dims[1] * sizeof(T), # WidthInBytes::Csize_t
+        dst.dims[1] * sizeof(eltype(dst)), # WidthInBytes::Csize_t
         dst.dims[2], # Height::Csize_t
         dst.dims[3], # Depth::Csize_t
     ))
     cuMemcpy3D(copy_ref)
     return dst
-end
-
-function CuTextureArray(a::Union{Array{T}, CuArray{T}}) where {T}
-    t = CuTextureArray{T}(size(a)...)
-    copyto!(t, a)
-    return t
 end
 
 
@@ -250,7 +229,7 @@ const mode_linear = CU_TR_FILTER_MODE_LINEAR
 @enum_without_prefix CUfilter_mode_enum CU_TR_
 
 """
-    CuTexture{T,N,P}
+    CuTexture{T,N,C,P}
 
 Type to handle CUDA texture objects. These objects do not hold data by themselves, but
 instead are bound either to `CuTextureArray`s (CUDA arrays) or to `CuArray`s. (Note: For
@@ -259,29 +238,27 @@ correct wrapping `CuArray`s it is necessary the their memory is well aligned and
 do texture fetchts inside kernels. When passed to kernels, `CuTexture` objects are
 transformed into `CuDeviceTexture`s objects.
 """
-mutable struct CuTexture{T,N,P}
-    mem::Union{CuArray{T,N}, CuTextureArray{T,N}}
+mutable struct CuTexture{T,N,C,P}
+    mem::Union{CuArray{T,N}, CuTextureArray{T,N,C}}
     handle::CUtexObject
 
     normalized_coordinates::Bool
 
     ctx::CuContext
 
-    function CuTexture{T,N,P}(texmemory::P;
+    function CuTexture{T,N,C,P}(texmemory::P;
                                 address_mode::Union{AddressMode,NTuple{N,AddressMode}}=ntuple(_->ADDRESS_MODE_CLAMP,N),
                                 filter_mode::FilterMode=FILTER_MODE_POINT,
-                                normalized_coordinates::Bool=false) where {T,N,P}
-        Ta = alias_type(T)
-        _assert_alias_size(T, Ta)
-        nchan, format, Te = _alias_type_to_nchan_and_format(Ta)
+                                normalized_coordinates::Bool=false) where {T,N,C,P}
+        format = convert(CUarray_format, T)
 
-        resDesc_ref = CUDA_RESOURCE_DESC(texmemory)
+        resDesc_ref = CUDA_RESOURCE_DESC(texmemory, C, format)
 
         flags = 0x0
         if normalized_coordinates
             flags |= CU_TRSF_NORMALIZED_COORDINATES
         end
-        if Te <: Integer
+        if T <: Integer
             flags |= CU_TRSF_READ_AS_INTEGER
         end
 
@@ -297,19 +274,19 @@ mutable struct CuTexture{T,N,P}
             0, # mipmapLevelBias::Cfloat
             0, # minMipmapLevelClamp::Cfloat
             0, # maxMipmapLevelClamp::Cfloat
-            ntuple(_->Cfloat(zero(Te)), 4), # borderColor::NTuple{4, Cfloat}
+            ntuple(_->Cfloat(zero(T)), 4), # borderColor::NTuple{4, Cfloat}
             ntuple(_->Cint(0), 12)))
 
         texObject_ref = Ref{CUtexObject}(0)
         cuTexObjectCreate(texObject_ref, resDesc_ref, texDesc_ref, C_NULL)
 
-        t = new{T,N,P}(texmemory, texObject_ref[], normalized_coordinates, context())
+        t = new{T,N,C,P}(texmemory, texObject_ref[], normalized_coordinates, context())
         finalizer(unsafe_destroy!, t)
         return t
     end
 end
 
-function CUDA_RESOURCE_DESC(texarr::CuTextureArray{T,N}) where {T,N}
+function CUDA_RESOURCE_DESC(texarr::CuTextureArray{T,N}, channels, format) where {T,N}
     # FIXME: manual construction due to invalid padding (JuliaInterop/Clang.jl#238)
     # res = Ref{ANONYMOUS1_res}()
     # unsafe_store!(Ptr{CUarray}(pointer_from_objref(res)), texarr.handle)
@@ -319,9 +296,9 @@ function CUDA_RESOURCE_DESC(texarr::CuTextureArray{T,N}) where {T,N}
     #     0 # flags::UInt32
     # ))
     resDesc_ref = Ref((CU_RESOURCE_TYPE_ARRAY, # resType::CUresourcetype
-                        texarr.handle, # 1 x UInt64
-                        ntuple(_->Int64(0), 15), # 15 x UInt64
-                        UInt32(0)))
+                       texarr.handle, # 1 x UInt64
+                       ntuple(_->Int64(0), 15), # 15 x UInt64
+                       UInt32(0)))
     return resDesc_ref
 end
 
@@ -329,24 +306,20 @@ end
 Base.unsafe_convert(::Type{Ptr{CUDA_RESOURCE_DESC}}, ref::Base.RefValue{T}) where {T} =
     convert(Ptr{CUDA_RESOURCE_DESC}, Base.unsafe_convert(Ptr{T}, ref))
 
-function CUDA_RESOURCE_DESC(arr::CuArray{T,N}) where {T,N}
+function CUDA_RESOURCE_DESC(arr::CuArray{T,N}, channels, format) where {T,N}
     # TODO: take care of allowed pitches
     1 <= N <= 2 || throw(ArgumentError("Only 1 or 2D CuArray objects can be wrapped in a texture"))
 
-    Ta = alias_type(T)
-    _assert_alias_size(T, Ta)
-    nchan, format, Te = _alias_type_to_nchan_and_format(Ta)
-
     # FIXME: manual construction due to invalid padding (JuliaInterop/Clang.jl#238)
     resDesc_ref = Ref(((N == 1 ? CU_RESOURCE_TYPE_LINEAR : CU_RESOURCE_TYPE_PITCH2D), # resType::CUresourcetype
-                        pointer(arr), # 1 x UInt64 (CUdeviceptr)
-                        format, # 1/2 x UInt64 (CUarray_format)
-                        UInt32(nchan), # 1/2 x UInt64
-                        (N == 2 ? size(arr, 1) : size(arr, 1) * sizeof(T)), # 1 x UInt64 nx
-                        (N == 2 ? size(arr, 2) : 0), # 1 x UInt64 ny
-                        (N == 2 ? size(arr, 1) * sizeof(T) : 0), # 1 x UInt64 pitch
-                        ntuple(_->Int64(0), 11), # 11 x UInt64
-                        UInt32(0)))
+                       pointer(arr), # 1 x UInt64 (CUdeviceptr)
+                       format, # 1/2 x UInt64 (CUarray_format)
+                       UInt32(channels), # 1/2 x UInt64
+                       (N == 2 ? size(arr, 1) : size(arr, 1) * sizeof(T)), # 1 x UInt64 nx
+                       (N == 2 ? size(arr, 2) : 0), # 1 x UInt64 ny
+                       (N == 2 ? size(arr, 1) * sizeof(T) : 0), # 1 x UInt64 pitch
+                       ntuple(_->Int64(0), 11), # 11 x UInt64
+                       UInt32(0)))
     return resDesc_ref
 end
 
@@ -358,20 +331,26 @@ end
 
 Base.convert(::Type{CUtexObject}, t::CuTexture) = t.handle
 
-CuTexture(texarr::CuTextureArray{T,N}; kwargs...) where {T,N} =
-    CuTexture{T,N,CuTextureArray{T,N}}(texarr; kwargs...)
-CuTexture{T}(n::Int; kwargs...) where {T} =
-    CuTexture(CuTextureArray{T,1}((n,)); kwargs...)
-CuTexture{T}(nx::Int, ny::Int; kwargs...) where {T} =
-    CuTexture(CuTextureArray{T,2}((nx, ny)); kwargs...)
-CuTexture{T}(nx::Int, ny::Int, nz::Int; kwargs...) where {T} =
-    CuTexture(CuTextureArray{T,3}((nx, ny, nz)); kwargs...)
-CuTexture(cuarr::CuArray{T,N}; kwargs...) where {T,N} =
-    CuTexture{T,N,CuArray{T,N}}(cuarr; kwargs...)
 
+## array interface
 
-Base.eltype(tm::CuTexture{T,N}) where {T,N} = T
+Base.eltype(tm::CuTexture{T,N,1}) where {T,N} = T
+Base.eltype(tm::CuTexture{T,N,C}) where {T,N,C} = NTuple{C,T}
+
 Base.size(tm::CuTexture) = size(tm.mem)
 
-Adapt.adapt_storage(::Adaptor, t::CuTexture{T,N}) where {T,N} =
-    CuDeviceTexture{T,N,alias_type(T),t.normalized_coordinates}(size(t.mem), t.handle)
+
+## interop with other arrays
+
+CuTexture{T,N,C}(x::Union{CuArray{T,N},CuTextureArray{T,N,C}}; kwargs...) where {T,N,C} =
+    CuTexture{T,N,C,typeof(x)}(x; kwargs...)
+
+CuTexture(x::CuTextureArray{T,N,C}; kwargs...) where {T,N,C} =
+    CuTexture{T,N,C}(x; kwargs...)
+CuTexture(x::CuArray{T,N}; kwargs...) where {T,N} =
+    CuTexture{T,N,1}(x; kwargs...)
+CuTexture(x::CuArray{NTuple{C,T},N}; kwargs...) where {T,N,C} =
+    CuTexture{T,N,C}(x; kwargs...)
+
+Adapt.adapt_storage(::Adaptor, t::CuTexture{T,N,C}) where {T,N,C} =
+    CuDeviceTexture{T,N,C,t.normalized_coordinates}(size(t.mem), t.handle)
