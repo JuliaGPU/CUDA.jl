ee67d67b585188139207c105266c26bf53b8e710
Roger-luo <rogerluo.rl18@gmail.com>
2020-06-11 01:25:31 +0000

    add symmetric support for mul

--- lib/cublas/linalg.jl
+++ lib/cublas/linalg.jl
@@ -399,3 +399,64 @@ for T in Base.uniontypes(CublasFloat) # needed to avoid ambiguous method error
     @eval LinearAlgebra.BLAS.trsm!(side::AbstractChar, uplo::AbstractChar, transa::AbstractChar, diag::AbstractChar, alpha::$T, A::CuMatrix{$T}, B::CuMatrix{$T}) =
         trsm!(side, uplo, transa, diag, alpha, A, B)
 end
+
+# symmetric mul!
+# level 2
+@inline function LinearAlgebra.mul!(y::CuVector{T}, A::Hermitian{T,<:CuMatrix}, x::CuVector{T},
+             α::Number, β::Number) where {T<:BlasReal}
+    alpha, beta = promote(α, β, zero(T))
+    if alpha isa Union{Bool,T} && beta isa Union{Bool,T}
+        return CUBLAS.symv!(A.uplo, alpha, A.data, x, beta, y)
+    else
+        error("only supports BLAS type, got $T")
+    end
+end
+
+@inline function LinearAlgebra.mul!(y::CuVector{T}, A::Hermitian{T,<:CuMatrix}, x::CuVector{T},
+             α::Number, β::Number) where {T<:BlasComplex}
+    alpha, beta = promote(α, β, zero(T))
+    if alpha isa Union{Bool,T} && beta isa Union{Bool,T}
+        return CUBLAS.hemv!(A.uplo, alpha, A.data, x, beta, y)
+    else
+        error("only supports BLAS type, got $T")
+    end
+end
+
+# level 3
+
+@inline function LinearAlgebra.mul!(C::CuMatrix{T}, A::Hermitian{T,<:CuMatrix}, B::CuMatrix{T},
+             α::Number, β::Number) where {T<:BlasReal}
+    alpha, beta = promote(α, β, zero(T))
+    if alpha isa Union{Bool,T} && beta isa Union{Bool,T}
+        return CUBLAS.symm!('L', A.uplo, alpha, A.data, B, beta, C)
+    else
+        error("only supports BLAS type, got $T")
+    end
+end
+@inline function LinearAlgebra.mul!(C::CuMatrix{T}, A::CuMatrix{T}, B::Hermitian{T,<:CuMatrix},
+             α::Number, β::Number) where {T<:BlasReal}
+    alpha, beta = promote(α, β, zero(T))
+    if alpha isa Union{Bool,T} && beta isa Union{Bool,T}
+        return CUBLAS.symm!('R', B.uplo, alpha, B.data, A, beta, C)
+    else
+        error("only supports BLAS type, got $T")
+    end
+end
+@inline function LinearAlgebra.mul!(C::CuMatrix{T}, A::Hermitian{T,<:CuMatrix}, B::CuMatrix{T},
+             α::Number, β::Number) where {T<:BlasComplex}
+    alpha, beta = promote(α, β, zero(T))
+    if alpha isa Union{Bool,T} && beta isa Union{Bool,T}
+        return CUBLAS.hemm!('L', A.uplo, alpha, A.data, B, beta, C)
+    else
+        error("only supports BLAS type, got $T")
+    end
+end
+@inline function LinearAlgebra.mul!(C::CuMatrix{T}, A::CuMatrix{T}, B::Hermitian{T,<:CuMatrix},
+             α::Number, β::Number) where {T<:BlasComplex}
+    alpha, beta = promote(α, β, zero(T))
+    if alpha isa Union{Bool,T} && beta isa Union{Bool,T}
+        return CUBLAS.hemm!('R', B.uplo, alpha, B.data, A, beta, C)
+    else
+        error("only supports BLAS type, got $T")
+    end
+end
