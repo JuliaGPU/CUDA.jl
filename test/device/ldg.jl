ff8ef96a7dd171056e58b15d19732ce4b35bd232
Valentin Churavy <vchuravy@users.noreply.github.com>
2020-11-17 02:34:12 -0500

    Fix address calculation for ldg (#549)

--- test/device/ldg.jl
+++ test/device/ldg.jl
@@ -36,6 +36,22 @@ end
 
     asm = String(take!(copy(buf)))
     @test occursin("ld.global.nc", asm)
+
+    function copy_const(A, _B)
+        B = Base.Experimental.Const(_B)
+        i = threadIdx().x
+        if i <= length(A)
+            @inbounds A[i] = B[i]
+        end
+        return nothing
+    end
+
+    x = CUDA.zeros(Float64, 32)
+    y = CUDA.ones(Float64, length(x))
+
+    @cuda threads=(length(x),) copy_const(x, y)
+    CUDA.synchronize()
+    @test x == y
 end
 
 end
