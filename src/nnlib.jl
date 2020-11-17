0be7a0e0d9eb12a148ae51057634edbe1831ca61
Michael Abbott <me@escbook>
2020-11-17 08:59:39 +0100

    one more pointer

--- src/nnlib.jl
+++ src/nnlib.jl
@@ -26,3 +26,6 @@ end
 # 1st argument is produced by NNlib.storage_type(A)
 NNlib._batched_gemm!(::Type{<:CuArray}, transA::Char, transB::Char, α::Number, A, B, β::Number, C) =
      CUBLAS.gemm_strided_batched!(transA, transB, α, A, B, β, C)
+
+Base.unsafe_convert(::Type{CuPtr{T}}, A::NNlib.BatchedAdjOrTrans{T}) where {T} =
+    Base.unsafe_convert(CuPtr{T}, parent(A))
