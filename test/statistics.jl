7126f01000300b8340e3349d53719078b102c97f
Stijn de Waele <stijn.de.waele123@gmail.com>
2020-11-29 20:01:54 -0500

    Implement methods Statistics.varm/stdm instead of Statistics. _var

--- test/statistics.jl
+++ test/statistics.jl
@@ -1,4 +1,5 @@
 using Statistics
+using StatsBase
 
 @testset "std" begin
     @test testf(std, rand(10))
@@ -25,4 +26,11 @@ end
     @test testf(x->mean(sin, x), rand(2,2))
     @test testf(x->mean(sin, x; dims=2), rand(2,2))
     @test testf(x->mean(sin, x; dims=[1,3]), rand(2,2,2))
+end
+
+@testset "combined $f" for f in (mean_and_std, mean_and_var)
+    @test testf(f, rand(10))
+    @test testf(f, rand(10,1,2))
+    @test testf(f, rand(10,1,2); corrected=true)
+    @test testf(f, rand(10,1,2),1)
 end
