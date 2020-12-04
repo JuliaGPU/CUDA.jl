05b78b81e955de0eecc7caf6b18238f9d9632d83
Valentin Churavy <vchuravy@users.noreply.github.com>
2020-12-04 18:27:47 -0500

    Add lock around `cfunction` lookup

--- lib/cudadrv/occupancy.jl
+++ lib/cudadrv/occupancy.jl
@@ -59,7 +59,9 @@ function launch_configuration(fun::CuFunction; shmem::Union{Integer,Base.Callabl
         cuOccupancyMaxPotentialBlockSize(blocks_ref, threads_ref, fun, C_NULL, shmem, max_threads)
     elseif Sys.ARCH == :x86 || Sys.ARCH == :x86_64
         shmem_cint = threads -> Cint(shmem(threads))
-        cb = @cfunction($shmem_cint, Cint, (Cint,))
+        cb = lock(_shmem_cb)
+            @cfunction($shmem_cint, Cint, (Cint,))
+        end
         cuOccupancyMaxPotentialBlockSize(blocks_ref, threads_ref, fun, cb, 0, max_threads)
     else
         lock(_shmem_cb_lock) do 
