a86c02830fd6c7cb21378b9d5ffc9ee9b2692d16
cfranken <cfranken@tofu.gps.caltech.edu>
2021-01-27 09:19:50 -0800

    Add fast getri_strided_batch

--- lib/cublas/wrappers.jl
+++ lib/cublas/wrappers.jl
@@ -1489,6 +1489,71 @@ for (fname, elty) in
         end
     end
 end
+## getriBatched - performs batched matrix inversion, no allocations inside
+for (fname, elty) in
+    ((:cublasDgetriBatched, :Float64),
+     (:cublasSgetriBatched, :Float32),
+     (:cublasZgetriBatched, :ComplexF64),
+     (:cublasCgetriBatched, :ComplexF32))
+    @eval begin
+    # cublasStatus_t cublasDgetriBatched(
+    #   cublasHandle_t handle, int n, double **A,
+    #   int lda, int *PivotArray, double **C,
+    #   int ldc, int *info, int batchSize)
+        function getri_batched!(n, Aptrs::CuVector{CuPtr{$elty}},
+                          lda, Cptrs::CuVector{CuPtr{$elty}},ldc,
+                          pivotArray::CuArray{Cint})
+            batchSize = length(Aptrs)
+            info = CuArray{Cint}(undef, batchSize)
+            CUBLAS.$fname(CUBLAS.handle(), n, Aptrs, lda, pivotArray, Cptrs, ldc, info, batchSize)
+            CUBLAS.unsafe_free!(Cptrs)
+            CUBLAS.unsafe_free!(Aptrs)
+            return info
+        end
+    end
+end
+
+
+for (fname, elty) in
+        ((:cublasDgetriBatched, :Float64),
+         (:cublasSgetriBatched, :Float32),
+         (:cublasZgetriBatched, :ComplexF64),
+         (:cublasCgetriBatched, :ComplexF32))
+    @eval begin
+        # cublasStatus_t cublasDgetriBatched(
+        #   cublasHandle_t handle, int n, double **A,
+        #   int lda, int *PivotArray, double **C,
+        #   int ldc, int *info, int batchSize)
+        function getri_batched!(A::Vector{<:CuMatrix{$elty}},
+                              C::Vector{<:CuMatrix{$elty}},
+                              pivotArray::CuMatrix{Cint})
+            n = size(A[1])[1]
+            lda = max(1, stride(A[1], 2))
+            ldc = max(1, stride(C[1], 2))
+            Aptrs = CUBLAS.unsafe_batch(A)
+            Cptrs = CUBLAS.unsafe_batch(C)
+            info = CuArrays.zeros(Cint, length(A))
+            CUBLAS.$fname(CUBLAS.handle(), n, Aptrs, lda, pivotArray, Cptrs, ldc, info, length(A))
+            CUBLAS.unsafe_free!(Cptrs)
+            CUBLAS.unsafe_free!(Aptrs)
+
+            return info
+        end
+    end
+end
+
+# CUDA has no strided batched getri, but we can at least avoid constructing costly views (based on getrf_strided_batch)
+function getri_strided_batched!(A::CuArray{<:Any,3}, C::CuArray{<:Any,3}, pivot::CuArray{Cint})
+    m, n = size(A, 1), size(A, 2)
+    if m != n
+        throw(DimensionMismatch("All matrices must be square!"))
+    end
+    ldc = max(1, stride(C, 2))
+    lda = max(1, stride(A, 2))
+    Cptrs = CUBLAS.unsafe_strided_batch(C)
+    Aptrs = CUBLAS.unsafe_strided_batch(A)
+    return getri_batched!(n, Aptrs, lda, Cptrs, ldc, pivot)
+end
 
 ## matinvBatched - performs batched matrix inversion
 
