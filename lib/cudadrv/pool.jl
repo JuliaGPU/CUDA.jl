5430e070fa17b3193971e352c952b0eb06cfc010
Tim Besard <tim@juliacomputing.com>
2021-03-02 13:04:28 +0100

    Use the default memory pool.
    It performs slightly better.

--- lib/cudadrv/pool.jl
+++ lib/cudadrv/pool.jl
@@ -1,30 +1,30 @@
 # Stream-orderdered memory allocator
 
-export CuMemoryPool, trim
+export CuMemoryPool, default_memory_pool, memory_pool, memory_pool!, trim
 
 mutable struct CuMemoryPool
     handle::CUmemoryPool
     ctx::CuContext
+end
 
-    function CuMemoryPool(dev::CuDevice)
-        props = Ref(CUmemPoolProps(
-            CU_MEM_ALLOCATION_TYPE_PINNED,
-            CU_MEM_HANDLE_TYPE_NONE,
-            CUmemLocation(
-                CU_MEM_LOCATION_TYPE_DEVICE,
-                deviceid(dev)
-            ),
-            C_NULL,
-            ntuple(i->Cuchar(0), 64)
-        ))
-        handle_ref = Ref{CUmemoryPool}()
-        cuMemPoolCreate(handle_ref, props)
+function CuMemoryPool(dev::CuDevice)
+    props = Ref(CUmemPoolProps(
+        CU_MEM_ALLOCATION_TYPE_PINNED,
+        CU_MEM_HANDLE_TYPE_NONE,
+        CUmemLocation(
+            CU_MEM_LOCATION_TYPE_DEVICE,
+            deviceid(dev)
+        ),
+        C_NULL,
+        ntuple(i->Cuchar(0), 64)
+    ))
+    handle_ref = Ref{CUmemoryPool}()
+    cuMemPoolCreate(handle_ref, props)
 
-        ctx = CuCurrentContext()
-        obj = new(handle_ref[], ctx)
-        finalizer(unsafe_destroy!, obj)
-        return obj
-    end
+    ctx = CuCurrentContext()
+    obj = CuMemoryPool(handle_ref[], ctx)
+    finalizer(unsafe_destroy!, obj)
+    return obj
 end
 
 function unsafe_destroy!(pool::CuMemoryPool)
@@ -37,5 +37,23 @@ Base.unsafe_convert(::Type{CUmemoryPool}, pool::CuMemoryPool) = pool.handle
 
 Base.:(==)(a::CuMemoryPool, b::CuMemoryPool) = a.handle == b.handle
 Base.hash(pool::CuMemoryPool, h::UInt) = hash(pool.handle, h)
+
+function default_memory_pool(dev::CuDevice)
+    handle_ref = Ref{CUmemoryPool}()
+    cuDeviceGetDefaultMemPool(handle_ref, dev)
+
+    ctx = CuCurrentContext()
+    CuMemoryPool(handle_ref[], ctx)
+end
+
+function memory_pool(dev::CuDevice)
+    handle_ref = Ref{CUmemoryPool}()
+    cuDeviceGetMemPool(handle_ref, dev)
+
+    ctx = CuCurrentContext()
+    CuMemoryPool(handle_ref[], ctx)
+end
+
+memory_pool!(dev::CuDevice, pool::CuMemoryPool) = cuDeviceSetMemPool(dev, pool)
 
 trim(pool::CuMemoryPool, bytes_to_keep::Integer=0) = cuMemPoolTrimTo(pool, bytes_to_keep)
