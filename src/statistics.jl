6de0d1681f524e0ff5dbf627d2f8c82c68a820d6
Eric Berquist <eric.berquist@gmail.com>
2021-04-28 11:45:02 -0400

    Implement statistics functions: correlation and covariance (#509)

--- src/statistics.jl
+++ src/statistics.jl
@@ -17,3 +17,27 @@ Statistics._mean(A::CuArray, ::Colon)    = sum(A) / length(A)
 Statistics._mean(f, A::CuArray, ::Colon) = sum(f, A) / length(A)
 Statistics._mean(A::CuArray, dims)    = mean!(Base.reducedim_init(t -> t/2, +, A, dims), A)
 Statistics._mean(f, A::CuArray, dims) = sum(f, A, dims=dims) / mapreduce(i -> size(A, i), *, unique(dims); init=1)
+
+function Statistics.covzm(x::CuMatrix, vardim::Int=1; corrected::Bool=true)
+    C = Statistics.unscaled_covzm(x, vardim)
+    T = promote_type(typeof(one(eltype(C)) / 1), eltype(C))
+    A = convert(AbstractArray{T}, C)
+    b = 1//(size(x, vardim) - corrected)
+    A .*= b
+    return A
+end
+
+function Statistics.cov2cor!(C::CuMatrix{T}, xsd::CuArray) where T
+    nx = length(xsd)
+    size(C) == (nx, nx) || throw(DimensionMismatch("inconsistent dimensions"))
+    tril!(C, -1)
+    C += adjoint(C)
+    C = Statistics.clampcor.(C ./ (xsd * xsd'))
+    C[diagind(C)] .= oneunit(T)
+    return C
+end
+
+function Statistics.corzm(x::CuMatrix, vardim::Int=1)
+    c = Statistics.unscaled_covzm(x, vardim)
+    return Statistics.cov2cor!(c, sqrt.(diag(c)))
+end
