d0b328e21840909febd81d4c5ee828ea3198334c
Tim Besard <tim.besard@gmail.com>
2021-08-05 07:56:05 +0200

    Bump RandomNumbers for Complex support.

--- test/random.jl
+++ test/random.jl
@@ -70,6 +70,7 @@ end
     rng = CUDA.RNG()
 
     for T in (Float16, Float32, Float64,
+              ComplexF16, Complex32, Complex64,
               Int8, Int16, Int32, Int64, Int128,
               UInt8, UInt16, UInt32, UInt64, UInt128)
         A = CuArray{T}(undef, 2048)
