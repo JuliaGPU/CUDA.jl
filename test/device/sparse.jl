1070b4620047edd4f459d4b5a00a24c7fc82abc0
Roger-Luo <rogerluo.rl18@gmail.com>
2021-09-18 21:38:00 -0400

    add test

--- test/device/sparse.jl
+++ test/device/sparse.jl
@@ -6,6 +6,12 @@ using CUDA: CuSparseDeviceVector, CuSparseDeviceMatrixCSC, CuSparseDeviceMatrixC
     CuSparseDeviceMatrixBSR, CuSparseDeviceMatrixCOO
 
 @testset "cudaconvert" begin
+    @test isbitstype(CuSparseVector{Float32, Cint})
+    @test isbitstype(CuSparseDeviceMatrixCSC{Float32, Cint})
+    @test isbitstype(CuSparseDeviceMatrixCSR{Float32, Cint})
+    @test isbitstype(CuSparseDeviceMatrixBSR{Float32, Cint})
+    @test isbitstype(CuSparseDeviceMatrixCOO{Float32, Cint})
+
     V = sprand(10, 0.5)
     cuV = CuSparseVector(V)
     @test cudaconvert(cuV) isa CuSparseDeviceVector
