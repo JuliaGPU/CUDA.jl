6bce1f553e998b60d685058a7ff99b4827829183
Tim Besard <tim.besard@gmail.com>
2021-10-07 13:12:58 +0200

    Reorganize code.

--- lib/cusolver/linalg.jl
+++ lib/cusolver/linalg.jl
@@ -2,8 +2,17 @@
 
 using LinearAlgebra
 
+CuMatOrAdj{T} = Union{CuMatrix,
+                      LinearAlgebra.Adjoint{T, <:CuMatrix{T}},
+                      LinearAlgebra.Transpose{T, <:CuMatrix{T}}}
+CuOrAdj{T} = Union{CuVecOrMat,
+                   LinearAlgebra.Adjoint{T, <:CuVecOrMat{T}},
+                   LinearAlgebra.Transpose{T, <:CuVecOrMat{T}}}
+
 
 # QR factorization
+
+using LinearAlgebra: AbstractQ
 
 struct CuQR{T,S<:AbstractMatrix} <: LinearAlgebra.Factorization{T}
     factors::S
@@ -11,7 +20,7 @@ struct CuQR{T,S<:AbstractMatrix} <: LinearAlgebra.Factorization{T}
     CuQR{T,S}(factors::AbstractMatrix{T}, τ::CuVector{T}) where {T,S<:AbstractMatrix} = new(factors, τ)
 end
 
-struct CuQRPackedQ{T,S<:AbstractMatrix} <: LinearAlgebra.AbstractQ{T}
+struct CuQRPackedQ{T,S<:AbstractMatrix} <: AbstractQ{T}
     factors::CuMatrix{T}
     τ::CuVector{T}
     CuQRPackedQ{T,S}(factors::AbstractMatrix{T}, τ::CuVector{T}) where {T,S<:AbstractMatrix} = new(factors, τ)
@@ -72,6 +81,15 @@ end
 LinearAlgebra.det(Q::CuQRPackedQ{<:Real}) = isodd(count(!iszero, Q.τ)) ? -1 : 1
 LinearAlgebra.det(Q::CuQRPackedQ) = prod(τ -> iszero(τ) ? one(τ) : -sign(τ)^2, Q.τ)
 
+# AbstractQ's `size` is the size of the full matrix,
+# while `Matrix(Q)` only gives the compact Q.
+# See JuliaLang/julia#26591 and JuliaGPU/CUDA.jl#969.
+CuMatrix{T}(Q::AbstractQ{S}) where {T,S} = convert(CuArray, Matrix{T}(Q))
+CuMatrix(Q::AbstractQ{T}) where {T} = CuMatrix{T}(Q)
+CuArray{T}(Q::AbstractQ) where {T} = CuMatrix{T}(Q)
+CuArray(Q::AbstractQ) = CuMatrix(Q)
+
+
 # Singular Value Decomposition
 
 struct CuSVD{T,Tr,A<:AbstractMatrix{T}} <: LinearAlgebra.Factorization{T}
@@ -130,4 +148,49 @@ if VERSION >= v"1.8-"
         C, info = LinearAlgebra._chol!(copy(parent(A)), A.uplo == 'U' ? UpperTriangular : LowerTriangular)
         return Cholesky(C.data, A.uplo, info)
     end
+end
+
+
+# matrix division
+
+function Base.:\(_A::CuMatOrAdj, _B::CuOrAdj)
+    A, B = copy(_A), copy(_B)
+    A, ipiv = CUSOLVER.getrf!(A)
+    return CUSOLVER.getrs!('N', A, ipiv, B)
+end
+
+# patch JuliaLang/julia#40899 to create a CuArray
+# (see https://github.com/JuliaLang/julia/pull/41331#issuecomment-868374522)
+if VERSION >= v"1.7-"
+_zeros(::Type{T}, b::AbstractVector, n::Integer) where {T} = CUDA.zeros(T, max(length(b), n))
+_zeros(::Type{T}, B::AbstractMatrix, n::Integer) where {T} = CUDA.zeros(T, max(size(B, 1), n), size(B, 2))
+function Base.:\(F::Union{LinearAlgebra.LAPACKFactorizations{<:Any,<:CuArray},
+                          Adjoint{<:Any,<:LinearAlgebra.LAPACKFactorizations{<:Any,<:CuArray}}},
+                 B::AbstractVecOrMat)
+    m, n = size(F)
+    if m != size(B, 1)
+        throw(DimensionMismatch("arguments must have the same number of rows"))
+    end
+
+    TFB = typeof(oneunit(eltype(B)) / oneunit(eltype(F)))
+    FF = Factorization{TFB}(F)
+
+    # For wide problem we (often) compute a minimum norm solution. The solution
+    # is larger than the right hand side so we use size(F, 2).
+    BB = _zeros(TFB, B, n)
+
+    if n > size(B, 1)
+        # Underdetermined
+        copyto!(view(BB, 1:m, :), B)
+    else
+        copyto!(BB, B)
+    end
+
+    ldiv!(FF, BB)
+
+    # For tall problems, we compute a least squares solution so only part
+    # of the rhs should be returned from \ while ldiv! uses (and returns)
+    # the complete rhs
+    return LinearAlgebra._cut_B(BB, 1:n)
+end
 end
