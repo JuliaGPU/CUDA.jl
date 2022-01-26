59ab8c44ac387da37f4652d13ea174554f6a7262
Takafumi Arakaki <aka.tkf@gmail.com>
2022-01-26 00:56:06 -0500

    Test non-concrete type

--- test/execution.jl
+++ test/execution.jl
@@ -102,6 +102,7 @@ end
     @test CUDA.return_type(identity, Tuple{Int}) === Int
     @test CUDA.return_type(CUDA.sin, Tuple{Float32}) === Float32
     @test CUDA.return_type(getindex, Tuple{CuDeviceArray{Float32,1,1},Int32}) === Float32
+    @test CUDA.return_type(getindex, Tuple{Base.RefValue{Integer}}) === Integer
 end
 
 
