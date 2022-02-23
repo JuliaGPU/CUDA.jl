3172225a62f753d9cb2ef1b034391f31669075ea
Tim Besard <tim.besard@gmail.com>
2022-02-23 07:43:47 +0100

    Implement sparse operations with UniformScaling using broadcast. (#1390)

--- lib/cusparse/broadcast.jl
+++ lib/cusparse/broadcast.jl
@@ -294,6 +294,9 @@ function sparse_to_dense_broadcast_kernel(f, output::CuDeviceArray, args...)
 end
 
 function Broadcast.copy(bc::Broadcasted{<:Union{CuSparseVecStyle,CuSparseMatStyle}})
+    # on 1.6, ntuple closures often fail to infer
+    VERSION < v"1.7" && @warn "Sparse broadcast is only supported on Julia 1.7 or higher" maxlog=1
+
     # find the sparse inputs
     bc = Broadcast.flatten(bc)
     sparse_args = findall(bc.args) do arg
