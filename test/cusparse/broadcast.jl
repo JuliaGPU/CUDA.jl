50e92f10254e4a4eec4594f9ba70e3e870ce01e1
Tim Besard <tim.besard@gmail.com>
2022-02-24 17:33:00 +0100

    Support limited sparse integer arrays by bitcasting to floating point.

--- test/cusparse/broadcast.jl
+++ test/cusparse/broadcast.jl
@@ -3,35 +3,34 @@ using CUDA.CUSPARSE, SparseArrays
 m,n = 2,3
 p = 0.5
 
-# we rely on CUSPARSE conversions, so only test supported types
-for elty in [Float32, Float64]
-    @testset "$typ" for typ in [CuSparseMatrixCSR, CuSparseMatrixCSC]
+for elty in [Int32, Int64, Float32, Float64]
+    @testset "$typ($elty)" for typ in [CuSparseMatrixCSR, CuSparseMatrixCSC]
         x = sprand(elty, m, n, p)
         dx = typ(x)
 
         # zero-preserving
-        y = x .* 1
-        dy = dx .* 1
+        y = x .* elty(1)
+        dy = dx .* elty(1)
         @test dy isa typ{elty}
         @test y == SparseMatrixCSC(dy)
 
         # not zero-preserving
-        y = x .+ 1
-        dy = dx .+ 1
+        y = x .+ elty(1)
+        dy = dx .+ elty(1)
         @test dy isa CuArray{elty}
         @test y == Array(dy)
 
         # involving something dense
-        y = x .* ones(m, n)
-        dy = dx .* CUDA.ones(m, n)
+        y = x .* ones(elty, m, n)
+        dy = dx .* CUDA.ones(elty, m, n)
         @test dy isa CuArray{elty}
         @test y == Array(dy)
 
         # multiple inputs
         y = sprand(elty, m, n, p)
         dy = typ(y)
-        z = x .* y .* 2
-        dz = dx .* dy .* 2
+        z = x .* y .* elty(2)
+        dz = dx .* dy .* elty(2)
         @test dz isa typ{elty}
         @test z == SparseMatrixCSC(dz)
     end
