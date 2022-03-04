f88ef2c2184d02021d96bce1cd5a1b8a82769d14
Tim Besard <tim.besard@gmail.com>
2022-03-04 18:17:43 +0100

    Switch to new LLVM context management for 1.9 compatibility.

--- src/device/runtime.jl
+++ src/device/runtime.jl
@@ -12,7 +12,7 @@ GPUCompiler.reset_runtime()
 function precompile_runtime(caps=CUDA.llvm_compat(LLVM.version()).cap)
     dummy_source = FunctionSpec(()->return, Tuple{})
     params = CUDACompilerParams()
-    Context() do ctx
+    JuliaContext() do ctx
         for cap in caps
             # NOTE: this often runs when we don't have a functioning set-up,
             #       so we don't use CUDACompilerTarget(...) which requires NVML
