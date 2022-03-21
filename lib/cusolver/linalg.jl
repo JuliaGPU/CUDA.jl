53326ef9a692918d1924a17660c8f03e1e88e8e5
Tim Besard <tim.besard@gmail.com>
2022-03-21 13:44:44 +0100

    Reorganize.

--- lib/cusolver/linalg.jl
+++ lib/cusolver/linalg.jl
@@ -2,20 +2,66 @@
 
 using LinearAlgebra
 
+
+# matrix division
+
+const CuMatOrAdj{T} = Union{CuMatrix,
+                            LinearAlgebra.Adjoint{T, <:CuMatrix{T}},
+                            LinearAlgebra.Transpose{T, <:CuMatrix{T}}}
+const CuOrAdj{T} = Union{CuVecOrMat,
+                         LinearAlgebra.Adjoint{T, <:CuVecOrMat{T}},
+                         LinearAlgebra.Transpose{T, <:CuVecOrMat{T}}}
+
+function Base.:\(_A::CuMatOrAdj, _B::CuOrAdj)
+    A, B = copy(_A), copy(_B)
+    A, ipiv = CUSOLVER.getrf!(A)
+    return CUSOLVER.getrs!('N', A, ipiv, B)
+end
+
+# patch JuliaLang/julia#40899 to create a CuArray
+# (see https://github.com/JuliaLang/julia/pull/41331#issuecomment-868374522)
+if VERSION >= v"1.7-"
+_zeros(::Type{T}, b::AbstractVector, n::Integer) where {T} = CUDA.zeros(T, max(length(b), n))
+_zeros(::Type{T}, B::AbstractMatrix, n::Integer) where {T} = CUDA.zeros(T, max(size(B, 1), n), size(B, 2))
+function Base.:\(F::Union{LinearAlgebra.LAPACKFactorizations{<:Any,<:CuArray},
+                          Adjoint{<:Any,<:LinearAlgebra.LAPACKFactorizations{<:Any,<:CuArray}}},
+                 B::AbstractVecOrMat)
+    m, n = size(F)
+    if m != size(B, 1)
+        throw(DimensionMismatch("arguments must have the same number of rows"))
+    end
+
+    TFB = typeof(oneunit(eltype(B)) / oneunit(eltype(F)))
+    FF = Factorization{TFB}(F)
+
+    # For wide problem we (often) compute a minimum norm solution. The solution
+    # is larger than the right hand side so we use size(F, 2).
+    BB = _zeros(TFB, B, n)
+
+    if n > size(B, 1)
+        # Underdetermined
+        copyto!(view(BB, 1:m, :), B)
+    else
+        copyto!(BB, B)
+    end
+
+    ldiv!(FF, BB)
+
+    # For tall problems, we compute a least squares solution so only part
+    # of the rhs should be returned from \ while ldiv! uses (and returns)
+    # the complete rhs
+    return LinearAlgebra._cut_B(BB, 1:n)
+end
+end
+
+
+# factorizations
+
 using LinearAlgebra: Factorization
 
-
-# QR factorization
+## QR
 
 using LinearAlgebra: AbstractQ
-
-# AbstractQ's `size` is the size of the full matrix,
-# while `Matrix(Q)` only gives the compact Q.
-# See JuliaLang/julia#26591 and JuliaGPU/CUDA.jl#969.
-CuMatrix{T}(Q::AbstractQ{S}) where {T,S} = convert(CuArray, Matrix{T}(Q))
-CuMatrix(Q::AbstractQ{T}) where {T} = CuMatrix{T}(Q)
-CuArray{T}(Q::AbstractQ) where {T} = CuMatrix{T}(Q)
-CuArray(Q::AbstractQ) = CuMatrix(Q)
 
 struct CuQR{T,S<:AbstractMatrix} <: Factorization{T}
     factors::S
@@ -29,8 +75,18 @@ struct CuQRPackedQ{T,S<:AbstractMatrix} <: AbstractQ{T}
     CuQRPackedQ{T,S}(factors::AbstractMatrix{T}, τ::CuVector{T}) where {T,S<:AbstractMatrix} = new(factors, τ)
 end
 
-CuQR(factors::AbstractMatrix{T}, τ::CuVector{T}) where {T} = CuQR{T,typeof(factors)}(factors, τ)
-CuQRPackedQ(factors::AbstractMatrix{T}, τ::CuVector{T}) where {T} = CuQRPackedQ{T,typeof(factors)}(factors, τ)
+CuQR(factors::AbstractMatrix{T}, τ::CuVector{T}) where {T} =
+    CuQR{T,typeof(factors)}(factors, τ)
+CuQRPackedQ(factors::AbstractMatrix{T}, τ::CuVector{T}) where {T} =
+    CuQRPackedQ{T,typeof(factors)}(factors, τ)
+
+# AbstractQ's `size` is the size of the full matrix,
+# while `Matrix(Q)` only gives the compact Q.
+# See JuliaLang/julia#26591 and JuliaGPU/CUDA.jl#969.
+CuMatrix{T}(Q::AbstractQ{S}) where {T,S} = convert(CuArray, Matrix{T}(Q))
+CuMatrix(Q::AbstractQ{T}) where {T} = CuMatrix{T}(Q)
+CuArray{T}(Q::AbstractQ) where {T} = CuMatrix{T}(Q)
+CuArray(Q::AbstractQ) = CuMatrix(Q)
 
 LinearAlgebra.qr!(A::CuMatrix{T}) where T = CuQR(geqrf!(A::CuMatrix{T})...)
 Base.size(A::CuQR) = size(A.factors)
@@ -84,7 +140,21 @@ end
 LinearAlgebra.det(Q::CuQRPackedQ{<:Real}) = isodd(count(!iszero, Q.τ)) ? -1 : 1
 LinearAlgebra.det(Q::CuQRPackedQ) = prod(τ -> iszero(τ) ? one(τ) : -sign(τ)^2, Q.τ)
 
-# Singular Value Decomposition
+function LinearAlgebra.ldiv!(_qr::CuQR, b::CuArray)
+    _x = UpperTriangular(_qr.R) \ (_qr.Q' * reshape(b,length(b),1))
+    b .= vec(_x)
+    unsafe_free!(_x)
+    return b
+end
+
+function LinearAlgebra.ldiv!(x::CuArray,_qr::CuQR, b::CuArray)
+    _x = UpperTriangular(_qr.R) \ (_qr.Q' * reshape(b,length(b),1))
+    x .= vec(_x)
+    unsafe_free!(_x)
+    return x
+end
+
+## SVD
 
 struct CuSVD{T,Tr,A<:AbstractMatrix{T}} <: Factorization{T}
     U::CuMatrix{T}
@@ -142,70 +212,4 @@ if VERSION >= v"1.8-"
         C, info = LinearAlgebra._chol!(copy(parent(A)), A.uplo == 'U' ? UpperTriangular : LowerTriangular)
         return Cholesky(C.data, A.uplo, info)
     end
-end
-
-
-# matrix division
-
-const CuMatOrAdj{T} = Union{CuMatrix,
-                            LinearAlgebra.Adjoint{T, <:CuMatrix{T}},
-                            LinearAlgebra.Transpose{T, <:CuMatrix{T}}}
-const CuOrAdj{T} = Union{CuVecOrMat,
-                         LinearAlgebra.Adjoint{T, <:CuVecOrMat{T}},
-                         LinearAlgebra.Transpose{T, <:CuVecOrMat{T}}}
-
-function LinearAlgebra.ldiv!(_qr::CuQR, b::CuArray)
-    _x = UpperTriangular(_qr.R) \ (_qr.Q' * reshape(b,length(b),1))
-    b .= vec(_x)
-    unsafe_free!(_x)
-    return b
-end
-
-function LinearAlgebra.ldiv!(x::CuArray,_qr::CuQR, b::CuArray)
-    _x = UpperTriangular(_qr.R) \ (_qr.Q' * reshape(b,length(b),1))
-    x .= vec(_x)
-    unsafe_free!(_x)
-    return x
-end
-
-function Base.:\(_A::CuMatOrAdj, _B::CuOrAdj)
-    A, B = copy(_A), copy(_B)
-    A, ipiv = CUSOLVER.getrf!(A)
-    return CUSOLVER.getrs!('N', A, ipiv, B)
-end
-
-# patch JuliaLang/julia#40899 to create a CuArray
-# (see https://github.com/JuliaLang/julia/pull/41331#issuecomment-868374522)
-if VERSION >= v"1.7-"
-_zeros(::Type{T}, b::AbstractVector, n::Integer) where {T} = CUDA.zeros(T, max(length(b), n))
-_zeros(::Type{T}, B::AbstractMatrix, n::Integer) where {T} = CUDA.zeros(T, max(size(B, 1), n), size(B, 2))
-function Base.:\(F::Union{LinearAlgebra.LAPACKFactorizations{<:Any,<:CuArray},
-                          Adjoint{<:Any,<:LinearAlgebra.LAPACKFactorizations{<:Any,<:CuArray}}},
-                 B::AbstractVecOrMat)
-    m, n = size(F)
-    if m != size(B, 1)
-        throw(DimensionMismatch("arguments must have the same number of rows"))
-    end
-
-    TFB = typeof(oneunit(eltype(B)) / oneunit(eltype(F)))
-    FF = Factorization{TFB}(F)
-
-    # For wide problem we (often) compute a minimum norm solution. The solution
-    # is larger than the right hand side so we use size(F, 2).
-    BB = _zeros(TFB, B, n)
-
-    if n > size(B, 1)
-        # Underdetermined
-        copyto!(view(BB, 1:m, :), B)
-    else
-        copyto!(BB, B)
-    end
-
-    ldiv!(FF, BB)
-
-    # For tall problems, we compute a least squares solution so only part
-    # of the rhs should be returned from \ while ldiv! uses (and returns)
-    # the complete rhs
-    return LinearAlgebra._cut_B(BB, 1:n)
-end
 end
