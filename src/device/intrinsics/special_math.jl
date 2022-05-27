1bfc76c8adf583f231501ccb0b67090b2eee738f
cossio <cossio@users.noreply.github.com>
2022-05-27 19:13:58 +0200

    lgamma -> loggamma
    Closes #1528

--- src/device/intrinsics/special_math.jl
+++ src/device/intrinsics/special_math.jl
@@ -21,8 +21,8 @@
 
 ## gamma function
 
-@device_override SpecialFunctions.lgamma(x::Float64) = ccall("extern __nv_lgamma", llvmcall, Cdouble, (Cdouble,), x)
-@device_override SpecialFunctions.lgamma(x::Float32) = ccall("extern __nv_lgammaf", llvmcall, Cfloat, (Cfloat,), x)
+@device_override SpecialFunctions.loggamma(x::Float64) = ccall("extern __nv_lgamma", llvmcall, Cdouble, (Cdouble,), x)
+@device_override SpecialFunctions.loggamma(x::Float32) = ccall("extern __nv_lgammaf", llvmcall, Cfloat, (Cfloat,), x)
 
 @device_override SpecialFunctions.gamma(x::Float64) = ccall("extern __nv_tgamma", llvmcall, Cdouble, (Cdouble,), x)
 @device_override SpecialFunctions.gamma(x::Float32) = ccall("extern __nv_tgammaf", llvmcall, Cfloat, (Cfloat,), x)
