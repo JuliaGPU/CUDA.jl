94c3f1aef208edd0725fbec4944d39406f2d6e15
Tim Besard <tim.besard@gmail.com>
2022-10-11 12:48:24 +0200

    Don't pass pointers to ccall.

--- lib/custatevec/src/statevec.jl
+++ lib/custatevec/src/statevec.jl
@@ -1,17 +1,17 @@
 function applyPauliExp!(sv::CuStateVec, theta::Float64, paulis::Vector{<:Pauli}, targets::Vector{Int32}, controls::Vector{Int32}, controlValues::Vector{Int32}=fill(one(Int32), length(controls)))
     cupaulis = CuStateVecPauli.(paulis)
-    custatevecApplyPauliRotation(handle(), pointer(sv.data), sv.nbits, theta, cupaulis, targets, length(targets), controls, controlVals, length(controls))
+    custatevecApplyPauliRotation(handle(), sv.data, sv.nbits, theta, cupaulis, targets, length(targets), controls, controlVals, length(controls))
     sv
 end
 
 function applyMatrix!(sv::CuStateVec, matrix::Union{Matrix, CuMatrix}, adjoint::Bool, targets::Vector{Int32}, controls::Vector{Int32}, controlValues::Vector{Int32}=fill(one(Int32), length(controls)))
     function bufferSize()
         out = Ref{Csize_t}()
-        custatevecApplyMatrixGetWorkspaceSize(handle(), eltype(sv), sv.nbits, pointer(matrix), eltype(matrix), CUSTATEVEC_MATRIX_LAYOUT_COL, Int32(adjoint), length(targets), length(controls), compute_type(eltype(sv), eltype(matrix)), out)
+        custatevecApplyMatrixGetWorkspaceSize(handle(), eltype(sv), sv.nbits, matrix, eltype(matrix), CUSTATEVEC_MATRIX_LAYOUT_COL, Int32(adjoint), length(targets), length(controls), compute_type(eltype(sv), eltype(matrix)), out)
         out[]
     end
     with_workspace(bufferSize) do buffer
-        custatevecApplyMatrix(handle(), pointer(sv.data), eltype(sv), sv.nbits, pointer(matrix), eltype(matrix), CUSTATEVEC_MATRIX_LAYOUT_COL, Int32(adjoint), targets, length(targets), controls, controlValues, length(controls), compute_type(eltype(sv), eltype(matrix)), pointer(buffer), length(buffer))
+        custatevecApplyMatrix(handle(), sv.data, eltype(sv), sv.nbits, matrix, eltype(matrix), CUSTATEVEC_MATRIX_LAYOUT_COL, Int32(adjoint), targets, length(targets), controls, controlValues, length(controls), compute_type(eltype(sv), eltype(matrix)), buffer, length(buffer))
     end
     sv
 end
@@ -19,13 +19,13 @@ end
 function expectation(sv::CuStateVec, matrix::Union{Matrix, CuMatrix}, basis_bits::Vector{Int32})
     function bufferSize()
         out = Ref{Csize_t}()
-        custatevecComputeExpectationGetWorkspaceSize(handle(), eltype(sv), sv.nbits, pointer(matrix), eltype(matrix), CUSTATEVEC_MATRIX_LAYOUT_COL, length(basis_bits), compute_type(eltype(sv), eltype(matrix)), out)
+        custatevecComputeExpectationGetWorkspaceSize(handle(), eltype(sv), sv.nbits, matrix, eltype(matrix), CUSTATEVEC_MATRIX_LAYOUT_COL, length(basis_bits), compute_type(eltype(sv), eltype(matrix)), out)
         out[]
     end
     expVal = Ref{Float64}()
     residualNorm = Ref{Float64}()
     with_workspace(bufferSize) do buffer
-        custatevecComputeExpectation(handle(), pointer(sv.data), eltype(sv), sv.nbits, expVal, Float64, residualNorm, pointer(matrix), eltype(matrix), CUSTATEVEC_MATRIX_LAYOUT_COL, basis_bits, length(basis_bits), compute_type(eltype(sv), eltype(matrix)), buffer, length(buffer))
+        custatevecComputeExpectation(handle(), sv.data, eltype(sv), sv.nbits, expVal, Float64, residualNorm, matrix, eltype(matrix), CUSTATEVEC_MATRIX_LAYOUT_COL, basis_bits, length(basis_bits), compute_type(eltype(sv), eltype(matrix)), buffer, length(buffer))
     end
     return expVal[], residualNorm[]
 end
