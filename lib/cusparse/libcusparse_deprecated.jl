3d90d12d5a99a7e4fd9d3e51d1c53239d9481e0f
Tim Besard <tim.besard@gmail.com>
2022-10-26 14:38:24 +0200

    Remove unused deprecated CUSPARSE functionality.

--- lib/cusparse/libcusparse_deprecated.jl
+++ lib/cusparse/libcusparse_deprecated.jl
@@ -1,53 +1,5 @@
 ## removed in CUDA 11
 
-@checked function cusparseScsr2csc(handle, m, n, nnz, csrSortedVal, csrSortedRowPtr,
-                                   csrSortedColInd, cscSortedVal, cscSortedRowInd,
-                                   cscSortedColPtr, copyValues, idxBase)
-    initialize_context()
-    ccall((:cusparseScsr2csc, libcusparse), cusparseStatus_t,
-                   (cusparseHandle_t, Cint, Cint, Cint, CuPtr{Cfloat}, CuPtr{Cint},
-                    CuPtr{Cint}, CuPtr{Cfloat}, CuPtr{Cint}, CuPtr{Cint}, cusparseAction_t,
-                    cusparseIndexBase_t),
-                   handle, m, n, nnz, csrSortedVal, csrSortedRowPtr, csrSortedColInd,
-                   cscSortedVal, cscSortedRowInd, cscSortedColPtr, copyValues, idxBase)
-end
-
-@checked function cusparseDcsr2csc(handle, m, n, nnz, csrSortedVal, csrSortedRowPtr,
-                                   csrSortedColInd, cscSortedVal, cscSortedRowInd,
-                                   cscSortedColPtr, copyValues, idxBase)
-    initialize_context()
-    ccall((:cusparseDcsr2csc, libcusparse), cusparseStatus_t,
-                   (cusparseHandle_t, Cint, Cint, Cint, CuPtr{Cdouble}, CuPtr{Cint},
-                    CuPtr{Cint}, CuPtr{Cdouble}, CuPtr{Cint}, CuPtr{Cint},
-                    cusparseAction_t, cusparseIndexBase_t),
-                   handle, m, n, nnz, csrSortedVal, csrSortedRowPtr, csrSortedColInd,
-                   cscSortedVal, cscSortedRowInd, cscSortedColPtr, copyValues, idxBase)
-end
-
-@checked function cusparseCcsr2csc(handle, m, n, nnz, csrSortedVal, csrSortedRowPtr,
-                                   csrSortedColInd, cscSortedVal, cscSortedRowInd,
-                                   cscSortedColPtr, copyValues, idxBase)
-    initialize_context()
-    ccall((:cusparseCcsr2csc, libcusparse), cusparseStatus_t,
-                   (cusparseHandle_t, Cint, Cint, Cint, CuPtr{cuComplex}, CuPtr{Cint},
-                    CuPtr{Cint}, CuPtr{cuComplex}, CuPtr{Cint}, CuPtr{Cint},
-                    cusparseAction_t, cusparseIndexBase_t),
-                   handle, m, n, nnz, csrSortedVal, csrSortedRowPtr, csrSortedColInd,
-                   cscSortedVal, cscSortedRowInd, cscSortedColPtr, copyValues, idxBase)
-end
-
-@checked function cusparseZcsr2csc(handle, m, n, nnz, csrSortedVal, csrSortedRowPtr,
-                                   csrSortedColInd, cscSortedVal, cscSortedRowInd,
-                                   cscSortedColPtr, copyValues, idxBase)
-    initialize_context()
-    ccall((:cusparseZcsr2csc, libcusparse), cusparseStatus_t,
-                   (cusparseHandle_t, Cint, Cint, Cint, CuPtr{cuDoubleComplex},
-                    CuPtr{Cint}, CuPtr{Cint}, CuPtr{cuDoubleComplex}, CuPtr{Cint},
-                    CuPtr{Cint}, cusparseAction_t, cusparseIndexBase_t),
-                   handle, m, n, nnz, csrSortedVal, csrSortedRowPtr, csrSortedColInd,
-                   cscSortedVal, cscSortedRowInd, cscSortedColPtr, copyValues, idxBase)
-end
-
 @checked function cusparseScsrmm2(handle, transA, transB, m, n, k, nnz, alpha, descrA,
                                   csrSortedValA, csrSortedRowPtrA, csrSortedColIndA, B,
                                   ldb, beta, C, ldc)
