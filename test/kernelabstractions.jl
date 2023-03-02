092f99d73796f19907f95804cc92c85f015565f9
Valentin Churavy <v.churavy@gmail.com>
2023-03-02 15:26:56 -0500

    update branch

--- test/kernelabstractions.jl
+++ test/kernelabstractions.jl
@@ -9,8 +9,14 @@ using CUDA.CUDAKernels
 if CUDA.functional()
     CUDA.versioninfo()
     CUDA.allowscalar(false)
+<<<<<<< HEAD
     Testsuite.testsuite(CUDADevice{false, false}, "CUDA", CUDA, CuArray, CUDA.CuDeviceArray)
     for (PreferBlocks, AlwaysInline) in Iterators.product((true, false), (true, false))
         Testsuite.unittest_testsuite(CUDADevice{true, false}, "CUDA", CUDA, CuArray, CUDA.CuDeviceArray)
+=======
+    Testsuite.testsuite(()->CUDABackend(false, false), backend, CUDA, CuArray, CUDA.CuDeviceArray)
+    for (PreferBlocks, AlwaysInline) in Iterators.product((true, false), (true, false))
+        Testsuite.unittest_testsuite(()->CUDABackend{PreferBlocks, AlwaysInline}, backend, CUDA, CuArray, CUDA.CuDeviceArray)
+>>>>>>> 925ec1fb8 (update branch)
     end
 end
