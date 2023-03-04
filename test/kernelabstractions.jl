c8397b1fc1a0f5d04ede62189c281e6a88aeb41a
Valentin Churavy <v.churavy@gmail.com>
2023-03-04 03:06:17 -0500

    fix runtests

--- test/kernelabstractions.jl
+++ test/kernelabstractions.jl
@@ -9,8 +9,8 @@ using CUDA.CUDAKernels
 if CUDA.functional()
     CUDA.versioninfo()
     CUDA.allowscalar(false)
-    Testsuite.testsuite(()->CUDABackend(false, false), backend, CUDA, CuArray, CUDA.CuDeviceArray)
+    Testsuite.testsuite(()->CUDABackend(false, false), "CUDA", CUDA, CuArray, CUDA.CuDeviceArray)
     for (PreferBlocks, AlwaysInline) in Iterators.product((true, false), (true, false))
-        Testsuite.unittest_testsuite(()->CUDABackend{PreferBlocks, AlwaysInline}, backend, CUDA, CuArray, CUDA.CuDeviceArray)
+        Testsuite.unittest_testsuite(()->CUDABackend{PreferBlocks, AlwaysInline}, "CUDA", CUDA, CuArray, CUDA.CuDeviceArray)
     end
 end
