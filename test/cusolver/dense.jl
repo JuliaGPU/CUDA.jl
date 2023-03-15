43e34d43798f3c6a50e3a1c4dff29a36bcf3e24c
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-03-15 01:25:12 -0400

    [CUSOLVER] Support A \ b for rectangular matrices

--- test/cusolver/dense.jl
+++ test/cusolver/dense.jl
@@ -630,18 +630,54 @@ end
 ], elty2 in [
     Float16, Float32, Float64, ComplexF16, ComplexF32, ComplexF64, Int32, Int64, Complex{Int32}, Complex{Int64}
 ]
-    A = rand(elty1,n,n)
-    B = rand(elty2,n,n)
-    b = rand(elty2,n)
-    d_A = CuArray(A)
-    d_B = CuArray(B)
-    d_b = CuArray(b)
-    cublasfloat = promote_type(Float32, promote_type(elty1, elty2))
-    Af = cublasfloat.(A)
-    Bf = cublasfloat.(B)
-    bf = cublasfloat.(b)
-    @test Array(d_A \ d_B) ≈ (Af \ Bf)
-    @test Array(d_A \ d_b) ≈ (Af \ bf)
-    @inferred d_A \ d_B
-    @inferred d_A \ d_b
+    @testset "Square linear systems" begin
+        A = rand(elty1,n,n)
+        B = rand(elty2,n,5)
+        b = rand(elty2,n)
+        d_A = CuArray(A)
+        d_B = CuArray(B)
+        d_b = CuArray(b)
+        cublasfloat = promote_type(Float32, promote_type(elty1, elty2))
+        Af = cublasfloat.(A)
+        Bf = cublasfloat.(B)
+        bf = cublasfloat.(b)
+        @test Array(d_A \ d_B) ≈ (Af \ Bf)
+        @test Array(d_A \ d_b) ≈ (Af \ bf)
+        @inferred d_A \ d_B
+        @inferred d_A \ d_b
+    end
+
+    @testset "Overdetermined linear systems" begin
+        A = rand(elty1,m,n)
+        B = rand(elty2,m,5)
+        b = rand(elty2,m)
+        d_A = CuArray(A)
+        d_B = CuArray(B)
+        d_b = CuArray(b)
+        cublasfloat = promote_type(Float32, promote_type(elty1, elty2))
+        Af = cublasfloat.(A)
+        Bf = cublasfloat.(B)
+        bf = cublasfloat.(b)
+        @test Array(d_A \ d_B) ≈ (Af \ Bf)
+        @test Array(d_A \ d_b) ≈ (Af \ bf)
+        @inferred d_A \ d_B
+        @inferred d_A \ d_b
+    end
+
+    @testset "Underdetermined linear systems" begin
+        A = rand(elty1,n,m)
+        B = rand(elty2,n,5)
+        b = rand(elty2,n)
+        d_A = CuArray(A)
+        d_B = CuArray(B)
+        d_b = CuArray(b)
+        cublasfloat = promote_type(Float32, promote_type(elty1, elty2))
+        Af = cublasfloat.(A)
+        Bf = cublasfloat.(B)
+        bf = cublasfloat.(b)
+        @test Array(d_A \ d_B) ≈ (Af \ Bf)
+        @test Array(d_A \ d_b) ≈ (Af \ bf)
+        @inferred d_A \ d_B
+        @inferred d_A \ d_b
+    end
 end
