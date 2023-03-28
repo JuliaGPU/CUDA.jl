387d5b16c00a22d9ab608e3005dc9161705dc56c
Tim Besard <tim.besard@gmail.com>
2023-03-28 10:36:45 +0200

    Simplify KernelAbstractions test suite.

--- test/kernelabstractions.jl
+++ test/kernelabstractions.jl
@@ -1,16 +1,7 @@
 import KernelAbstractions
-using Test
-
 include(joinpath(dirname(pathof(KernelAbstractions)), "..", "test", "testsuite.jl"))
 
-using CUDA
-using CUDA.CUDAKernels
-
-if CUDA.functional()
-    CUDA.versioninfo()
-    CUDA.allowscalar(false)
-    Testsuite.testsuite(()->CUDABackend(false, false), "CUDA", CUDA, CuArray, CUDA.CuDeviceArray)
-    for (PreferBlocks, AlwaysInline) in Iterators.product((true, false), (true, false))
-        Testsuite.unittest_testsuite(()->CUDABackend(PreferBlocks, AlwaysInline), "CUDA", CUDA, CUDA.CuDeviceArray)
-    end
+Testsuite.testsuite(()->CUDABackend(false, false), "CUDA", CUDA, CuArray, CuDeviceArray)
+for (PreferBlocks, AlwaysInline) in Iterators.product((true, false), (true, false))
+    Testsuite.unittest_testsuite(()->CUDABackend(PreferBlocks, AlwaysInline), "CUDA", CUDA, CuDeviceArray)
 end
