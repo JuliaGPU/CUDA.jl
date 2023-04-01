55955a67cf53a7f43f72ca5e19dd71c8d60bd870
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2023-03-31 22:20:38 -0400

    add some tests

--- test/iterator.jl
+++ test/iterator.jl
@@ -25,3 +25,17 @@ end
 @test eltype(cubatches) == eltype(batch for batch in batches) == Any
 @test Base.IteratorEltype(typeof(CuIterator(batches))) isa Base.HasEltype
 @test eltype(CuIterator(batches)) == eltype(batches)  # Vector
+
+it_nt = CuIterator((x=Float32[i,i/2], y=i) for i in 1:4)
+@test first(it_nt).x isa CuArray{Float32}
+batch1, state = iterate(it_nt)
+@test batch1.x == cu([1,1/2])
+batch2, _ = iterate(it_nt, state)
+@test batch2.x == cu([2,2/2])
+@test batch1.x.storage === nothing  # unsafe_free! has worked inside
+
+it_vec = CuIterator([[i,i/2], [i/3, i/4]] for i in 1:4)
+@test first(it_vec)[1] isa CuArray{Float64}
+
+it_static = CuIterator([SA[i,i/2], SA[i/3, i/4]] for i in 1:4)
+@test first(it_static) isa CuArray{<:SVector}
