583329dc9cb649c00b98aaeb38e774ce5afe2e9c
Simon Byrne <simonbyrne@gmail.com>
2023-07-16 23:31:02 -0700

    Adapt Type call broadcasting to a function (#2000)
    This is a more generic solution to the existing `broadcasted` definition that fixes #1761 (as suggested in https://github.com/JuliaGPU/CUDA.jl/issues/1761#issuecomment-1425496244).

--- test/base/broadcast.jl
+++ test/base/broadcast.jl
@@ -39,3 +39,11 @@ end
   A = CuArray{ComplexF64}(undef, (2,2))
   @test eltype(convert.(ComplexF32, A)) == ComplexF32
 end
+
+# https://github.com/JuliaGPU/CUDA.jl/issues/1761
+@testset "Broadcast Type(args)" begin
+  A = CuArray{ComplexF64}(undef, (2,2))
+  @test eltype(ComplexF32.(A)) == ComplexF32
+  @test eltype(A .+ ComplexF32.(1)) == ComplexF64
+  @test eltype(ComplexF32.(A) .+ ComplexF32.(1)) == ComplexF32
+end
