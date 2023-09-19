9fe79f661ba0892714c9787729825b9fc853ccec
Documenter.jl <documenter@juliadocs.github.io>
2023-09-19 08:46:26 +0000

    build based on 2fa65728e

--- versions.js
+++ versions.js
@@ -1,5 +1,6 @@
 var DOC_VERSIONS = [
   "stable",
+  "v5.0",
   "v4.4",
   "v4.3",
   "v4.2",
@@ -24,5 +25,5 @@ var DOC_VERSIONS = [
   "v2.2",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v4.4.1";
+var DOCUMENTER_NEWEST = "v5.0.0";
 var DOCUMENTER_STABLE = "stable";
