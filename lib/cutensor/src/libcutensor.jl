b9a64eac0519ec901d328f5c22cacc9706bd8ec6
Tim Besard <tim.besard@gmail.com>
2023-12-20 13:48:31 +0100

    Add wrappers for compute descriptors.

--- lib/cutensor/src/libcutensor.jl
+++ lib/cutensor/src/libcutensor.jl
@@ -505,3 +505,15 @@ end
 end
 
 # Skipping MacroDefinition: CUTENSOR_EXTERN extern
+
+# compute descriptors are accessed through external symbols
+for desc in [:CUTENSOR_COMPUTE_DESC_16F,
+             :CUTENSOR_COMPUTE_DESC_16BF,
+             :CUTENSOR_COMPUTE_DESC_TF32,
+             :CUTENSOR_COMPUTE_DESC_3XTF32,
+             :CUTENSOR_COMPUTE_DESC_32F,
+             :CUTENSOR_COMPUTE_DESC_64F]
+    @eval begin
+        $desc() = cutensorComputeDescriptor_t(cglobal(($(QuoteNode(desc)), libcutensor)))
+    end
+end
