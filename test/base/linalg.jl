ed30f605c938d9b76256a25ee1354c37ca24b332
Alberto Mercurio <61953577+albertomercurio@users.noreply.github.com>
2023-12-21 20:14:35 +0100

    Added kronecker product support for dense matrices (#2177)

--- test/base/linalg.jl
+++ test/base/linalg.jl
@@ -17,3 +17,20 @@ end
 
     @test testf(dot, rand(Bool, 1024, 1024), rand(Float64, 1024, 1024))
 end
+
+@testset "kron" begin
+    dim1A = 50
+    dim2A = 80
+    dim1B = 90
+    dim2B = 40
+
+    @testset for T in [Int16, Int32, Int64,
+                       Float16, Float32, Float64,
+                       ComplexF16, ComplexF32, ComplexF64]
+
+        A = CUDA.rand(T, dim1A, dim2A)
+        B = CUDA.rand(T, dim1B, dim2B);
+        @test Array(kron(A, B)) ≈ kron(Array(A), Array(B))
+        @test Array(kron(B, A)) ≈ kron(Array(B), Array(A))
+    end
+end
