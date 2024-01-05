f1a250fc2c2e862262f080b062516f347d3f5c18
Tim Besard <tim.besard@gmail.com>
2024-01-05 15:16:05 +0100

    WIP

--- perf/latency.jl
+++ perf/latency.jl
@@ -6,30 +6,13 @@ using BenchmarkTools
 function main()
     results = BenchmarkGroup()
 
+    CUDA.versioninfo()
+
     base_cmd = Base.julia_cmd()
     if Base.JLOptions().project != C_NULL
         base_cmd = `$base_cmd --project=$(unsafe_string(Base.JLOptions().project))`
     end
-    # NOTE: we don't ust Base.active_project() here because of how CI launches this script,
-    #       starting with --project in the main CUDA.jl project.
-
-    # time to precompile the package and its dependencies
-    precompile_cmd =
-        `$base_cmd -e "pkg = Base.identify_package(\"CUDA\")
-                       Base.compilecache(pkg)"`
-    results["precompile"] = @benchmark run($precompile_cmd) evals=1 seconds=60
-
-    # time to actually import the package
-    import_cmd =
-        `$base_cmd -e "using CUDA"`
-    results["import"] = @benchmark run($import_cmd) evals=1 seconds=30
-
-    # time to actually compile a kernel
-    ttfp_cmd =
-        `$base_cmd -e "using CUDA
-                       kernel() = return
-                       CUDA.code_ptx(devnull, kernel, Tuple{}; kernel=true)"`
-    results["ttfp"] = @benchmark run($ttfp_cmd) evals=1 seconds=60
+    run(`$base_cmd -e "using CUDA; device()"`)
 
     results
 end
--- perf/runbenchmarks.jl
+++ perf/runbenchmarks.jl
@@ -2,6 +2,8 @@
 
 using CUDA
 
+CUDA.versioninfo()
+
 using BenchmarkTools
 
 using StableRNGs
