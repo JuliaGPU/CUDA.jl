9e04c887013e18f7068ae2b3cc6de1aed5b106a9
Tim Besard <tim.besard@gmail.com>
2024-01-19 16:17:49 +0100

    Simplify.

--- lib/cutensor/src/interfaces.jl
+++ lib/cutensor/src/interfaces.jl
@@ -6,7 +6,9 @@ function Base.:(+)(A::CuTensor, B::CuTensor)
     α = convert(eltype(A), 1.0)
     γ = convert(eltype(B), 1.0)
     C = similar(B)
-    elementwise_binary_execute!(α, A.data, A.inds, CUTENSOR_OP_IDENTITY, γ, B.data, B.inds, CUTENSOR_OP_IDENTITY, C.data, C.inds, CUTENSOR_OP_ADD)
+    elementwise_binary_execute!(α, A.data, A.inds, CUTENSOR_OP_IDENTITY,
+                                γ, B.data, B.inds, CUTENSOR_OP_IDENTITY,
+                                C.data, C.inds, CUTENSOR_OP_ADD)
     C
 end
 
@@ -14,7 +16,9 @@ function Base.:(-)(A::CuTensor, B::CuTensor)
     α = convert(eltype(A), 1.0)
     γ = convert(eltype(B), -1.0)
     C = similar(B)
-    elementwise_binary_execute!(α, A.data, A.inds, CUTENSOR_OP_IDENTITY, γ, B.data, B.inds, CUTENSOR_OP_IDENTITY, C.data, C.inds, CUTENSOR_OP_ADD)
+    elementwise_binary_execute!(α, A.data, A.inds, CUTENSOR_OP_IDENTITY,
+                                γ, B.data, B.inds, CUTENSOR_OP_IDENTITY,
+                                C.data, C.inds, CUTENSOR_OP_ADD)
     C
 end
 
@@ -24,8 +28,8 @@ function Base.:(*)(A::CuTensor, B::CuTensor)
     B_uniqs = [(idx, i) for (idx, i) in enumerate(B.inds) if !(i in A.inds)]
     A_sizes = map(x->size(A,x[1]), A_uniqs)
     B_sizes = map(x->size(B,x[1]), B_uniqs)
-    A_inds = map(x->Char(x[2]), A_uniqs)
-    B_inds = map(x->Char(x[2]), B_uniqs)
+    A_inds = map(x->x[2], A_uniqs)
+    B_inds = map(x->x[2], B_uniqs)
     C = CuTensor(CUDA.zeros(tC, Dims(vcat(A_sizes, B_sizes))), vcat(A_inds, B_inds))
     return mul!(C, A, B)
 end
@@ -52,6 +56,7 @@ end
 function LinearAlgebra.mul!(C::CuTensor, A::CuTensor, B::CuTensor, α::Number, β::Number)
    contract!(α, A.data, A.inds, CUTENSOR_OP_IDENTITY,
              B.data, B.inds, CUTENSOR_OP_IDENTITY, β,
-             C.data, C.inds, CUTENSOR_OP_IDENTITY, CUTENSOR_OP_IDENTITY)
+             C.data, C.inds, CUTENSOR_OP_IDENTITY, CUTENSOR_OP_IDENTITY;
+             jit=CUTENSOR_JIT_MODE_DEFAULT)
    return C
 end
