780aa73dab598ed9a1620d97785bb04fac66570d
William S. Moses <gh@wsmoses.com>
2024-05-06 21:07:43 -0400

    Custom rule cudaconvert

--- ext/EnzymeCoreExt.jl
+++ ext/EnzymeCoreExt.jl
@@ -39,6 +39,36 @@ function EnzymeCore.EnzymeRules.forward(ofn::Const{typeof(cufunction)},
     end)
 end
 
+function EnzymeCore.EnzymeRules.forward(ofn::Const{typeof(cudaconvert)},
+                                        ::Type{RT}, x::IT) where {RT, IT}
+    if RT <: Duplicated
+        return Duplicated(ofn.val(x.val), ofn.val(x.dval))
+    elseif RT <: Const
+        return ofn.val(x.val)
+    elseif RT <: DuplicatedNoNeed
+        return ofn.val(x.val)
+    else
+        tup = ntuple(Val(EnzymeRules.batch_width(RT))) do i
+            Base.@_inline_meta
+            ofn.val(x.dval[i])
+        end
+        if RT <: BatchDuplicated
+            return BatchDuplicated(ofv.val(x.val, tup))
+        else
+            return tup
+        end
+    end
+end
+
+function EnzymeCore.EnzymeRules.forward(ofn::Const{typeof(cufunction)},
+                                        ::Type{BatchDuplicated{T,N}}, f::Const{F},
+                                        tt::Const{TT}; kwargs...) where {F,TT,T,N}
+    res = ofn.val(f.val, tt.val; kwargs...)
+    return BatchDuplicated(res, ntuple(Val(N)) do _
+        res
+    end)
+end
+
 function EnzymeCore.EnzymeRules.forward(ofn::EnzymeCore.Annotation{CUDA.HostKernel{F,TT}},
                                         ::Type{Const{Nothing}}, args...;
                                         kwargs...) where {F,TT}
