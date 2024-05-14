cb6b733ec8125520e6ee7e0eafe0271f44804f38
William Moses <gh@wsmoses.com>
2024-05-14 11:45:40 -0700

    Update EnzymeCoreExt.jl

--- ext/EnzymeCoreExt.jl
+++ ext/EnzymeCoreExt.jl
@@ -6,6 +6,7 @@ using CUDA
 import CUDA: GPUCompiler, CUDABackend
 
 isdefined(Base, :get_extension) ? (import EnzymeCore) : (import ..EnzymeCore)
+isdefined(Base, :get_extension) ? (using EnzymeCore) : (using ..EnzymeCore)
 
 function EnzymeCore.compiler_job_from_backend(::CUDABackend, @nospecialize(F::Type), @nospecialize(TT::Type))
     mi = GPUCompiler.methodinstance(F, TT)
