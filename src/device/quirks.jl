e1e5be2b6bf17f03a367cebeb18c4645e593f80d
Tim Besard <tim.besard@gmail.com>
2024-06-10 12:51:37 +0200

    Conditionalize a quirk. (#2411)

--- src/device/quirks.jl
+++ src/device/quirks.jl
@@ -88,5 +88,7 @@ end
 # rational.jl
 @device_override @noinline Base.__throw_rational_argerror_zero(::Type{T}) where {T} =
     @gputhrow "ArgumentError" "invalid rational: 0//0"
+@static if VERSION < v"1.11.0-DEV.708"
 @device_override @noinline Base.__throw_rational_argerror_typemin(::Type{T}) where {T} =
     @gputhrow "OverflowError" "rational numerator is typemin"
+end
