1bdbb86dd918a68049068a4ac11c4a4c099f1c11
Erik Schnetter <schnetter@gmail.com>
2024-07-18 06:20:16 -0400

    CUFFT: Support Float16 by switching to Xt APIs. (#2430)

--- lib/cufft/util.jl
+++ lib/cufft/util.jl
@@ -1,25 +1,20 @@
-const cufftNumber = Union{cufftDoubleReal,cufftReal,cufftDoubleComplex,cufftComplex}
-const cufftReals = Union{cufftDoubleReal,cufftReal}
-const cufftComplexes = Union{cufftDoubleComplex,cufftComplex}
-const cufftDouble = Union{cufftDoubleReal,cufftDoubleComplex}
-const cufftSingle = Union{cufftReal,cufftComplex}
-const cufftTypeDouble = Union{Type{cufftDoubleReal},Type{cufftDoubleComplex}}
-const cufftTypeSingle = Union{Type{cufftReal},Type{cufftComplex}}
+const cufftReals = Union{cufftDoubleReal,cufftReal,Float16}
+const cufftComplexes = Union{cufftDoubleComplex,cufftComplex,Complex{Float16}}
+const cufftNumber = Union{cufftReals,cufftComplexes}
 
 cufftfloat(x) = _cufftfloat(float(x))
-_cufftfloat(::Type{T}) where {T<:cufftReals} = T
-_cufftfloat(::Type{Float16}) = Float32
-_cufftfloat(::Type{Complex{T}}) where {T} = Complex{_cufftfloat(T)}
+_cufftfloat(::Type{T}) where {T<:cufftNumber} = T
 _cufftfloat(::Type{T}) where {T} = error("type $T not supported")
 _cufftfloat(x::T) where {T} = _cufftfloat(T)(x)
 
-complexfloat(x::DenseCuArray{Complex{<:cufftReals}}) = x
 realfloat(x::DenseCuArray{<:cufftReals}) = x
+realfloat(x::DenseCuArray{T}) where {T<:Real} = copy1(cufftfloat(T), x)
+realfloat(x::DenseCuArray{T}) where {T} = error("type $T not supported")
 
-complexfloat(x::DenseCuArray{T}) where {T<:Complex} = copy1(typeof(cufftfloat(zero(T))), x)
-complexfloat(x::DenseCuArray{T}) where {T<:Real} = copy1(typeof(complex(cufftfloat(zero(T)))), x)
-
-realfloat(x::DenseCuArray{T}) where {T<:Real} = copy1(typeof(cufftfloat(zero(T))), x)
+complexfloat(x::DenseCuArray{<:cufftComplexes}) = x
+complexfloat(x::DenseCuArray{T}) where {T<:Complex} = copy1(cufftfloat(T), x)
+complexfloat(x::DenseCuArray{T}) where {T<:Real} = copy1(cufftfloat(complex(T)), x)
+complexfloat(x::DenseCuArray{T}) where {T} = error("type $T not supported")
 
 function copy1(::Type{T}, x) where T
     y = CuArray{T}(undef, map(length, axes(x)))
