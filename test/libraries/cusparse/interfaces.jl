f689f04f45c9ad5ea903a89d0d1be0c8e9490a0d
William Alexaindre <66435800+walexaindre@users.noreply.github.com>
2024-08-07 09:00:43 -0400

    Implementation of spdiagm for CUSPARSE (#2458)
    Co-authored-by: Alexis Montoison <35051714+amontoison@users.noreply.github.com>

--- test/libraries/cusparse/interfaces.jl
+++ test/libraries/cusparse/interfaces.jl
@@ -538,3 +538,21 @@ using LinearAlgebra, SparseArrays
         end
     end
 end
+
+@testset "SparseArrays" begin
+    @testset "spdiagm(CuVector{$elty})" for elty in [Float32, Float64, ComplexF32, ComplexF64]
+        ref_vec = collect(elty, 100:121)
+        cuda_vec = CuVector(ref_vec)
+
+        ref_spdiagm = spdiagm(ref_vec) # SparseArrays
+        cuda_spdiagm = spdiagm(cuda_vec) # CuSparseMatrixCSC
+
+        ref_cuda_sparse = CuSparseMatrixCSC(ref_spdiagm)
+
+        @test ref_cuda_sparse.rowVal == cuda_spdiagm.rowVal
+
+        @test ref_cuda_sparse.nzVal == cuda_spdiagm.nzVal
+
+        @test ref_cuda_sparse.colPtr == cuda_spdiagm.colPtr
+    end
+end
