06bf34b5e424eca3d966c99f18c4fa38a3d91580
Tim Besard <tim.besard@gmail.com>
2024-09-10 16:50:42 +0200

    Add missing GC.@preserves.

--- lib/nvml/system.jl
+++ lib/nvml/system.jl
@@ -4,7 +4,7 @@ function version()
 
     # the version string is too long for Julia to handle, e.g. 11.450.36.06,
     # so split off the driver part into the build suffix
-    ver = unsafe_string(pointer(buf))
+    ver = GC.@preserve buf unsafe_string(pointer(buf))
     parts = parse.(Int, split(ver, '.'))
     return VersionNumber(parts[1], 0, 0, (), Tuple(parts[2:end]))
 end
@@ -12,7 +12,7 @@ end
 function driver_version()
     buf = Vector{Cchar}(undef, NVML_SYSTEM_DRIVER_VERSION_BUFFER_SIZE)
     nvmlSystemGetDriverVersion(pointer(buf), length(buf))
-    return VersionNumber(unsafe_string(pointer(buf)))
+    return GC.@preserve buf VersionNumber(unsafe_string(pointer(buf)))
 end
 
 function cuda_driver_version()
