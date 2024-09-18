a56682ed21ead11c910360b96487d31af4d6d9a1
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2024-09-18 06:01:03 -0500

    [CUSPARSE] Implement a sparse GEMV for CuSparseMatrixCSC * CuSparseVector (#2488)
    This operation is not used by default by `*`.
    
    Co-authored-by: Tim Besard <tim.besard@gmail.com>

--- lib/cusparse/interfaces.jl
+++ lib/cusparse/interfaces.jl
@@ -73,6 +73,7 @@ function LinearAlgebra.generic_matvecmul!(C::CuVector{T}, tA::AbstractChar, A::C
     tA = tA in ('S', 's', 'H', 'h') ? 'N' : tA
     mv_wrapper(tA, alpha, A, B, beta, C)
 end
+
 function LinearAlgebra.generic_matvecmul!(C::CuVector{T}, tA::AbstractChar, A::CuSparseMatrix{T}, B::CuSparseVector{T}, alpha::Number, beta::Number) where {T <: Union{Float16, ComplexF16, BlasFloat}}
     tA = tA in ('S', 's', 'H', 'h') ? 'N' : tA
     mv_wrapper(tA, alpha, A, CuVector{T}(B), beta, C)
