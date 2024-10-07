2486af3ad8e1c0741257c9a3646eafe96c0a267a
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2024-10-07 14:00:02 -0500

    Added multiplication of transpose / adjoint matrices by diagonal matrices  (#2518)

--- lib/cublas/linalg.jl
+++ lib/cublas/linalg.jl
@@ -338,6 +338,22 @@ function LinearAlgebra.mul!(C::CuMatrix{T}, A::Diagonal{T,<:CuVector}, B::CuMatr
     return dgmm!('L', B, A.diag, C)
 end
 
+function LinearAlgebra.mul!(C::CuMatrix{T}, A::Transpose{T,<:CuMatrix}, B::Diagonal{T,<:CuVector}) where {T<:CublasFloat}
+    return dgmm!('R', CuMatrix(A), B.diag, C)
+end
+
+function LinearAlgebra.mul!(C::CuMatrix{T}, A::Diagonal{T,<:CuVector}, B::Transpose{T,<:CuMatrix}) where {T<:CublasFloat}
+    return dgmm!('L', CuMatrix(B), A.diag, C)
+end
+
+function LinearAlgebra.mul!(C::CuMatrix{T}, A::Adjoint{T,<:CuMatrix}, B::Diagonal{T,<:CuVector}) where {T<:CublasFloat}
+    return dgmm!('R', CuMatrix(A), B.diag, C)
+end
+
+function LinearAlgebra.mul!(C::CuMatrix{T}, A::Diagonal{T,<:CuVector}, B::Adjoint{T,<:CuMatrix}) where {T<:CublasFloat}
+    return dgmm!('L', CuMatrix(B), A.diag, C)
+end
+
 # symmetric mul!
 
 op_wrappers = ((identity, T -> 'N', identity),
--- test/libraries/cublas.jl
+++ test/libraries/cublas.jl
@@ -2293,6 +2293,30 @@ end
             d_Y = Diagonal(d_y)
             mul!(d_AY, d_A, d_Y)
             Array(d_AY) ≈ A * Diagonal(y)
+
+            YA = rand(elty,n,m)
+            d_YA = CuArray(YA)
+            d_Y = Diagonal(d_y)
+            mul!(d_YA, d_Y, transpose(d_A))
+            Array(d_YA) ≈ Diagonal(y) * transpose(A)
+
+            AX = rand(elty,n,m)
+            d_AX = CuArray(AX)
+            d_X = Diagonal(d_x)
+            mul!(d_AX, transpose(d_A), d_X)
+            Array(d_AX) ≈ transpose(A) * Diagonal(x)
+
+            YA = rand(elty,n,m)
+            d_YA = CuArray(YA)
+            d_Y = Diagonal(d_y)
+            mul!(d_YA, d_Y, d_A')
+            Array(d_YA) ≈ Diagonal(y) * A'
+
+            AX = rand(elty,n,m)
+            d_AX = CuArray(AX)
+            d_X = Diagonal(d_x)
+            mul!(d_AX, d_A', d_X)
+            Array(d_AX) ≈ A' * Diagonal(x)
         end
     end # extensions
 
