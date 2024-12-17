b8194cd0b9c0c08f69dec7a9a89fae3217120180
Anton Smirnov <tonysmn97@gmail.com>
2024-12-17 14:52:18 +0200

    upd

--- src/cache_allocator.jl
+++ src/cache_allocator.jl
@@ -1,13 +1,5 @@
-const CacheAllocatorName = ScopedValue(:none)
-
-const CuCacheAllocator = GPUArrays.PerDeviceCacheAllocator(CuArray)
-
-GPUArrays.cache_alloc_scope(::CUDABackend) = CacheAllocatorName
+const CuCacheAllocator = GPUArrays.PerDeviceCacheAllocator(CuArray; free_immediately=true)
 
 GPUArrays.cache_allocator(::CUDABackend) = CuCacheAllocator
 
-GPUArrays.free_busy_cache_alloc!(pdcache::GPUArrays.PerDeviceCacheAllocator{CuArray}, name::Symbol) =
-    GPUArrays.free_busy!(GPUArrays.named_cache_allocator!(pdcache, CUDA.device(), name))
-
-GPUArrays.invalidate_cache_allocator!(pdcache::GPUArrays.PerDeviceCacheAllocator{CuArray}, name::Symbol) =
-    GPUArrays.invalidate_cache_allocator!(pdcache, CUDA.device(), name)
+GPUArrays.device(::CUDABackend) = CUDA.device()
