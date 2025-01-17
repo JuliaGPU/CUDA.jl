0df63e1d88be43ce3813549885e100c3abddac60
Valentin Churavy <v.churavy@gmail.com>
2025-01-17 20:15:18 +0100

    don't use Ref for argument passing

--- lib/cudadrv/execution.jl
+++ lib/cudadrv/execution.jl
@@ -2,6 +2,16 @@
 
 export cudacall
 
+# In contrast to `Base.RefValue` we just need a container for both pass-by-ref (Symbol),
+# and pass-by-value (immutable structs).
+mutable struct Arg{T}
+    const val::T
+end
+
+function Base.unsafe_convert(P::Union{Type{Ptr{T}},Type{Ptr{Cvoid}}}, b::Arg{T})::P where T
+    # TODO: What to do if T is not a leaftype (compare case 3 for RefValue)
+    return pointer_from_objref(b)
+end
 
 ## device
 
@@ -21,7 +31,7 @@ export cudacall
     arg_refs = Vector{Symbol}(undef, length(args))
     for i in 1:length(args)
         arg_refs[i] = gensym()
-        push!(ex.args, :($(arg_refs[i]) = Base.RefValue(args[$i])))
+        push!(ex.args, :($(arg_refs[i]) = $Arg(args[$i])))
     end
 
     # generate an array with pointers
@@ -141,11 +151,7 @@ end
         converted_args[i] = gensym()
         arg_ptrs[i] = gensym()
         push!(ex.args, :($(converted_args[i]) = Base.cconvert($(types[i]), args[$i])))
-        if types[i] === Symbol
-            push!(ex.args, :($(arg_ptrs[i]) = Base.pointer_from_objref($(converted_args[i]))))
-        else
-            push!(ex.args, :($(arg_ptrs[i]) = Base.unsafe_convert($(types[i]), $(converted_args[i]))))
-        end
+        push!(ex.args, :($(arg_ptrs[i]) = Base.unsafe_convert($(types[i]), $(converted_args[i]))))
     end
 
     append!(ex.args, (quote
