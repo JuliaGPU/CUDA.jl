7fc03c863020aa57df17e45006e77ad4720fe633
Tim Besard <tim.besard@gmail.com>
2025-02-06 10:40:35 +0100

    Remove unneeded import.

--- lib/cublas/CUBLAS.jl
+++ lib/cublas/CUBLAS.jl
@@ -4,7 +4,7 @@ using ..APIUtils
 
 using ..CUDA
 using ..CUDA: CUstream, cuComplex, cuDoubleComplex, libraryPropertyType, cudaDataType, i32
-using ..CUDA: unsafe_free!, retry_reclaim, isdebug, @sync, initialize_context, CuRefArray, AbstractMemory
+using ..CUDA: unsafe_free!, retry_reclaim, isdebug, @sync, initialize_context, CuRefArray
 
 using ..CUDA: CUDA_Runtime
 using ..CUDA_Runtime
