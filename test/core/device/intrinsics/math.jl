c3c1530f0a012f2cfdd726543d225456fc8c0f5b
Katharine Hyatt <khyatt@flatironinstitute.org>
2025-02-27 11:41:58 -0500

    Use map to avoid spawing lots of kernels

--- test/core/device/intrinsics/math.jl
+++ test/core/device/intrinsics/math.jl
@@ -114,9 +114,7 @@ using SpecialFunctions
     @testset "Float16 - $op" for op in (log,exp,exp2,exp10,log2,log10)
         all_float_16 = collect(reinterpret(Float16, pattern) for pattern in  UInt16(0):UInt16(1):typemax(UInt16))
         all_float_16 = filter(!isnan, all_float_16)
-        for each_float in all_float_16
-            @test testf(x->op.(x), Float16[each_float])
-        end
+        @test testf(x->map(op, x), all_float_16)
     end
 
     @testset "fastmath" begin
