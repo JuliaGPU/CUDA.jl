d0e067649fb6daaf607a53990b7f2108a31d4c65
Katharine Hyatt <kshyatt@users.noreply.github.com>
2025-03-22 12:38:21 -0400

    Small fixes and missed tests for CUTENSORNET (#2713)
    * Small fixes and missed tests for CUTENSORNET
    
    * More fixes and tests

--- lib/cutensornet/test/runtests.jl
+++ lib/cutensornet/test/runtests.jl
@@ -15,6 +15,37 @@ import cuTensorNet: CuTensorNetwork, rehearse_contraction, perform_contraction!,
 using TensorOperations
 
 @testset "cuTensorNet" begin
+    @testset "Helpers and types" begin
+        @test convert(cuTensorNet.cutensornetComputeType_t, Int8)  == cuTensorNet.CUTENSORNET_COMPUTE_8I
+        @test convert(cuTensorNet.cutensornetComputeType_t, UInt8) == cuTensorNet.CUTENSORNET_COMPUTE_8U
+        @test convert(cuTensorNet.cutensornetComputeType_t, Float16) == cuTensorNet.CUTENSORNET_COMPUTE_16F
+        @test convert(cuTensorNet.cutensornetComputeType_t, Int32)  == cuTensorNet.CUTENSORNET_COMPUTE_32I
+        @test convert(cuTensorNet.cutensornetComputeType_t, UInt32) == cuTensorNet.CUTENSORNET_COMPUTE_32U
+        @test_throws ArgumentError("cuTensorNet type equivalent for compute type ComplexF64 does not exist!") convert(cuTensorNet.cutensornetComputeType_t, ComplexF64)
+        @test convert(Type, cuTensorNet.CUTENSORNET_COMPUTE_8I) == Int8
+        @test convert(Type, cuTensorNet.CUTENSORNET_COMPUTE_8U) == UInt8
+        @test convert(Type, cuTensorNet.CUTENSORNET_COMPUTE_16F) == Float16
+        @test convert(Type, cuTensorNet.CUTENSORNET_COMPUTE_32F) == Float32
+        @test convert(Type, cuTensorNet.CUTENSORNET_COMPUTE_32U) == UInt32
+        @test convert(Type, cuTensorNet.CUTENSORNET_COMPUTE_32I) == Int32
+        @test convert(Type, cuTensorNet.CUTENSORNET_COMPUTE_64F) == Float64
+
+        
+        modesA = ['m', 'h', 'k', 'n']
+        extent = Dict{Char, Int}()
+        extent['m'] = 96;
+        extent['n'] = 96;
+        extent['h'] = 64;
+        extent['k'] = 64;
+        extentsA = [extent[mode] for mode in modesA]
+        @testset for elty in [Float32, Float64, ComplexF32, ComplexF64]
+            A = CUDA.rand(elty, extentsA...)
+            descA = cuTensorNet.CuTensorDescriptor(A, modesA)
+            @test ndims(descA) == ndims(A)
+            @test size(descA)  == size(A)
+            @test strides(descA) == strides(A)
+        end
+    end
     n = 8
     m = 16
     k = 32
@@ -84,6 +115,10 @@ using TensorOperations
             @test cuTensorNet.reduced_extent(info)   == n
             @test cuTensorNet.discarded_weight(info) ≈ 0.0
             @test collect(U)*diagm(collect(S))*collect(V) ≈ collect(A)
+            config = cuTensorNet.CuTensorSVDConfig()
+            @test cuTensorNet.abs_cutoff(config) == 0.0
+            @test cuTensorNet.rel_cutoff(config) == 0.0
+            @test cuTensorNet.normalization(config) == cuTensorNet.CUTENSORNET_TENSOR_SVD_NORMALIZATION_NONE
         end
         @testset "GateSplit" begin
             a = 16
