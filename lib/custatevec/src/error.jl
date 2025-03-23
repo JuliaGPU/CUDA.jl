05db2516fc5c936fd200a8a6f341fcb8115f2589
Katharine Hyatt <khyatt@flatironinstitute.org>
2025-03-23 13:21:32 -0400

    Add return statements

--- lib/custatevec/src/error.jl
+++ lib/custatevec/src/error.jl
@@ -13,30 +13,30 @@ name(err)  = string(err.code)
 
 function description(err)
     if err.code == CUSTATEVEC_STATUS_SUCCESS
-        "the operation completed successfully"
+        return "the operation completed successfully"
     elseif err.code == CUSTATEVEC_STATUS_NOT_INITIALIZED
-        "the library was not initialized"
+        return "the library was not initialized"
     elseif err.code == CUSTATEVEC_STATUS_ALLOC_FAILED
-        "the resource allocation failed"
+        return "the resource allocation failed"
     elseif err.code == CUSTATEVEC_STATUS_INVALID_VALUE
-        "an invalid value was used as an argument"
+        return "an invalid value was used as an argument"
     elseif err.code == CUSTATEVEC_STATUS_ARCH_MISMATCH
-        "an absent device architectural feature is required"
+        return "an absent device architectural feature is required"
     elseif err.code == CUSTATEVEC_STATUS_EXECUTION_FAILED
-        "the GPU program failed to execute"
+        return "the GPU program failed to execute"
     elseif err.code == CUSTATEVEC_STATUS_INTERNAL_ERROR
-        "an internal operation failed"
+        return "an internal operation failed"
     elseif err.code == CUSTATEVEC_STATUS_NOT_SUPPORTED
-        "the API is not supported by the backend."
+        return "the API is not supported by the backend."
     elseif err.code == CUSTATEVEC_STATUS_INSUFFICIENT_WORKSPACE
-        "the workspace on the device is too small to execute."
+        return "the workspace on the device is too small to execute."
     elseif err.code == CUSTATEVEC_STATUS_SAMPLER_NOT_PREPROCESSED
-        "the sampler was called prior to preprocessing."
+        return "the sampler was called prior to preprocessing."
     elseif err.code == CUSTATEVEC_STATUS_NO_DEVICE_ALLOCATOR
-        "the device memory pool was not set."
+        return "the device memory pool was not set."
     elseif err.code == CUSTATEVEC_STATUS_DEVICE_ALLOCATOR_ERROR
-        "operation with the device memory pool failed"
+        return "operation with the device memory pool failed"
     else
-        "no description for this error"
+        return "no description for this error"
     end
 end
