cd4c6d2c19ef8456a8010222e31436219a569361
fps <mista.tapas@gmx.net>
2025-04-21 21:18:50 +0200

    Make runic happy

--- src/debug.jl
+++ src/debug.jl
@@ -1,8 +1,8 @@
 # debug functionality
 
-function isdebug(group, mod=CUDA)
+function isdebug(group, mod = CUDA)
     level = Base.CoreLogging.Debug
     logger = Base.CoreLogging.current_logger_for_env(level, group, mod)
     # TODO: Which id to choose here instead of 0?
-    logger !== nothing && Base.CoreLogging.shouldlog(logger, level, mod, group, 0)
+    return logger !== nothing && Base.CoreLogging.shouldlog(logger, level, mod, group, 0)
 end
