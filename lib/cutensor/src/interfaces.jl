ebab5902ba8d837a1bb625c2916343d157496134
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2025-05-08 14:42:46 -0300

    Match output storage type

--- lib/cutensor/src/interfaces.jl
+++ lib/cutensor/src/interfaces.jl
@@ -30,7 +30,7 @@ function Base.:(*)(A::CuTensor, B::CuTensor)
     B_sizes = map(x->size(B,x[1]), B_uniqs)
     A_inds = map(x->x[2], A_uniqs)
     B_inds = map(x->x[2], B_uniqs)
-    C = CuTensor(CUDA.zeros(tC, Dims(vcat(A_sizes, B_sizes))), vcat(A_inds, B_inds))
+    C = CuTensor(fill!(similar(B.data, tC, Dims(vcat(A_sizes, B_sizes))), zero(tC)), vcat(A_inds, B_inds))
     return mul!(C, A, B)
 end
 
