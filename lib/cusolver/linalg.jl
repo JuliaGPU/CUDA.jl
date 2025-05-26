252e0ce77edc4e8f594b1fe2fa196b62c9975e12
Matteo Seclì <secli.matteo@gmail.com>
2025-05-26 17:38:14 +0200

    Add returns

--- lib/cusolver/linalg.jl
+++ lib/cusolver/linalg.jl
@@ -114,71 +114,71 @@ end
 
 function LinearAlgebra.eigen(A::Symmetric{T,<:CuMatrix}) where {T<:BlasReal}
     A2 = copy(A.data)
-    Eigen(syevd!('V', 'U', A2)...)
+    return Eigen(syevd!('V', 'U', A2)...)
 end
 function LinearAlgebra.eigen(A::Hermitian{T,<:CuMatrix}) where {T<:BlasComplex}
     A2 = copy(A.data)
-    Eigen(heevd!('V', 'U', A2)...)
+    return Eigen(heevd!('V', 'U', A2)...)
 end
 function LinearAlgebra.eigen(A::Hermitian{T,<:CuMatrix}) where {T<:BlasReal}
-    eigen(Symmetric(A))
+    return eigen(Symmetric(A))
 end
 
 function LinearAlgebra.eigen(A::CuMatrix{T}) where {T<:BlasReal}
     A2 = copy(A)
     r = Xgeev!('N', 'V', A2)
-    Eigen(r[1], r[3])
+    return Eigen(r[1], r[3])
 end
 function LinearAlgebra.eigen(A::CuMatrix{T}) where {T<:BlasComplex}
     A2 = copy(A)
     r = Xgeev!('N', 'V', A2)
-    Eigen(r[1], r[3])
+    return Eigen(r[1], r[3])
 end
 
 # eigvals
 
-function LinearAlgebra.eigvals(A::Symmetric{T,<:CuMatrix}) where {T<:BlasReal}
+function LinearAlgebra.eigvals(A::Symmetric{T, <:CuMatrix}) where {T <: BlasReal}
     A2 = copy(A.data)
-    syevd!('N', 'U', A2)[1]
+    return syevd!('N', 'U', A2)[1]
 end
-function LinearAlgebra.eigvals(A::Hermitian{T,<:CuMatrix}) where {T<:BlasComplex}
+function LinearAlgebra.eigvals(A::Hermitian{T, <:CuMatrix}) where {T <: BlasComplex}
     A2 = copy(A.data)
-    heevd!('N', 'U', A2)[1]
+    return heevd!('N', 'U', A2)[1]
 end
-function LinearAlgebra.eigvals(A::Hermitian{T,<:CuMatrix}) where {T<:BlasReal}
-    eigvals(Symmetric(A))
+function LinearAlgebra.eigvals(A::Hermitian{T, <:CuMatrix}) where {T <: BlasReal}
+    return eigvals(Symmetric(A))
 end
 
-function LinearAlgebra.eigvals(A::CuMatrix{T}) where {T<:BlasReal}
+function LinearAlgebra.eigvals(A::CuMatrix{T}) where {T <: BlasReal}
     A2 = copy(A)
-    Xgeev!('N', 'N', A2)[1]
+    return Xgeev!('N', 'N', A2)[1]
 end
-function LinearAlgebra.eigvals(A::CuMatrix{T}) where {T<:BlasComplex}
+function LinearAlgebra.eigvals(A::CuMatrix{T}) where {T <: BlasComplex}
     A2 = copy(A)
-    Xgeev!('N', 'N', A2)[1]
+    return Xgeev!('N', 'N', A2)[1]
 end
 
 # eigvecs
 
-function LinearAlgebra.eigvecs(A::Symmetric{T,<:CuMatrix}) where {T<:BlasReal}
+function LinearAlgebra.eigvecs(A::Symmetric{T, <:CuMatrix}) where {T <: BlasReal}
     A2 = copy(A.data)
-    syevd!('V', 'U', A2)[2]
+    return syevd!('V', 'U', A2)[2]
 end
-function LinearAlgebra.eigvecs(A::Hermitian{T,<:CuMatrix}) where {T<:BlasComplex}
+function LinearAlgebra.eigvecs(A::Hermitian{T, <:CuMatrix}) where {T <: BlasComplex}
     A2 = copy(A.data)
-    heevd!('V', 'U', A2)[2]
+    return heevd!('V', 'U', A2)[2]
 end
-function LinearAlgebra.eigvecs(A::Hermitian{T,<:CuMatrix}) where {T<:BlasReal}
-    eigvals(Symmetric(A))
+function LinearAlgebra.eigvecs(A::Hermitian{T, <:CuMatrix}) where {T <: BlasReal}
+    return eigvals(Symmetric(A))
 end
 
-function LinearAlgebra.eigvecs(A::CuMatrix{T}) where {T<:BlasReal}
+function LinearAlgebra.eigvecs(A::CuMatrix{T}) where {T <: BlasReal}
     A2 = copy(A)
-    Xgeev!('N', 'V', A2)[3]
+    return Xgeev!('N', 'V', A2)[3]
 end
-function LinearAlgebra.eigvecs(A::CuMatrix{T}) where {T<:BlasComplex}
+function LinearAlgebra.eigvecs(A::CuMatrix{T}) where {T <: BlasComplex}
     A2 = copy(A)
-    Xgeev!('N', 'V', A2)[3]
+    return Xgeev!('N', 'V', A2)[3]
 end
 
 # factorizations
