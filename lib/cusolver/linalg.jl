4becc5a87baaf27a9943c407f94f8e0273c06e1f
Matteo Seclì <secli.matteo@gmail.com>
2025-05-26 17:28:36 +0200

    Expand eigen() and add eig[vals,vecs]()
    - Expand LinearAlgebra.eigen() to handle non-symmetric matrices via recent `Xgeev`
    - Add LinearAlgebra.eigvals() and LinearAlgebra.eigvecs()

--- lib/cusolver/linalg.jl
+++ lib/cusolver/linalg.jl
@@ -110,7 +110,7 @@ function Base.:\(F::Union{LinearAlgebra.LAPACKFactorizations{<:Any,<:CuArray},
     return LinearAlgebra._cut_B(BB, 1:n)
 end
 
-# eigenvalues
+# eigen
 
 function LinearAlgebra.eigen(A::Symmetric{T,<:CuMatrix}) where {T<:BlasReal}
     A2 = copy(A.data)
@@ -126,13 +126,60 @@ end
 
 function LinearAlgebra.eigen(A::CuMatrix{T}) where {T<:BlasReal}
     A2 = copy(A)
-    issymmetric(A) ? Eigen(syevd!('V', 'U', A2)...) : error("GPU eigensolver supports only Hermitian or Symmetric matrices.")
+    r = Xgeev!('N', 'V', A2)
+    Eigen(r[1], r[3])
 end
 function LinearAlgebra.eigen(A::CuMatrix{T}) where {T<:BlasComplex}
     A2 = copy(A)
-    ishermitian(A) ? Eigen(heevd!('V', 'U', A2)...) : error("GPU eigensolver supports only Hermitian or Symmetric matrices.")
+    r = Xgeev!('N', 'V', A2)
+    Eigen(r[1], r[3])
 end
 
+# eigvals
+
+function LinearAlgebra.eigvals(A::Symmetric{T,<:CuMatrix}) where {T<:BlasReal}
+    A2 = copy(A.data)
+    syevd!('N', 'U', A2)[1]
+end
+function LinearAlgebra.eigvals(A::Hermitian{T,<:CuMatrix}) where {T<:BlasComplex}
+    A2 = copy(A.data)
+    heevd!('N', 'U', A2)[1]
+end
+function LinearAlgebra.eigvals(A::Hermitian{T,<:CuMatrix}) where {T<:BlasReal}
+    eigvals(Symmetric(A))
+end
+
+function LinearAlgebra.eigvals(A::CuMatrix{T}) where {T<:BlasReal}
+    A2 = copy(A)
+    Xgeev!('N', 'N', A2)[1]
+end
+function LinearAlgebra.eigvals(A::CuMatrix{T}) where {T<:BlasComplex}
+    A2 = copy(A)
+    Xgeev!('N', 'N', A2)[1]
+end
+
+# eigvecs
+
+function LinearAlgebra.eigvecs(A::Symmetric{T,<:CuMatrix}) where {T<:BlasReal}
+    A2 = copy(A.data)
+    syevd!('V', 'U', A2)[2]
+end
+function LinearAlgebra.eigvecs(A::Hermitian{T,<:CuMatrix}) where {T<:BlasComplex}
+    A2 = copy(A.data)
+    heevd!('V', 'U', A2)[2]
+end
+function LinearAlgebra.eigvecs(A::Hermitian{T,<:CuMatrix}) where {T<:BlasReal}
+    eigvals(Symmetric(A))
+end
+
+function LinearAlgebra.eigvecs(A::CuMatrix{T}) where {T<:BlasReal}
+    A2 = copy(A)
+    Xgeev!('N', 'V', A2)[3]
+end
+function LinearAlgebra.eigvecs(A::CuMatrix{T}) where {T<:BlasComplex}
+    A2 = copy(A)
+    Xgeev!('N', 'V', A2)[3]
+end
 
 # factorizations
 
