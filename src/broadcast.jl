fe503ecac6181b5a10bb82c9a6882e545fc538e7
Oscar Smith <oscardssmith@gmail.com>
2025-09-08 20:13:11 -0400

    fix more explicit imports

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -6,18 +6,18 @@ struct CuArrayStyle{N,B} <: AbstractGPUArrayStyle{N} end
 CuArrayStyle{M,B}(::Val{N}) where {N,M,B} = CuArrayStyle{N,B}()
 
 # identify the broadcast style of a (wrapped) CuArray
-BroadcastStyle(::Type{<:CuArray{T,N,B}}) where {T,N,B} = CuArrayStyle{N,B}()
-BroadcastStyle(W::Type{<:AnyCuArray{T,N}}) where {T,N} =
+Broadcast.BroadcastStyle(::Type{<:CuArray{T,N,B}}) where {T,N,B} = CuArrayStyle{N,B}()
+Broadcast.BroadcastStyle(W::Type{<:AnyCuArray{T,N}}) where {T,N} =
     CuArrayStyle{N, memory_type(Adapt.unwrap_type(W))}()
 
 # when we are dealing with different memory types, we cannot know
 # which one is better, so use unified memory
-BroadcastStyle(::CUDA.CuArrayStyle{N1, B1},
+Broadcast.BroadcastStyle(::CUDA.CuArrayStyle{N1, B1},
                ::CUDA.CuArrayStyle{N2, B2}) where {N1,N2,B1,B2} =
     CuArrayStyle{max(N1,N2), UnifiedMemory}()
 
 # resolve ambiguity: different N, same memory type
-BroadcastStyle(::CUDA.CuArrayStyle{N1, B},
+Broadcast.BroadcastStyle(::CUDA.CuArrayStyle{N1, B},
                ::CUDA.CuArrayStyle{N2, B}) where {N1,N2,B} =
     CuArrayStyle{max(N1,N2), B}()
 
