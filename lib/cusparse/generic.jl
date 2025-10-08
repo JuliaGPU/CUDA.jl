740196784e2868f4da54357a8ae012906f617304
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-10-08 01:03:58 -0500

    [CUSPARSE] Interface generic mv! for SparseMatrixBSR

--- lib/cusparse/generic.jl
+++ lib/cusparse/generic.jl
@@ -230,8 +230,10 @@ function vv!(transx::SparseChar, X::CuSparseVector{T}, Y::DenseCuVector{T}, inde
     return result[]
 end
 
-function mv!(transa::SparseChar, alpha::Number, A::Union{CuSparseMatrixCSC{TA},CuSparseMatrixCSR{TA},CuSparseMatrixCOO{TA}}, X::DenseCuVector{T},
+function mv!(transa::SparseChar, alpha::Number, A::CuSparseMatrix{TA}, X::DenseCuVector{T},
              beta::Number, Y::DenseCuVector{T}, index::SparseChar, algo::cusparseSpMVAlg_t=CUSPARSE_SPMV_ALG_DEFAULT) where {TA, T}
+
+    (A isa CuSparseMatrixBSR) && (CUSPARSE.version() < v"12.6.3") && throw(ErrorException("This operation is not supported by the current CUDA version."))
 
     # Support transa = 'C' for real matrices
     transa = T <: Real && transa == 'C' ? 'T' : transa
@@ -388,12 +390,12 @@ function bmm!(transa::SparseChar, transb::SparseChar, alpha::Number, A::CuSparse
         return out[]
     end
     with_workspace(bufferSize) do buffer
-        # We should find a way to reuse the buffer (issue #1362)
-        if !(A isa CuSparseMatrixCOO)
-            cusparseSpMM_preprocess(
-                handle(), transa, transb, Ref{T}(alpha), descA, descB, Ref{T}(beta),
-                descC, T, algo, buffer)
-        end
+        # Uncomment if we find a way to reuse the buffer (issue #1362)
+        # if !(A isa CuSparseMatrixCOO)
+        #     cusparseSpMM_preprocess(
+        #         handle(), transa, transb, Ref{T}(alpha), descA, descB, Ref{T}(beta),
+        #         descC, T, algo, buffer)
+        # end
         cusparseSpMM(
             handle(), transa, transb, Ref{T}(alpha), descA, descB, Ref{T}(beta),
             descC, T, algo, buffer)
@@ -442,12 +444,12 @@ function mm!(transa::SparseChar, transb::SparseChar, alpha::Number, A::DenseCuMa
         return out[]
     end
     with_workspace(bufferSize) do buffer
-        # We should find a way to reuse the buffer (issue #1362)
-        if !(B isa CuSparseMatrixCOO)
-            cusparseSpMM_preprocess(
-                handle(), transb, transa, Ref{T}(alpha), descB, descA, Ref{T}(beta),
-                descC, T, algo, buffer)
-        end
+        # Uncomment if we find a way to reuse the buffer (issue #1362)
+        # if !(B isa CuSparseMatrixCOO)
+        #     cusparseSpMM_preprocess(
+        #         handle(), transb, transa, Ref{T}(alpha), descB, descA, Ref{T}(beta),
+        #         descC, T, algo, buffer)
+        # end
         cusparseSpMM(
             handle(), transb, transa, Ref{T}(alpha), descB, descA, Ref{T}(beta),
             descC, T, algo, buffer)
