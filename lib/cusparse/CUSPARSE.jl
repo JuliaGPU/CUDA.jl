f7a1927f2cbfe074613cce7dbf3df2e32c536447
Katharine Hyatt <khyatt@flatironinstitute.org>
2025-10-14 04:05:48 -0400

    Try integrating with the GPUArrays sparse migration

--- lib/cusparse/CUSPARSE.jl
+++ lib/cusparse/CUSPARSE.jl
@@ -47,11 +47,6 @@ include("generic.jl")
 # high-level integrations
 include("interfaces.jl")
 
-# native functionality
-include("device.jl")
-include("broadcast.jl")
-include("reduce.jl")
-
 include("batched.jl")
 
 
