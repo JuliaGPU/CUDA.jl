0048e5a51a7504c1772044b007f0fe845058ba23
Katharine Hyatt <kshyatt@users.noreply.github.com>
2025-11-19 14:33:11 +0100

    Add some matrix functions for symm/herm (#2962)

--- test/libraries/cusolver/dense.jl
+++ test/libraries/cusolver/dense.jl
@@ -945,3 +945,19 @@ end
         @inferred d_A \ d_b
     end
 end
+
+@testset "Hermitian/Symmetric matrix functions, elty = $elty" for elty in [Float32, Float64, ComplexF32, ComplexF64]
+    A = rand(elty, n, n)
+    Ah = A * A' # make posdef for atan, asinh, atanh
+    d_Ah = CuArray(Ah)
+    @testset for func in (exp, cos, sin, tan, cosh, sinh, tanh, atan, asinh)
+        @test Array(func(d_Ah)) ≈ func(Ah)
+    end
+    @static if VERSION >= v"1.11.0" # not supported on 1.10 or for Complex
+        if elty <: Real
+            @testset for func in (cbrt,) # have to dispatch explicitly
+                @test Array(parent(func(Hermitian(d_Ah)))) ≈ func(Ah)
+            end
+        end
+    end
+end
