6db286af1487614c519fb6fad1b3b8ecf0cd4412
Erik Schnetter <schnetter@gmail.com>
2026-01-15 19:24:47 -0500

    Add cluster sync test

--- test/core/device/intrinsics/clusters.jl
+++ test/core/device/intrinsics/clusters.jl
@@ -30,5 +30,49 @@ end
 
 ###########################################################################################
 
+@testset "distributed shared memory" begin
+    function f(A::AbstractArray{Int32,3})
+        ti = threadIdx().x
+        nt = blockDim().x
+        @assert 1<=ti<=nt
+        bi = blockIdxInCluster().x
+        nb = clusterDim().x
+        @assert 1<=bi<=nb
+        ci = clusterIdx().x
+        nc = gridClusterDim().x
+        @assert 1<=ci<=nc
+
+        sm = CuStaticSharedArray(Int32, 8)
+        for i in 1:nb
+            sm[i] = -1
+        end
+        cluster_wait()
+
+        for i in 1:nb
+            dsm = CuDistributedSharedArray(sm, i)
+            dsm[bi] = bi
+        end
+        cluster_wait()
+
+        for i in 1:nb
+            A[i,bi,ci] = sm[i]
+        end
+        return nothing
+    end
+
+    A = CUDA.zeros(Int32, clustersize, clustersize, blocks ÷ clustersize)
+
+    threads = 1
+    clustersize = 4
+    blocks = 16
+    @cuda threads=threads blocks=blocks clustersize=clustersize f(A)
+
+    B = Array(A)
+    goodB = [i for i in 1:clustersize, bi in 1:clustersize, ci in 1:blocks ÷ clustersize]
+    @test B == goodB
+end
+
+###########################################################################################
+
 end
 end
