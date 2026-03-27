c49365944676fa45cbdff4377319b62c960b8ae2
Karl Pierce <karl.m.pierce@gmail.com>
2026-03-26 17:36:19 -0700

    Apply suggestion from @lkdvos
    Remove left over code. Will need to make something like this to define mul! in the future
    
    Co-authored-by: Lukas Devos <ldevos98@gmail.com>

--- lib/cutensor/src/blocksparse/interfaces.jl
+++ lib/cutensor/src/blocksparse/interfaces.jl
@@ -1,16 +1,3 @@
-## For now call contract in ITensor and rely on UnallocatedArrays to make 
-## C in a dry-run of the contraction.
-# function Base.:(*)(A::CuTensorBS, B::CuTensorBs)
-#     tC = promote_type(eltype(A), eltype(B))
-#     A_uniqs = [(idx, i) for (idx, i) in enumerate(A.inds) if !(i in B.inds)]
-#     B_uniqs = [(idx, i) for (idx, i) in enumerate(B.inds) if !(i in A.inds)]
-#     A_sizes = map(x->size(A,x[1]), A_uniqs)
-#     B_sizes = map(x->size(B,x[1]), B_uniqs)
-#     A_inds = map(x->x[2], A_uniqs)
-#     B_inds = map(x->x[2], B_uniqs)
-#     C = CuTensor(CUDA.zeros(tC, Dims(vcat(A_sizes, B_sizes))), vcat(A_inds, B_inds))
-#     return mul!(C, A, B)
-# end
 
 
 ## LinearAlgebra
