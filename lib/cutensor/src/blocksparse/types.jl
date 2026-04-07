3ebd626a4360d128c86b82414b4011c8997a0737
kmp5VT <kmp5@vt.edu>
2026-04-07 14:15:23 -0400

    Add type restrictions to CuTensorBS type to make downstream easier

--- lib/cutensor/src/blocksparse/types.jl
+++ lib/cutensor/src/blocksparse/types.jl
@@ -6,14 +6,17 @@ export CuTensorBS
 mutable struct CuTensorBS{T, N}
     nonzero_data::Vector{<:CuArray}
     inds::Vector{Int}
-    blocks_per_mode::Vector{Int}
+    blocks_per_mode::Vector{Int32}
     ## This expects a Vector{Tuple(Int)} right now
-    block_extents
+    block_extents::Vector{<:Tuple}
     ## This expects a Vector{Tuple(Int)} right now
-    nonzero_block_coords
+    nonzero_block_coords::Vector{NTuple{N,Int32}}
 
-    function CuTensorBS{T, N}(nonzero_data::Vector{<:CuArray}, 
-        blocks_per_mode::Vector{Int}, block_extents, nonzero_block_coords, inds::Vector) where {T<:Number, N}
+    function CuTensorBS{T, N}(nonzero_data, 
+        blocks_per_mode, 
+        block_extents, 
+        nonzero_block_coords, 
+        inds) where {T<:Number, N}
         CuArrayT = eltype(nonzero_data)
         @assert eltype(CuArrayT) == T
         # @assert ndims(CuArrayT) == N
@@ -23,7 +26,7 @@ mutable struct CuTensorBS{T, N}
 end
 
 function CuTensorBS(nonzero_data::Vector{<:CuArray{T}}, 
-    blocks_per_mode, block_extents, nonzero_block_coords, inds::Vector) where {T<:Number}
+    blocks_per_mode, block_extents, nonzero_block_coords, inds) where {T<:Number}
     CuTensorBS{T,length(block_extents)}(nonzero_data, 
     blocks_per_mode, block_extents, nonzero_block_coords, inds)
 end
@@ -33,7 +36,7 @@ function Base.size(T::CuTensorBS)
 end
 Base.length(T::CuTensorBS) = prod(size(T))
 nonzero_length(T::CuTensorBS) = sum(length.(T.nonzero_data))
-Base.ndims(T::CuTensorBS) = length(T.inds)
+Base.ndims(T::CuTensorBS) = Int32(length(T.inds))
 
 Base.strides(T::CuTensorBS) = vcat([[st...] for st in strides.(T.nonzero_data)]...)
 Base.eltype(T::CuTensorBS) = eltype(eltype(T.nonzero_data))
@@ -66,13 +69,13 @@ mutable struct CuTensorBSDescriptor
     handle::cutensorBlockSparseTensorDescriptor_t
     # inner constructor handles creation and finalizer of the descriptor
     function CuTensorBSDescriptor(
-        numModes,
-        numNonZeroBlocks,
-        numSectionsPerMode,
-        extent,
-        nonZeroCoordinates,
-        stride,
-        eltype)
+        numModes::Int32,
+        numNonZeroBlocks::Int64,
+        numSectionsPerMode::Vector{Int32},
+        extent::Vector{Int64},
+        nonZeroCoordinates::Vector{Int32},
+        stride, ## Union{Vector{Int64}, C_NULL},
+        eltype::Type)
 
         desc = Ref{cuTENSOR.cutensorBlockSparseTensorDescriptor_t}()
         cutensorCreateBlockSparseTensorDescriptor(handle(), desc, 
@@ -87,13 +90,13 @@ end
 
 ## This function assumes that strides are C_NULL, i.e. canonical stride
 function CuTensorBSDescriptor(
-    numModes,
-    numNonZeroBlocks,
-    numSectionsPerMode,
-    extent,
-    nonZeroCoordinates,
+    numModes::Int32,
+    numNonZeroBlocks::Int64,
+    numSectionsPerMode::Vector{Int32},
+    extent::Vector{Int64},
+    nonZeroCoordinates::Vector{Int32},
     # strides = C_NULL,
-    eltype)
+    eltype::Type)
 
     return CuTensorBSDescriptor(numModes, numNonZeroBlocks, numSectionsPerMode, extent, nonZeroCoordinates, C_NULL, eltype)
 end
@@ -108,12 +111,13 @@ end
 
 ## Descriptor function for CuTensorBS type. Please overwrite for custom objects
 function CuTensorBSDescriptor(A::CuTensorBS)
-    numModes = Int32(ndims(A))
-    numNonZeroBlocks = Int64(length(A.nonzero_block_coords))
-    numSectionsPerMode = collect(Int32, A.blocks_per_mode)
+    numModes = ndims(A)
+    numNonZeroBlocks = length(A.nonzero_block_coords)
+    numSectionsPerMode = collect(nblocks_per_mode(A))
     extent = block_extents(A)
-    nonZeroCoordinates =  Int32.(vcat([[x...] for x in A.nonzero_block_coords]...) .- 1)
+    nonZeroCoordinates = collect(Base.Iterators.flatten(A.nonzero_block_coords)) .- Int32(1)
     st = strides(A)
+
     dataType = eltype(A)
 
     ## Right now assume stride is NULL. I am not sure if stride works, need to discuss with cuTENSOR team.
