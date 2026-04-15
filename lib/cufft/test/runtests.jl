31c02719911b212501eb27ce756a7fc7232c3176
RainerHeintzmann <heintzmann@gmail.com>
2026-04-15 10:48:49 +0200

    also chaned `ensure_increasing` naming in the tests

--- lib/cufft/test/runtests.jl
+++ lib/cufft/test/runtests.jl
@@ -1,13 +1,13 @@
 include("setup.jl")
 @test cuFFT.version() isa VersionNumber
 
-@testset "ensure_raising" begin
-    @test (1,2,3) == cuFFT.ensure_raising((1,2,3))
-    @test (1,2,3) == cuFFT.ensure_raising((2,1,3))
-    @test (1,2,3) == cuFFT.ensure_raising((1,3,2))
-    @test (1,2,3) == cuFFT.ensure_raising((3,1,2))
-    @test (1,2,3) == cuFFT.ensure_raising((2,3,1))
-    @test (10,20,30) == cuFFT.ensure_raising((30,20,10))
+@testset "ensure_increasing" begin
+    @test (1,2,3) == cuFFT.ensure_increasing((1,2,3))
+    @test (1,2,3) == cuFFT.ensure_increasing((2,1,3))
+    @test (1,2,3) == cuFFT.ensure_increasing((1,3,2))
+    @test (1,2,3) == cuFFT.ensure_increasing((3,1,2))
+    @test (1,2,3) == cuFFT.ensure_increasing((2,3,1))
+    @test (10,20,30) == cuFFT.ensure_increasing((30,20,10))
 end
 
 include("complex.jl")
