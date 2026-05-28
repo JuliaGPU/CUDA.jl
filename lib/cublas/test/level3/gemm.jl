7e0e41f6ea1b337ec519ce0b83fef277cd268d3d
Łukasz Pawela <lukasz.pawela@gmail.com>
2026-05-28 17:32:16 +0200

    fix pointer types in `Hgemm`

--- lib/cublas/test/level3/gemm.jl
+++ lib/cublas/test/level3/gemm.jl
@@ -16,7 +16,7 @@ n = 35
 k = 13
 
 @testset "level 3" begin
-    @testset for elty in [Float32, Float64, ComplexF32, ComplexF64]
+    @testset for elty in [Float16, Float32, Float64, ComplexF32, ComplexF64]
 
         @testset "mul! C = $f(A) * $g(B) * $Ts(a) + C * $Ts(b)" for f in (identity, transpose, adjoint), g in (identity, transpose, adjoint), Ts in (Int, elty)
             C, A, B = rand(elty, 5, 5), rand(elty, 5, 5), rand(elty, 5, 5)
@@ -42,12 +42,14 @@ k = 13
             @test Array(dC) ≈ C
         end
 
-        @testset "hermitian" begin
-            C, A, B = rand(elty, 5, 5), Hermitian(rand(elty, 5, 5)), rand(elty, 5, 5)
-            dC, dA, dB = CuArray(C), Hermitian(CuArray(A)), CuArray(B)
-            mul!(dC, dA, dB)
-            mul!(C, A, B)
-            @test Array(dC) ≈ C
+        if !(elty <: Float16)
+            @testset "hermitian" begin
+                C, A, B = rand(elty, 5, 5), Hermitian(rand(elty, 5, 5)), rand(elty, 5, 5)
+                dC, dA, dB = CuArray(C), Hermitian(CuArray(A)), CuArray(B)
+                mul!(dC, dA, dB)
+                mul!(C, A, B)
+                @test Array(dC) ≈ C
+            end
         end
 
         @testset "gemm!" begin
@@ -130,42 +132,44 @@ k = 13
             @test C1 ≈ h_C1
             @test C1 ≈ h_C2
         end
-        @testset "symm!" begin
-            alpha = rand(elty)
-            beta = rand(elty)
-            sA = rand(elty,m,m)
-            sA = sA + transpose(sA)
-            dsA = CuArray(sA)
-            B = rand(elty,m,n)
-            C = rand(elty,m,n)
-            Bbad = rand(elty,m+1,n+1)
-            d_B = CuArray(B)
-            d_C = CuArray(C)
-            d_Bbad = CuArray(Bbad)
-            cuBLAS.symm!('L','U',alpha,dsA,d_B,beta,d_C)
-            C = (alpha*sA)*B + beta*C
-            # compare
-            h_C = Array(d_C)
-            @test C ≈ h_C
-            @test_throws DimensionMismatch cuBLAS.symm!('L','U',alpha,dsA,d_Bbad,beta,d_C)
-        end
 
-        @testset "symm" begin
-            sA = rand(elty,m,m)
-            sA = sA + transpose(sA)
-            dsA = CuArray(sA)
-            B = rand(elty,m,n)
-            C = rand(elty,m,n)
-            Bbad = rand(elty,m+1,n+1)
-            d_B = CuArray(B)
-            d_C = CuArray(C)
-            d_Bbad = CuArray(Bbad)
-            d_C = cuBLAS.symm('L','U',dsA,d_B)
-            C = sA*B
-            # compare
-            h_C = Array(d_C)
-            @test C ≈ h_C
-            @test_throws DimensionMismatch cuBLAS.symm('L','U',dsA,d_Bbad)
+        if !(elty <: Float16)
+            @testset "symm!" begin
+                alpha = rand(elty)
+                beta = rand(elty)
+                sA = rand(elty,m,m)
+                sA = sA + transpose(sA)
+                dsA = CuArray(sA)
+                B = rand(elty,m,n)
+                C = rand(elty,m,n)
+                Bbad = rand(elty,m+1,n+1)
+                d_B = CuArray(B)
+                d_C = CuArray(C)
+                d_Bbad = CuArray(Bbad)
+                cuBLAS.symm!('L','U',alpha,dsA,d_B,beta,d_C)
+                C = (alpha*sA)*B + beta*C
+                # compare
+                h_C = Array(d_C)
+                @test C ≈ h_C
+                @test_throws DimensionMismatch cuBLAS.symm!('L','U',alpha,dsA,d_Bbad,beta,d_C)
+            end
+            @testset "symm" begin
+                sA = rand(elty,m,m)
+                sA = sA + transpose(sA)
+                dsA = CuArray(sA)
+                B = rand(elty,m,n)
+                C = rand(elty,m,n)
+                Bbad = rand(elty,m+1,n+1)
+                d_B = CuArray(B)
+                d_C = CuArray(C)
+                d_Bbad = CuArray(Bbad)
+                d_C = cuBLAS.symm('L','U',dsA,d_B)
+                C = sA*B
+                # compare
+                h_C = Array(d_C)
+                @test C ≈ h_C
+                @test_throws DimensionMismatch cuBLAS.symm('L','U',dsA,d_Bbad)
+            end
         end
 
         if elty <: Complex
