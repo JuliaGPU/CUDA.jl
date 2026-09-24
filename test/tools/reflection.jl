e04891815d54a886cf7d6cfa4c9638b47f067ef8
Tim Besard <tim.besard@gmail.com>
2026-09-24 17:47:43 +0200

    Only skip CUPTI SASS tests on affected versions.
    NVIDIA bug #4667039 was fixed in CUPTI 2025.1 (CUDA 12.8), so there's no
    need to keep skipping these tests on newer versions.

--- test/tools/reflection.jl
+++ test/tools/reflection.jl
@@ -6,7 +6,7 @@
     CUDA.code_warntype(devnull, dummy, Tuple{})
     CUDA.code_llvm(devnull, dummy, Tuple{})
     CUDA.code_ptx(devnull, dummy, Tuple{})
-    if can_use_cupti() && !(v"2024.2.0" <= CUPTI.library_version()) # NVIDIA bug #4667039
+    if can_use_cupti() && !(v"2024.2.0" <= CUPTI.library_version() < v"2025.1.0") # NVIDIA bug #4667039
         # functions defined in Julia
         sass = sprint(io->CUDA.code_sass(io, dummy, Tuple{}))
         @test occursin(".text._Z5dummy", sass)
@@ -25,7 +25,7 @@
     @device_code_warntype io=devnull @cuda dummy()
     @device_code_llvm io=devnull @cuda dummy()
     @device_code_ptx io=devnull @cuda dummy()
-    if can_use_cupti() && !(v"2024.2.0" <= CUPTI.library_version()) # NVIDIA bug #4667039
+    if can_use_cupti() && !(v"2024.2.0" <= CUPTI.library_version() < v"2025.1.0") # NVIDIA bug #4667039
         # functions defined in Julia
         sass = sprint(io->@device_code_sass io=io @cuda dummy())
         @test occursin(".text._Z5dummy", sass)
@@ -63,7 +63,7 @@
     @test occursin("dummy", sprint(io->(@device_code_llvm io=io optimize=false @cuda dummy())))
     @test occursin("dummy", sprint(io->(@device_code_llvm io=io @cuda dummy())))
     @test occursin("dummy", sprint(io->(@device_code_ptx io=io @cuda dummy())))
-    if can_use_cupti() && !(v"2024.2.0" <= CUPTI.library_version()) # NVIDIA bug #4667039
+    if can_use_cupti() && !(v"2024.2.0" <= CUPTI.library_version() < v"2025.1.0") # NVIDIA bug #4667039
         @test occursin("dummy", sprint(io->(@device_code_sass io=io @cuda dummy())))
     end
 
